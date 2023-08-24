986c975dcd62b23db81f9363748976c50944a0be
Green Gem <chris_waitt@me.com>
2023-08-24 18:43:41 +0100

    Added a Bootswatch theme to give me some inspiration.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootswatch/dist/sketchy/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
