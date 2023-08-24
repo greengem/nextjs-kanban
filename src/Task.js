7cf8ddc86877e709f4a749110b24a639bc375ad1
Green Gem <chris_waitt@me.com>
2023-08-24 16:29:45 +0100

    Added functionality to allow editing of the task text. Next steps are to implement logic to update the task text.

--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 function Task({ task, toggleCompleted }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(task.text);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleEditChange = (event) => {
+    setEditedText(event.target.value);
+  };
+
+  const handleEditSave = () => {
+    // Implement logic to update the task text
+
+    setIsEditing(false);
+  };
+
   return (
     <InputGroup className={`mb-3 task ${task.completed ? 'completed' : ''}`}>
       <InputGroup.Checkbox 
@@ -12,10 +29,24 @@ function Task({ task, toggleCompleted }) {
         checked={task.completed}
         aria-label="Checkbox for following text input"
       />
-      <Form.Control 
-        aria-label="Text input with checkbox" 
-        value={task.text}
-      />
+      {isEditing ? (
+        <Form.Control 
+          aria-label="Text input with checkbox" 
+          value={editedText}
+          onChange={handleEditChange}
+        />
+      ) : (
+        <Form.Control 
+          aria-label="Text input with checkbox" 
+          value={task.text}
+          readOnly
+        />
+      )}
+      {isEditing ? (
+        <Button onClick={handleEditSave}>Save</Button>
+      ) : (
+        <Button onClick={handleEditClick}>Edit</Button>
+      )}
     </InputGroup>
   );
 }
