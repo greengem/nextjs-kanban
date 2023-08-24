6e140ab14910cec713beae30e9bcfcbee3163fb2
Green Gem <chris_waitt@me.com>
2023-08-24 18:07:53 +0100

    feat: Add input text box for creating new tasks
    Added a new input text box above the "Add Task" button to allow users to enter the text for new tasks. The entered text is used to create a new task when the button is clicked. This enhancement improves the user experience by providing a more intuitive way to specify the task's content during creation.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import AppNavbar from './Navbar';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function App() {
   const [tasks, setTasks] = useState([
     { id: 1, text: 'Sample task 1', completed: false },
     { id: 2, text: 'Sample task 2', completed: true },
   ]);
+  const [newTaskText, setNewTaskText] = useState('');
 
   const addTask = () => {
-    const newTask = { id: tasks.length + 1, text: 'New task', completed: false };
-    setTasks([...tasks, newTask]);
+    if (newTaskText.trim() !== '') {
+      const newTask = { id: tasks.length + 1, text: newTaskText, completed: false };
+      setTasks([...tasks, newTask]);
+      setNewTaskText('');
+    }
   };
 
   const toggleCompleted = (taskId) => {
@@ -33,9 +39,17 @@ function App() {
       <Container className='mt-4'>
         <Card>
           <Card.Body>
-            <Card.Text>
+            <InputGroup className='mb-3'> 
               <Button variant="primary" onClick={addTask}>Add Task</Button>
-            </Card.Text>
+              <Form.Control
+                type="text"
+                placeholder="Enter task text"
+                value={newTaskText}
+                onChange={(e) => setNewTaskText(e.target.value)}
+                className='shadow-none'
+              />
+            </InputGroup>
+            
             {tasks.map((task) => (
               <Task
                 key={task.id}
