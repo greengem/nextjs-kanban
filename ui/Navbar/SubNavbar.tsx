c1e96077e685c09ae501867ecffc1ad23b17a6a9
Green Gem <chris_waitt@me.com>
2023-11-26 19:13:58 +0000

    Style sub navbar

--- ui/Navbar/SubNavbar.tsx
+++ ui/Navbar/SubNavbar.tsx
@@ -6,13 +6,12 @@ import Link from "next/link";
 export default function SubNavbar () {
 
     return (
-        <nav className="flex px-5 py-2 bg-gray-500 items-center justify-between">
-            <ul className="flex items-center text-xs space-x-3 text-white">
-                <li className="bg-blue-500 px-3 py-1 rounded-md"><Link href="#">Item 1</Link></li>
-                <li className="px-3 py-1"><Link href="#">Item 2</Link></li>
+        <nav className="flex px-5 py-2 bg-blue-400 items-center justify-between">
+            <ul className="flex items-center text-xs space-x-3 text-white overflow-x-scroll no-scrollbar">
+                <li className="px-3 py-1"><Link href="#">Item 1</Link></li>
+                <li className="bg-blue-500 rounded-md px-3 py-1"><Link href="#">Item 2</Link></li>
                 <li className="px-3 py-1"><Link href="#">Item 3</Link></li>
                 <li className="px-3 py-1"><Link href="#">Item 4</Link></li>
-
             </ul>
         </nav>
     )
