23132215edd37b621ee826b23c32718fef65ece0
Green Gem <chris_waitt@me.com>
2023-11-26 16:36:49 +0000

    Sidebar header to house the hide button and possibly a logo/brand name

---
+++ ui/Sidebar/SidebarHeader.tsx
@@ -0,0 +1,11 @@
+import { useSidebarVisibility } from "@/contexts/SidebarContext";
+import { IconArrowLeft, IconFlower } from "@tabler/icons-react";
+
+export default function SidebarHeader() {
+    const { isSidebarVisible, isSlimSidebar, toggleSidebar } = useSidebarVisibility();
+    return (
+        <div className="px-5 py-4 text-white">
+            <button onClick={toggleSidebar}><IconArrowLeft /></button>
+        </div>
+    )
+}
