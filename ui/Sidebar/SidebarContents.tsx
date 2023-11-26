2450033f6e36ffec09d9f09a495e2ec2e1c9dcd5
Green Gem <chris_waitt@me.com>
2023-11-26 19:14:14 +0000

    conditionally render sidebar width based on the context

--- ui/Sidebar/SidebarContents.tsx
+++ ui/Sidebar/SidebarContents.tsx
@@ -5,18 +5,30 @@ import SidebarNav from "./SidebarNav";
 
 export default function SidebarContents() {
     const { isSidebarVisible, isSlimSidebar } = useSidebarVisibility();
-  
+
+    const determineWidthClass = () => {
+      if (!isSidebarVisible) {
+        return 'w-0';
+      } else if (isSlimSidebar) {
+        return 'w-16';
+      } else {
+        return 'w-64';
+      }
+    };
+    
     return (
       <aside className={`
         bg-blue-500 
         z-10 
+        flex-none
         overflow-hidden 
         transition-width duration-500 ease-in-out 
-        ${isSidebarVisible ? 'w-64' : 'w-0'} 
-        ${isSlimSidebar ? 'w-16' : 'w-64'}`}
+        ${determineWidthClass()}`}
       >
-        <SidebarHeader />
-        <SidebarNav />
+        <div className="border-r-2 border-white h-full">
+          <SidebarHeader />
+          <SidebarNav />
+        </div>
       </aside>
     );
   }
