9c926506de0d45573cb5d58badc3215c33d767f9
Green Gem <chris_waitt@me.com>
2023-11-27 12:04:01 +0000

    Trying out TS Pick for my prisma data

---
+++ types/types.ts
@@ -0,0 +1,15 @@
+import { Board, Column, Task } from "@prisma/client";
+
+export type BoardSummary = Pick<Board, 'id' | 'title' | 'description' | 'createdAt' | 'updatedAt'>;
+
+export type ColumnWithTasks = {
+    id: string;
+    title: string;
+    tasks: TaskSummary[];
+};
+
+export type TaskSummary = Pick<Task, 'id' | 'title' | 'description' | 'priority' | 'dueDate' | 'createdAt' | 'updatedAt'>;
+
+export type BoardDetails = Pick<Board, 'id' | 'title' | 'description' | 'createdAt' | 'updatedAt'> & {
+    columns: ColumnWithTasks[];
+};
