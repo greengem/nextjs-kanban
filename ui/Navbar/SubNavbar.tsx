12a5b23b7a24414396fa95f71f949c2b9eea7409
Green Gem <chris_waitt@me.com>
2023-11-27 12:05:49 +0000

    Fetch boards to display in the sub navbar, i need to consoloate the fetching of boards or access a universal cache

--- ui/Navbar/SubNavbar.tsx
+++ ui/Navbar/SubNavbar.tsx
@@ -1,17 +1,14 @@
-'use client'
-import { useSidebarVisibility } from "@/contexts/SidebarContext"
-import { IconMenu2 } from "@tabler/icons-react";
+import { getBoardsSummary } from "@/lib/FetchData";
 import Link from "next/link";
 
-export default function SubNavbar () {
-
+export default async function SubNavbar () {
+    const boards = await getBoardsSummary();
     return (
         <nav className="flex px-5 py-2 bg-blue-400 items-center justify-between">
             <ul className="flex items-center text-xs space-x-3 text-white overflow-x-scroll no-scrollbar">
-                <li className="px-3 py-1"><Link href="#">Item 1</Link></li>
-                <li className="bg-blue-500 rounded-md px-3 py-1"><Link href="#">Item 2</Link></li>
-                <li className="px-3 py-1"><Link href="#">Item 3</Link></li>
-                <li className="px-3 py-1"><Link href="#">Item 4</Link></li>
+                {boards.map((board) => (
+                    <li key={board.id} className="px-3 py-1"><Link href={`/board/${board.id}`}>{board.title}</Link></li>
+                ))}
             </ul>
         </nav>
     )
