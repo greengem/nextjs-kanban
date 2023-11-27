fe2335d75b876ef5efe5c024a5b0d822cfa4f7bc
Green Gem <chris_waitt@me.com>
2023-11-27 16:30:11 +0000

    New form for creating tasks using server actions

---
+++ ui/Forms/CreateTaskForm.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { handleCreateTask } from "@/actions/TaskActions";
+import { useFormState, useFormStatus } from "react-dom";
+
+const initialState = {
+  message: "",
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <button 
+      type="submit" 
+      aria-disabled={pending}
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+    >
+      Create New Task
+    </button>
+  )
+}
+
+export default function CreateTaskForm({ boardId, columnId }: { boardId: string; columnId: string;  }) {
+  const [state, formAction] = useFormState(handleCreateTask, initialState)
+  return (
+    <form action={formAction}>
+      <div className="mb-3">
+        <label htmlFor="taskTitle" className="block mb-2 text-sm font-medium">Task Title</label>
+        <input type="text" id="taskTitle" name="taskTitle" className="w-full p-2 border rounded" required />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="taskDescription" className="block mb-2 text-sm font-medium">Task Description</label>
+        <input type="text" id="taskDescription" name="taskDescription" className="w-full p-2 border rounded" />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="taskPriority" className="block mb-2 text-sm font-medium">Task Priority</label>
+        <input type="text" id="taskPriority" name="taskPriority" className="w-full p-2 border rounded" />
+      </div>
+
+      <input type="hidden" name="boardId" value={boardId} />
+      <input type="hidden" name="columnId" value={columnId} />
+
+      <SubmitButton />
+      
+      <p aria-live="polite" className="sr-only" role="status">
+        {state?.message}
+      </p>
+      
+    </form>
+  )
+}
