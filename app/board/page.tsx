b87312e60e7cd0d9d5667eaa0ada689dc6319d5f
Green Gem <chris_waitt@me.com>
2023-11-27 16:44:14 +0000

    fix missing key in map

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -10,7 +10,7 @@ export default async function Boards() {
       <p>Boards</p>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
         {boards.map((board) => (
-          <div className="p-5 bg-blue-300 rounded-xl">
+          <div key={board.id} className="p-5 bg-blue-300 rounded-xl">
             <h4 className="font-semibold">{board.title}</h4>
             <p className="text-sm">{board.description}</p>
             <Link href={`/board/${board.id}`}>View</Link>
