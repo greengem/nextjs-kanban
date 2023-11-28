3347705f727a84a74c6f3d3a9474cec06410b044
Green Gem <chris_waitt@me.com>
2023-11-28 11:48:23 +0000

    Now we're using postgres we can cascade delete relational records, deleting a column will delete all its tasks

---
+++ prisma/migrations/20231128114648_cascade_delete/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Column" DROP CONSTRAINT "Column_boardId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Task" DROP CONSTRAINT "Task_columnId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Column" ADD CONSTRAINT "Column_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_columnId_fkey" FOREIGN KEY ("columnId") REFERENCES "Column"("id") ON DELETE CASCADE ON UPDATE CASCADE;
