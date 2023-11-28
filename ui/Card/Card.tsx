6d020f088944f77141fe89ca580a5787b5a3aa8c
Green Gem <chris_waitt@me.com>
2023-11-28 17:59:50 +0000

    Created a reusaable Card component

---
+++ ui/Card/Card.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from "react";
+
+export function Card({ children, className = '' }: { children: ReactNode, className?: string }) {
+    return (
+        <div className={`
+            w-full
+            flex-none
+            bg-white 
+            rounded-lg 
+            shadow-md
+            ${className}
+        `}>
+            {children}
+        </div>
+    )
+}
+
+export function CardHeader({ children, className = '' }: { children: ReactNode, className?: string }) {
+    return (
+        <div className={`
+            flex justify-between items-center 
+            p-3
+            ${className}
+        `}>
+            {children}
+        </div>
+    )
+}
+
+export function CardBody({ children, className = '' }: { children: ReactNode, className?: string }) {
+    return (
+        <div className={`
+            p-3
+            ${className}
+        `}>
+            {children}
+        </div>
+    )
+}
+
+export function CardFooter({ children, className = '' }: { children: ReactNode, className?: string }) {
+    return (
+        <div className={`
+            p-3
+            ${className}
+        `}>
+            {children}
+        </div>
+    )
+}
