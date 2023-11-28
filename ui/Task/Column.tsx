887658284e81428301a7314fa0ce2fc26b6a13ca
Green Gem <chris_waitt@me.com>
2023-11-28 18:21:24 +0000

    Updated column to use Card

--- ui/Task/Column.tsx
+++ ui/Task/Column.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import DeleteColumnForm from '../Forms/DeleteColumnForm';
 import CreateTaskFormSimple from '../Forms/CreateTaskFormSimple';
+import { Card, CardHeader, CardBody, CardFooter } from '@/ui/Card/Card';
 
 interface ColumnProps {
   title: string;
@@ -11,20 +12,20 @@ interface ColumnProps {
 
 const Column: React.FC<ColumnProps> = ({ title, columnId, boardId, children }) => {
   return (
-    <div className="w-full md:w-64">
-      <div className='flex justify-between items-center bg-blue-300 rounded-t-lg py-2 px-5'>
+    <Card>
+      <CardHeader>
         <h4 className='tracking-tight'>{title}</h4>
         <DeleteColumnForm boardId={boardId} columnId={columnId} columnTitle={title} />
-      </div>
-      <div className='bg-blue-200 px-2 py-3'>
-        <ul className='text-sm space-y-2'>
+      </CardHeader>
+      <CardBody>
+        <ul className='text-sm'>
           {children}
         </ul>
-      </div>
-      <div className='bg-blue-300 p-3 rounded-b-lg'>
+      </CardBody>
+      <CardFooter>
         <CreateTaskFormSimple boardId={boardId} columnId={columnId} />
-      </div>
-    </div>
+      </CardFooter>
+    </Card>
   );
 }
 
