2e13f80be842900a0f80bce3df485a4087684b4b
Green Gem <chris_waitt@me.com>
2023-11-28 18:21:40 +0000

    Task Item refactor and ui update

--- ui/Task/TaskItem.tsx
+++ ui/Task/TaskItem.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
+'use client'
 import DeleteTaskForm from '../Forms/DeleteTaskForm';
 import EditTaskForm from '../Forms/EditTaskForm';
-import { IconEdit } from "@tabler/icons-react";
+import { IconArrowDown, IconArrowUp, IconEdit, IconTrash } from '@tabler/icons-react';
+
 interface TaskItemProps {
   taskId: string;
   boardId: string;
+  columnId: string;
   title: string;
   priority: string | null;
 }
 
-const TaskItem: React.FC<TaskItemProps> = ({ title, priority, taskId, boardId }) => {
+const TaskItem: React.FC<TaskItemProps> = ({ title, priority, taskId, boardId, columnId }) => {
   const priorityClass = priority ? priority.toLowerCase() : 'default-priority';
 
   return (
-    <li className={`bg-blue-300 flex justify-between items-center rounded-lg px-3 py-1 ${priorityClass}`}>
-      <EditTaskForm title={title} boardId={boardId} taskId={taskId} />
-      <DeleteTaskForm taskId={taskId} boardId={boardId} taskTitle={title} />
+    <li className={`bg-blue-100 rounded-lg space-y-2 px-3 py-2 my-2 ${priorityClass}`}>
+      <div><EditTaskForm title={title} boardId={boardId} taskId={taskId} /></div>
+      <div className='flex justify-end gap-1'>
+        <button className='p-1 bg-blue-500 text-white rounded-md'><IconArrowUp size={14} /></button>
+        <button className='p-1 bg-blue-500 text-white rounded-md'><IconArrowDown size={14} /></button>
+        <DeleteTaskForm taskId={taskId} boardId={boardId} columnId={columnId} />
+      </div>
     </li>
   );
 }
