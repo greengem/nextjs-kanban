a026c71a3194f447d01ae6d7b68d8f717860ccf6
Green Gem <chris_waitt@me.com>
2023-11-30 14:37:48 +0000

    Allow images from github for avatars

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'avatars.githubusercontent.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'loremflickr.com',
+            port: '',
+          },
+        ]
+      }
 }
 
 module.exports = nextConfig
