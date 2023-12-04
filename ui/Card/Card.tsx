22723ae5e2e9922db165082611157a6c4c0653a8
Green Gem <chris_waitt@me.com>
2023-12-04 23:01:59 +0000

    Redesign and improvements to Card component

--- ui/Card/Card.tsx
+++ ui/Card/Card.tsx
@@ -1,53 +1,38 @@
 import { ReactNode } from "react";
+import { IconGripHorizontal } from '@tabler/icons-react';
 
 export function Card({ children, className = '' }: { children: ReactNode, className?: string }) {
     return (
-        <div className={`
-            flex-none
-            shadow-md
-            text-white
-            bg-zinc-900
-            rounded-lg
-            ${className}
-        `}>
+        <div className={`flex-none shadow-md text-white rounded-lg ${className}`}>
             {children}
         </div>
     )
 }
 
-export function CardHeaderGrab({ children, className = '' }: { children: ReactNode, className?: string }) {
+export function CardHeaderGrab() {
     return (
-        <div className={`
-            rounded-t-lg
-            py-1
-            text-purple-500
-            bg-zinc-800
-            cursor-grab
-            flex justify-center items-center
-            ${className}
-        `}>
-            {children}
+        <div className='py-1 text-purple-500 bg-zinc-800 cursor-grab flex justify-center items-center rounded-t-lg'>
+            <IconGripHorizontal />
         </div>
     )
 }
 
-export function CardHeader({ children, className = '' }: { children: ReactNode, className?: string }) {
+
+export function CardHeader({ children, className = '', showGrab = false }: { children: ReactNode, className?: string, showGrab?: boolean }) {
     return (
-        <div className={`
-            px-5 pt-3
-            ${className}
-        `}>
-            {children}
+        <div className={`${showGrab ? '' : 'rounded-t-lg'} ${className}`}>
+            {showGrab && <CardHeaderGrab />}
+            <div className={`px-5 pt-3 bg-zinc-900 ${showGrab ? '' : 'rounded-t-lg'}`}>
+                {children}
+            </div>
         </div>
     )
 }
+
 
 export function CardBody({ children, className = '' }: { children: ReactNode, className?: string }) {
     return (
-        <div className={`
-            p-3
-            ${className}
-        `}>
+        <div className={`p-3 bg-zinc-900 ${className}`}>
             {children}
         </div>
     )
@@ -55,10 +40,7 @@ export function CardBody({ children, className = '' }: { children: ReactNode, cl
 
 export function CardFooter({ children, className = '' }: { children: ReactNode, className?: string }) {
     return (
-        <div className={`
-            p-3 rounded-b-lg
-            ${className}
-        `}>
+        <div className={`p-3 bg-zinc-900 rounded-b-lg ${className}`}>
             {children}
         </div>
     )
