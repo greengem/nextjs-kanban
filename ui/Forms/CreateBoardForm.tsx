dc4cc6043644c5c84905c6828236b1d95b27c938
Green Gem <chris_waitt@me.com>
2023-12-05 11:43:12 +0000

    Updated create board form with new RHF features, we can now fire off a toast message, catch errors and redirect to the board page with client side features

--- ui/Forms/CreateBoardForm.tsx
+++ ui/Forms/CreateBoardForm.tsx
@@ -1,67 +1,69 @@
 'use client'
+import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { handleCreateBoard } from "@/actions/BoardActions";
+import { Card, CardHeader, CardBody, CardFooter } from '@/ui/Card/Card';
+import { revalidatePath } from 'next/cache';
+import toast from 'react-hot-toast';
 
-// Define your Zod schema for form validation
 const CreateBoardSchema = z.object({
   boardTitle: z.string().min(3, "Title must be at least 3 characters"),
   boardDescription: z.string().optional(),
 });
 
 export default function CreateBoardForm() {
-  // Initialize react-hook-form with Zod resolver for validation
+  const router = useRouter()
   const {
     register,
     handleSubmit,
-    reset,
     formState: { isSubmitting },
   } = useForm({
     resolver: zodResolver(CreateBoardSchema),
   });
 
-  // Handle form submission
   const onSubmit = async (data: any) => {
-    await handleCreateBoard(null, data);
-    reset(); // Reset form fields after submission
+    const response = await handleCreateBoard(null, data);
+  
+    if (response.success && response.boardId) {
+      router.push(`/board/${response.boardId}`);
+      toast.success('Board Created!');
+    } else {
+      console.error(response.message);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* Board Title Input */}
-      <div className="mb-3">
-        <label htmlFor="boardTitle" className="block mb-2 text-sm font-medium">Board Title</label>
-        <input 
-          type="text" 
-          id="boardTitle" 
-          {...register('boardTitle')} // Register with react-hook-form
-          className="w-full p-2 border rounded text-black" 
-          autoFocus 
-        />
-      </div>
+    <Card className="w-64 shrink-0">
+      <CardBody className="
+        text-center 
+        bg-gradient-to-br from-purple-600 to-purple-800
+        h-28
+      ">
+        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-between h-full'>
 
-      {/* Board Description Input */}
-      <div className="mb-3 hidden">
-        <label htmlFor="boardDescription" className="block mb-2 text-sm font-medium">Board Description</label>
-        <input 
-          type="text" 
-          id="boardDescription" 
-          {...register('boardDescription')} // Register with react-hook-form
-          className="w-full p-2 border rounded text-black" 
-        />
-      </div>
+          <div>
+            <label htmlFor="boardTitle" className="block mb-2 text-sm font-medium sr-only">Board Title</label>
+            <input 
+              type="text" 
+              id="boardTitle" 
+              {...register('boardTitle')}
+              className="w-full p-2 border rounded text-zinc-900 focus:outline-none text-sm" 
+              autoFocus 
+            />
+          </div>
 
-      {/* Submit Button */}
-      <button 
-        type="submit" 
-        disabled={isSubmitting}
-        className="px-4 py-2 bg-purple-500 text-white rounded-md" 
-      >
-        {isSubmitting ? 'Creating...' : 'Create New Board'}
-      </button>
+          <button 
+            type="submit" 
+            disabled={isSubmitting}
+            className="px-4 py-2 bg-purple-500 text-white rounded-md text-sm w-full" 
+          >
+            {isSubmitting ? 'Creating...' : 'Create New Board'}
+          </button>
 
-      {/* Optional: Display submission status, errors, etc. */}
-    </form>
+        </form>
+      </CardBody>
+    </Card>
   );
 }
