d7f5934c03e32e767c3e3249fa2254d476216c98
Green Gem <chris_waitt@me.com>
2023-12-05 19:21:40 +0000

    Create Task form updated to use React Hook Form

--- ui/Forms/CreateTaskFormSimple.tsx
+++ ui/Forms/CreateTaskFormSimple.tsx
@@ -1,86 +1,80 @@
 'use client'
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast from 'react-hot-toast';
 import { handleCreateTask } from "@/actions/TaskActions";
-import { useFormState, useFormStatus } from "react-dom";
-import { useState, useEffect } from "react";
+import { CreateTaskSchema } from '@/types/zodTypes';
+import { TaskCreationData } from '@/types/types';
 import { IconPlus, IconX } from "@tabler/icons-react";
-import toast from "react-hot-toast";
-
-const initialState = {
-  message: '',
-}
-
-function SubmitButton() {
-  const { pending } = useFormStatus()
-
-  return (
-    <button 
-      type="submit" 
-      aria-disabled={pending}
-      className="px-3 py-1 bg-purple-500 text-white rounded-md text-sm"
-    >
-      Add Card
-    </button>
-  )
-}
 
 function CloseButton({ onClick }: { onClick: () => void }) {
   return (
-    <button 
-      type="button"
-      aria-label="Close form"
-      onClick={onClick}
-    >
+    <button type="button" aria-label="Close form" onClick={onClick}>
       <IconX size={20} />
     </button>
   );
 }
 
-export default function CreateTaskForm({ boardId, columnId }: { boardId: string; columnId: string;  }) {
-  const [state, formAction] = useFormState(handleCreateTask, initialState);
+export default function CreateTaskForm({ 
+  boardId, columnId 
+} : { 
+  boardId: string; columnId: string; 
+}) {
   const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => setIsEditing(!isEditing);
 
-  useEffect(() => {
-    if (state?.message) {
+  const { register, handleSubmit, reset, formState: { isSubmitting } } = useForm<TaskCreationData>({
+    resolver: zodResolver(CreateTaskSchema),
+    defaultValues: { boardId, columnId }
+  });
+
+  const onSubmit: SubmitHandler<TaskCreationData> = async (data) => {
+    const response = await handleCreateTask(data);
+
+    if (response.success) {
+      toast.success('Task Created');
+      reset();
       setIsEditing(false);
-      toast.success(state.message);
+    } else {
+      toast.error(response.message);
     }
-  }, [state?.message]);
-
-  const toggleEdit = () => {
-    setIsEditing(!isEditing);
   };
 
   return (
     <div>
       {isEditing ? (
-
-        <form action={formAction}>
-
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-2">
             <label htmlFor="taskTitle" className="block mb-2 font-medium sr-only">Task Title</label>
-            <input autoFocus type="text" id="taskTitle" name="taskTitle" className="w-full px-2 py-2 border rounded text-sm text-black" required />
+            <input 
+              autoFocus 
+              type="text" 
+              id="taskTitle" 
+              {...register('title')}
+              className="w-full px-2 py-2 border rounded text-sm text-black" 
+              required 
+            />
           </div>
 
-          <input type="hidden" name="boardId" value={boardId} />
-          <input type="hidden" name="columnId" value={columnId} />
+          <input type="hidden" {...register('boardId')} />
+          <input type="hidden" {...register('columnId')} />
 
           <div className="flex justify-between items-center">
-            <SubmitButton />
+            <button 
+              type="submit" 
+              disabled={isSubmitting}
+              className="px-3 py-1 bg-purple-500 text-white rounded-md text-sm"
+            >
+              Add Card
+            </button>
             <CloseButton onClick={toggleEdit} />
           </div>
-
-          <p aria-live="polite" className="sr-only" role="status">
-            {state?.message}
-          </p>
-
         </form>
-
       ) : (
-
         <button onClick={toggleEdit} className="text-sm flex items-center gap-2 w-full">
           <IconPlus size={16} />Add a card
         </button>
-
       )}
     </div>
   )
