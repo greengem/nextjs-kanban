0fbbf7950a827a9fad29388e9e01f16d9ddd282f
Green Gem <chris_waitt@me.com>
2023-12-05 21:00:07 +0000

    redesigned board listing, adding an icon with the total number of tasks for each board

--- app/(board-layout)/board/page.tsx
+++ app/(board-layout)/board/page.tsx
@@ -1,11 +1,10 @@
 import { getBoardsSummary } from "@/lib/FetchData";
 import { BoardSummary } from "@/types/types";
 import CreateBoardForm from "@/ui/Forms/CreateBoardForm";
-import DeleteBoardForm from "@/ui/Forms/DeleteBoardForm";
 import Link from "next/link";
 import { Card, CardHeader, CardBody, CardFooter } from '@/ui/Card/Card';
 import PageHeading from "@/ui/PageHeading";
-
+import { IconList  } from "@tabler/icons-react";
 export const dynamic = 'auto'
 
 export default async function Boards() {
@@ -15,22 +14,21 @@ export default async function Boards() {
     <>
       <PageHeading title='Boards' />
       <div className="flex gap-5">
+        <CreateBoardForm />
         {boards.map((board) => (
           <Link key={board.id} href={`/board/${board.id}`}>
             <Card className="w-64 shrink-0">
               <CardBody className="
-                h-28
-                text-center 
-                bg-gradient-to-br from-purple-600 to-purple-800
-                hover:from-purple-500 hover:to-purple-700
-                flex flex-col justify-center
+                h-28 
+                flex flex-col justify-end relative
               ">
+                <span className="absolute text-xs top-2 right-2 bg-zinc-800 w-10 h-6 rounded-lg flex items-center justify-center text-purple-500 gap-1"><IconList size={16} />3</span>
                 {board.title}
               </CardBody>
             </Card>
           </Link>
         ))}
-        <CreateBoardForm />
+        
       </div>
     </>
   );
