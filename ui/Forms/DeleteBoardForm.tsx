7c49e29fd8f1c7bd26956c90270015a2289e82d6
Green Gem <chris_waitt@me.com>
2023-12-05 19:21:49 +0000

    Delete board form updated to use React Hook Form

--- ui/Forms/DeleteBoardForm.tsx
+++ ui/Forms/DeleteBoardForm.tsx
@@ -1,44 +1,48 @@
 'use client'
-
-import { useFormState, useFormStatus } from "react-dom";
+import { useRouter } from 'next/navigation'
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast from 'react-hot-toast';
 import { handleDeleteBoard } from "@/actions/BoardActions";
+import { DeleteBoardSchema } from '@/types/zodTypes';
+import { BoardDeletionData } from '@/types/types';
 import { IconTrash } from "@tabler/icons-react";
 
-function DeleteButton() {
-  const { pending } = useFormStatus()
+export default function DeleteBoardForm({ 
+  boardId 
+} : { 
+  boardId: string; 
+}) {
+  const router = useRouter()
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<BoardDeletionData>({
+    resolver: zodResolver(DeleteBoardSchema),
+    defaultValues: { id: boardId }
+  });
+
+  const onSubmit: SubmitHandler<BoardDeletionData> = async (data) => {
+    if (window.confirm('Are you sure you want to delete this board?')) {
+      const response = await handleDeleteBoard(data);
+
+      if (response.success) {
+        router.push('/board/');
+        toast.success('Board Deleted');
+      } else {
+        toast.error(response.message);
+      }
+    }
+  };
 
   return (
-    <button 
-      type="submit" 
-      aria-disabled={pending}
-      className="p-1 bg-red-500 text-white rounded"
-    >
-      <IconTrash size={16} />
-    </button>
-  )
-}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="hidden" {...register('id')} />
 
-export default function DeleteBoardForm({ boardTitle, boardId }: { boardTitle: string; boardId: string; }) {
-  const [state, formAction] = useFormState(handleDeleteBoard, null)
-
-  return (
-    <form 
-      action={formAction}
-      onSubmit={(e) => {
-        e.preventDefault();
-        const confimed = confirm("Are you sure you want to delete this board?");
-        if (confimed) {
-          formAction(new FormData(e.currentTarget));
-        }
-      }}
-    >
-      <input type="hidden" name="boardId" value={boardId} />
-      <input type="hidden" name="boardTitle" value={boardTitle} />
-      <DeleteButton />
-      <p aria-live="polite" className="sr-only" role="status">
-        {state?.message}
-      </p>
+      <button 
+        type="submit" 
+        disabled={isSubmitting}
+        className="p-1 bg-red-500 text-white rounded"
+      >
+        {isSubmitting ? 'Deleting...' : <IconTrash size={16} />}
+      </button>
     </form>
   )
 }
-
