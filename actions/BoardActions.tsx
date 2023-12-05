4218059c7075bd5f79eaa6a749659f56854909b0
Green Gem <chris_waitt@me.com>
2023-12-05 19:19:19 +0000

    Updated board actions to use React Hook Form

--- actions/BoardActions.tsx
+++ actions/BoardActions.tsx
@@ -1,25 +1,17 @@
 'use server';
-import { z } from 'zod';
+import { auth } from "@/auth";
 import prisma from '@/db/prisma';
 import { revalidatePath } from 'next/cache';
-import { auth } from "@/auth";
+import { CreateBoardSchema, DeleteBoardSchema } from '@/types/zodTypes';
+import { BoardCreationData, BoardDeletionData } from "@/types/types";
 
-interface BoardData {
-  boardTitle: string;
-  boardDescription?: string;
-}
-
-export async function handleCreateBoard(prevState: any, data: BoardData) {
+// Create Board
+export async function handleCreateBoard(data: BoardCreationData) {
   const session = await auth();
 
   if (!session?.user?.id) {
     return { success: false, message: 'User is not authenticated' };
   }
-
-  const CreateBoardSchema = z.object({
-    boardTitle: z.string().min(1, "Title is required"),
-    boardDescription: z.string().optional(),
-  });
 
   const parse = CreateBoardSchema.safeParse(data);
 
@@ -31,8 +23,8 @@ export async function handleCreateBoard(prevState: any, data: BoardData) {
     const createdBoard = await prisma.board.create({
       data: {
         userId: session?.user?.id,
-        title: parse.data.boardTitle,
-        description: parse.data.boardDescription,
+        title: parse.data.title,
+        description: parse.data.description,
       }
     });
 
@@ -44,36 +36,26 @@ export async function handleCreateBoard(prevState: any, data: BoardData) {
   }
 }
 
+// Delete Board
+export async function handleDeleteBoard(data: BoardDeletionData) {
 
-export async function handleDeleteBoard(prevState: any, formData: FormData) {
-
-    const DeleteBoardSchema = z.object({
-        boardId: z.string().min(1),
-        boardTitle: z.string().min(1),
-    });
-
-    const parse = DeleteBoardSchema.safeParse({
-        boardId: formData.get('boardId') as string,
-        boardTitle: formData.get('boardTitle') as string,
-    })
+    const parse = DeleteBoardSchema.safeParse(data);
 
     if (!parse.success) {
-        return { success: false, message: 'Failed to delete task' };
+      return { success: false, message: 'Failed to delete board due to validation error' };
     }
-
-    const data = parse.data;
 
     try {
 
-        const column = await prisma.board.delete({
+        await prisma.board.delete({
             where: {
-                id: data.boardId,
+                id: parse.data.id,
             }
         });
 
         revalidatePath(`/board/`);
 
-        return { success: true, message: `Deleted board ${data.boardTitle}` }
+        return { success: true, message: `Deleted board` }
     } catch (e) {
         return { success: false, message: 'Failed to delete board' }
     }
