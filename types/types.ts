b533b1b15935f3694854190bc80b0dc84bf79965
Green Gem <chris_waitt@me.com>
2023-12-05 19:19:59 +0000

    New Types and zodTypes for CRUD

--- types/types.ts
+++ types/types.ts
@@ -11,3 +11,42 @@ export type TaskSummary = Pick<Task, 'id' | 'order' | 'title' | 'description' | 
 export type BoardDetails = BoardSummary & {
     columns: ColumnWithTasks[];
 };
+
+
+// Form validation
+
+export type BoardCreationData = Pick<Board, 'title' | 'description'>;
+
+export type BoardDeletionData = Pick<Board, 'id'>;
+
+export type ColumnCreationData = {
+    boardId: Board['id'];
+    title: Column['title'];
+};
+
+export type ColumnDeletionData = {
+    boardId: Board['id'];
+    id: Column['id'];
+};
+
+export type TaskCreationData = {
+    title: Task['title'];
+    description?: Task['description'];
+    priority: Task['priority'];
+    boardId: Board['id'];
+    columnId: Column['id'];
+};
+
+export type TaskEditData = {
+    id: Task['id'];
+    title: Task['title'];
+    description?: Task['description'];
+    priority: Task['priority'];
+    boardId: Board['id'];
+};
+
+export type TaskDeletionData = {
+    id: Task['id'];
+    boardId: Board['id'];
+    columnId: Column['id'];
+};
---
+++ types/zodTypes.ts
@@ -0,0 +1,47 @@
+import { z } from 'zod';
+
+// Boards
+export const CreateBoardSchema = z.object({
+    title: z.string().min(3, "Title must be at least 3 characters"),
+    description: z.string().optional(),
+});
+
+export const DeleteBoardSchema = z.object({
+    id: z.string().min(1, "ID is required"),
+});
+
+
+// Columns
+export const CreateColumnSchema = z.object({
+    boardId: z.string().min(1, "ID is required"),
+    title: z.string().min(3),
+});
+
+export const DeleteColumnSchema = z.object({
+    id: z.string().min(1, "ID is required"),
+    boardId: z.string().min(1, "ID is required"),
+});
+
+
+// Tasks
+export const CreateTaskSchema = z.object({
+    title: z.string().min(1), 
+    description: z.union([z.string(), z.null()]).optional(),
+    priority: z.union([z.string(), z.null()]).optional(),
+    columnId: z.string().min(1),
+    boardId: z.string().min(1),
+});
+
+export const DeleteTaskSchema = z.object({
+    id: z.string().min(1),
+    columnId: z.string().min(1),
+    boardId: z.string().min(1),
+});
+
+export const EditTaskSchema = z.object({
+    id: z.string().min(1),
+    title: z.string().min(1), 
+    description: z.union([z.string(), z.null()]).optional(),
+    priority: z.union([z.string(), z.null()]).optional(),
+    boardId: z.string().min(1),
+});
