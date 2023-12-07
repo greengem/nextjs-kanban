0e4ed8a7480b90deb8339f50b83f60e75e778438
Green Gem <chris_waitt@me.com>
2023-12-07 15:08:21 +0000

    Updated splash page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
+import { Button } from "@nextui-org/react";
+import { IconLayoutKanban } from "@tabler/icons-react";
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex justify-center items-center bg-zinc-900">
       <div className="flex-none text-center">
-        <h1 className="text-7xl tracking-tighter font-semibold mb-2">Kanban</h1>
-        <h1 className="text-7xl tracking-tighter font-semibold mb-20 text-purple-500">Reimagined</h1>
-        <Link 
-          className="bg-purple-500 rounded-xl text-white text-lg px-8 py-3 "
-          href='/board'
-        >
-            Get Started</Link>
+        <h1 className="text-7xl tracking-tighter font-semibold mb-1 flex items-center gap-2">
+          <IconLayoutKanban size={80} className="text-purple-500" />Next.Kanban
+        </h1>
+        <p className="mb-5">Unleash Your Team's Productivity</p>
+        <Button variant="ghost" color="secondary" size="lg" as={Link} href="/board">
+          Get Started
+        </Button>
       </div>
     </div>
   );
