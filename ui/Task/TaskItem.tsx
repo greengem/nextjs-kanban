c59303f8f2a26486af5dd6559a1e2f041c007f89
Green Gem <chris_waitt@me.com>
2023-12-07 14:07:47 +0000

    Massive refactor of task component, moved all logic other than name and the drag handle to a modal that witll provide detailed info about the task and allow for editing

--- ui/Task/TaskItem.tsx
+++ ui/Task/TaskItem.tsx
@@ -3,40 +3,27 @@ import EditTaskForm from '../Forms/EditTaskForm';
 import { IconGripVertical } from '@tabler/icons-react';
 import { TaskSummary } from '@/types/types';
 import { IconInfoCircle } from '@tabler/icons-react';
+import { Button } from '@nextui-org/button';
 
 interface TaskItemProps {
   task: TaskSummary;
-  boardId: string;
-  columnId: string;
+  onTaskClick: (task: TaskSummary) => void;
+  dragHandleProps: any;
 }
 
-export default function TaskItem({ task, boardId, columnId }: TaskItemProps) {
-
+export default function TaskItem({ task, onTaskClick, dragHandleProps }: TaskItemProps) {
   return (
-    <div className={`flex select-none rounded-lg bg-zinc-800 shadow-md`}>
-      
-      {/* Drag Handle */}
-      <div className='px-0 flex items-center cursor-grab touch-none'>
-        <IconGripVertical className='text-purple-500' />
+    <div className='flex select-none rounded-lg bg-zinc-800 shadow-md ring-0 hover:ring-2 hover:ring-purple-500'>
+
+
+      <div className='pl-1 flex items-center cursor-grab touch-none' {...dragHandleProps}>
+        <IconGripVertical className='text-purple-500' size={22} />
       </div>
 
-      {/* Task Contents */}
-      <div className='flex-grow px-3 py-2'>
-
-        {/* Task title and edit toggle */}
-        <div className='mb-1 text-sm'>
-          <EditTaskForm title={task.title} boardId={boardId} taskId={task.id} />
+      <div className='flex-grow px-3 py-2' onClick={() => onTaskClick(task)}>
+        <div className='text-sm cursor-pointer'>
+            {task.title}
         </div>
-
-        {/* Task Controls */}
-        <div className='flex justify-between items-center'>
-          <div className='text-xs font-semibold text-purple-300'>{task.priority && `${task.priority}`}</div>
-          <div className='flex gap-1'>
-            <button className='bg-purple-500 p-1 rounded-md text-white'><IconInfoCircle size={14} /></button>
-            <DeleteTaskForm taskId={task.id} boardId={boardId} columnId={columnId} />
-          </div>
-        </div>
-        
       </div>
 
     </div>
