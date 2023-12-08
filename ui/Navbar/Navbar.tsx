c9090e1d5a51763d9000465511fcfbb7572431fc
Green Gem <chris_waitt@me.com>
2023-12-08 11:43:59 +0000

    Add theme selector to navbar for now

--- ui/Navbar/Navbar.tsx
+++ ui/Navbar/Navbar.tsx
@@ -3,20 +3,23 @@ import NavbarTitle from "./NavbarTitle";
 import Image from "next/image";
 import { SignOutButton } from "../AuthButtons";
 import { Avatar } from "@nextui-org/avatar";
+import ThemePicker from "./ColourPicker";
+import ThemeNavbar from "./ThemeNavbar";
 
 export default async function Navbar () {
     const session = await auth();
     const defaultImageUrl = "https://loremflickr.com/36/36";
 
     return (
-        <nav className="flex px-5 py-2 bg-zinc-900 items-center justify-between">
+        <>
+        <nav className="flex gap-2 px-5 py-2 bg-zinc-900 items-center justify-between opacity-80">
             <NavbarTitle />
-            <div className="flex gap-5 items-center">
-                <div className="flex gap-2">
-                    <SignOutButton />
-                </div>
-                <Avatar isBordered color="secondary" src={session?.user?.image || defaultImageUrl} size="sm" />
+            <ThemeNavbar />
+            <div className="w-8 flex justify-end shrink-0 grow-0">
+                <Avatar isBordered color="primary" src={session?.user?.image || defaultImageUrl} size="sm" />
             </div>
         </nav>
+        
+        </>
     )
 }
---
+++ ui/Navbar/ThemeNavbar.tsx
@@ -0,0 +1,9 @@
+import ThemePicker from "./ColourPicker";
+
+export default function ThemeNavbar() {
+    return (
+        <div className="overflow-x-scroll no-scrollbar">
+            <ThemePicker />
+        </div>
+    )
+}
