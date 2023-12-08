909f403db9c303b63c5aa5809ad17315093d2204
Green Gem <chris_waitt@me.com>
2023-12-08 11:43:21 +0000

    remove forced dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='dark'>
-      <body className={`${inter.className} bg-zinc-950 text-white`}>
+    <html lang="en">
+      <body className={`${inter.className} bg-gradient-to-r from-zinc-700 to-primary text-white`}>
         <Toaster />
         <Providers>
           {children}
