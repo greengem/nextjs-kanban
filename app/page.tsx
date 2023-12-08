53b6706da5424d55199f8bd43c2e33e660f9c944
Green Gem <chris_waitt@me.com>
2023-12-08 11:44:09 +0000

    Tidied up splash page

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ export default function Home() {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-zinc-900">
       <div className="flex-none text-center">
-        <h1 className="text-7xl tracking-tighter font-semibold mb-1 flex items-center gap-2">
-          <IconLayoutKanban size={80} className="text-purple-500" />Next.Kanban
+        <h1 className="text-7xl tracking-tighter font-semibold flex items-center">
+          <IconLayoutKanban size={80} className="text-primary" /><span className="text-primary">N</span>ext
         </h1>
-        <p className="mb-5">Unleash Your Team's Productivity</p>
-        <Button variant="ghost" color="secondary" size="lg" as={Link} href="/board">
-          Get Started
+        <h1 className="text-7xl tracking-tighter font-semibold"><span className="text-primary">K</span>anban</h1>
+        <p className="mb-5 text-zinc-500 tracking-tight">Unleash Your Team's Productivity</p>
+        <Button variant="ghost" color="primary" size="lg" as={Link} href="/board" isIconOnly>
+          🚀
         </Button>
       </div>
     </div>
