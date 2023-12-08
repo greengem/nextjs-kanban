553f678a1a7d4676fe30e15e8f3bd595de584e5d
Green Gem <chris_waitt@me.com>
2023-12-08 19:11:54 +0000

    Refactored column actions

--- ui/Board/ColumnActions.tsx
+++ ui/Board/ColumnActions.tsx
@@ -14,74 +14,74 @@ export default function ColumnActions({
     columnId: string; boardId: string; columnTitle: string;
 }) {
 	const [isEditing, setIsEditing] = useState(false);
-    const [editedTitle, setEditedTitle] = useState(columnTitle);
+  const [editedTitle, setEditedTitle] = useState(columnTitle);
 
-    const handleAction = async (action: 'edit' | 'delete') => {
-        if (action === 'delete' && window.confirm('Are you sure you want to delete this column?')) {
-          const response = await handleDeleteColumn({ id: columnId, boardId });
-          if (response.success) {
-            toast.success('Column Deleted');
-          } else {
-            toast.error(response.message);
-          }
-        } else if (action === 'edit') {
-			setIsEditing(true);
-        }
+  const handleAction = async (action: 'edit' | 'delete') => {
+    if (action === 'delete' && window.confirm('Are you sure you want to delete this column?')) {
+      const response = await handleDeleteColumn({ id: columnId, boardId });
+      if (response.success) {
+        toast.success('Column Deleted');
+      } else {
+        toast.error(response.message);
+      }
+    } else if (action === 'edit') {
+      setIsEditing(true);
+    }
+  };
+
+  const handleSave = async () => {
+    const editData = {
+      columnId: columnId,
+      title: editedTitle,
+      boardId: boardId
     };
 
-	const handleSave = async () => {
-		const editData = {
-		  columnId: columnId,
-		  title: editedTitle,
-		  boardId: boardId
-		};
-	  
-		const response = await handleEditColumn(editData);
-	  
-		if (response.success) {
-		  toast.success('Column Updated');
-		} else {
-		  toast.error(response.message);
-		}
-	  
-		setIsEditing(false);
-	  };
-	  
+    const response = await handleEditColumn(editData);
+    
+    if (response.success) {
+      toast.success('Column Updated');
+    } else {
+      toast.error(response.message);
+    }
+    
+    setIsEditing(false);
+  };
+  
 
-    const handleCancel = () => {
-        setEditedTitle(columnTitle);
-        setIsEditing(false);
-    };
+  const handleCancel = () => {
+    setEditedTitle(columnTitle);
+    setIsEditing(false);
+  };
 
-    return(
-		<>
-            {isEditing ? (
-                <div className="flex-col w-full">
-                    <Input 
-                        labelPlacement="outside"
-                        value={editedTitle}
-                        onChange={(e) => setEditedTitle(e.target.value)}
-                        className="pl-0 mb-2"
-                    />
-                    <div className="flex gap-2">
-                        <Button size="sm" color="primary" onClick={handleSave}>Save</Button>
-                        <Button size="sm" onClick={handleCancel}>Cancel</Button>
-                    </div>
-                </div>
-            ) : (
-                <>
-                    <h3>{columnTitle}</h3>
-                    <Dropdown>
-                        <DropdownTrigger>
-                            <Button variant="flat" isIconOnly size='sm'><IconMenu2 size={20} /></Button>
-                        </DropdownTrigger>
-                        <DropdownMenu aria-label="Column Actions" onAction={(key) => handleAction(key as 'edit' | 'delete')}>
-                            <DropdownItem key="edit">Edit Name</DropdownItem>
-                            <DropdownItem key="delete" className="text-danger" color="danger">Delete Column</DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
-                </>
-            )}
-		</>
-    )
+  return(
+  <>
+          {isEditing ? (
+              <div className="flex-col w-full">
+                  <Input 
+                      labelPlacement="outside"
+                      value={editedTitle}
+                      onChange={(e) => setEditedTitle(e.target.value)}
+                      className="pl-0 mb-2"
+                  />
+                  <div className="flex gap-2">
+                      <Button size="sm" color="primary" onClick={handleSave}>Save</Button>
+                      <Button size="sm" onClick={handleCancel}>Cancel</Button>
+                  </div>
+              </div>
+          ) : (
+              <>
+                  <h3>{columnTitle}</h3>
+                  <Dropdown>
+                      <DropdownTrigger>
+                          <Button variant="flat" isIconOnly size='sm'><IconMenu2 size={20} /></Button>
+                      </DropdownTrigger>
+                      <DropdownMenu aria-label="Column Actions" onAction={(key) => handleAction(key as 'edit' | 'delete')}>
+                          <DropdownItem key="edit">Edit Name</DropdownItem>
+                          <DropdownItem key="delete" className="text-danger" color="danger">Delete Column</DropdownItem>
+                      </DropdownMenu>
+                  </Dropdown>
+              </>
+          )}
+  </>
+  )
 }
