4cb988c9dfc9fac72ea7645b39a2d3192aecdc23
Green Gem <chris_waitt@me.com>
2023-12-08 11:42:50 +0000

    Define theme names and colours

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,37 @@
 'use client'
 import { SidebarProvider } from '@/contexts/SidebarContext'
 import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <SidebarProvider>
-        {children}
-      </SidebarProvider>
+            <NextThemesProvider 
+              attribute="class" 
+              defaultTheme="purple"
+              themes={[
+                'red',
+                'orange',
+                'yellow',
+                'lime',
+                'green',
+                'emerald',
+                'teal',
+                'cyan',
+                'sky',
+                'blue',
+                'indigo',
+                'violet',
+                'purple',
+                'fuchsia',
+                'pink',
+                'rose'
+              ]}
+            >
+        <SidebarProvider>
+          {children}
+        </SidebarProvider>
+      </NextThemesProvider>
     </NextUIProvider>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,26 @@ const config: Config = {
   ],
   theme: {},
   darkMode: "class",
-  plugins: [nextui()]
+  plugins: [nextui({
+    themes: {
+      "red": { extend: "dark", colors: { primary: { DEFAULT: "#ef4444"} } },
+      "orange": { extend: "dark", colors: { primary: { DEFAULT: "#f97316"} } },
+      "amber": { extend: "dark", colors: { primary: { DEFAULT: "#f59e0b"} } },
+      "yellow": { extend: "dark", colors: { primary: { DEFAULT: "#eab308"} } },
+      "lime": { extend: "dark", colors: { primary: { DEFAULT: "#84cc16"} } },
+      "green": { extend: "dark", colors: { primary: { DEFAULT: "#22c55e"} } },
+      "emerald": { extend: "dark", colors: { primary: { DEFAULT: "#10b981"} } },
+      "teal": { extend: "dark", colors: { primary: { DEFAULT: "#14b8a6"} } },
+      "cyan": { extend: "dark", colors: { primary: { DEFAULT: "#06b6d4"} } },
+      "sky": { extend: "dark", colors: { primary: { DEFAULT: "#0ea5e9"} } },
+      "blue": { extend: "dark", colors: { primary: { DEFAULT: "#3b82f6"} } },
+      "indigo": { extend: "dark", colors: { primary: { DEFAULT: "#6366f1"} } },
+      "violet": { extend: "dark", colors: { primary: { DEFAULT: "#8b5cf6"} } },
+      "purple": { extend: "dark", colors: { primary: { DEFAULT: "#a855f7"} } },
+      "fuchsia": { extend: "dark", colors: { primary: { DEFAULT: "#d946ef"} } },
+      "pink": { extend: "dark", colors: { primary: { DEFAULT: "#ec4899"} } },
+      "rose": { extend: "dark", colors: { primary: { DEFAULT: "#f43f5e"} } },
+    }
+  })]
 }
 export default config
