b2da4201a67ff9866ce1bd1efb84154ef26f2e19
Green Gem <chris_waitt@me.com>
2023-12-10 11:18:34 +0000

    Added a component for all thje modal actions

---
+++ ui/TaskModal/TaskModalActions.tsx
@@ -0,0 +1,26 @@
+import DeleteTaskForm from '@/ui/Forms/DeleteTaskForm';
+import { IconArrowRight, IconTrash } from '@tabler/icons-react';
+
+export default function TaskModalActions({
+    selectedTask, boardId, onClose,
+} : {
+    selectedTask: any, boardId: string, onClose: any;
+}) {
+    return (
+        <>
+              <h4 className='text-sm text-zinc-500'>Actions</h4>
+              <ul className='text-sm space-y-2'>
+                <li className='flex items-center gap-2 bg-zinc-800 px-2 py-1 rounded-md'><IconArrowRight size={14} /> Move</li>
+                <li className='bg-zinc-800 px-2 py-1 rounded-md'>
+                  <DeleteTaskForm 
+                    taskId={selectedTask.id} 
+                    boardId={boardId} 
+                    columnId={selectedTask.columnId}
+                    onCloseModal={onClose}
+                  />
+                </li>
+                <li className='flex items-center gap-2 bg-zinc-800 px-2 py-1 rounded-md'><IconTrash size={14} /> Make Template</li>
+              </ul>
+        </>
+    )
+}
