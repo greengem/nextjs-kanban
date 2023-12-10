3a6dbf42029a2de828093ffe3c6ff12fc9c41461
Green Gem <chris_waitt@me.com>
2023-12-10 11:19:55 +0000

    Refactored task title modal component

---
+++ ui/TaskModal/TaskModalActivity.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { IconActivity } from "@tabler/icons-react"
+import TaskModalActivityItem from "./TaskModalActivityItem"
+import { Avatar } from "@nextui-org/avatar"
+import { Textarea } from "@nextui-org/input"
+import { Button } from "@nextui-org/button"
+export default function TaskModalActivity() {
+
+    return (
+        <div className='flex gap-2 w-full'>
+            <IconActivity size={20} className='mt-1 w-5' />
+            <div className='flex-col w-full'>
+                <h4 className='text-large font-semibold mb-2'>Activity</h4>
+                <div className="flex gap-3 mb-3">
+                    <Avatar 
+                        showFallback 
+                        name='Chris Waitt' 
+                        src='https://images.unsplash.com/broken' 
+                    />
+                    <div className="w-full" >
+                        <Textarea
+                            size="sm"
+                            className="mb-2"
+                        />
+                        <div className="flex gap-2">
+                            <Button size="sm" color="primary">Save</Button>
+                            <Button size="sm">Cancel</Button>
+                        </div>
+                    </div>
+                </div>
+                <ul>
+                    <TaskModalActivityItem />
+                </ul>
+            </div>
+        </div>
+    )
+}
---
+++ ui/TaskModal/TaskModalActivityItem.tsx
@@ -0,0 +1,17 @@
+'use client'
+import {User} from "@nextui-org/user";
+
+export default function TaskModalActivityItem() {
+
+    return (
+        <li>
+            <User   
+                name="Chris Waitt"
+                description="Added this card to in Progress"
+                avatarProps={{
+                src: ""
+                }}
+            />
+        </li>
+    )
+}
