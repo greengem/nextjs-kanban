cd305e592790d4e4adb274c0bfdaf91765a9c28a
Green Gem <chris_waitt@me.com>
2023-12-11 16:51:18 +0000

    Initial attempt to fetch task data in the modal with server action, data is erturned but not updated when we perfrom crud in the modal

--- ui/TaskModal/TaskModal.tsx
+++ ui/TaskModal/TaskModal.tsx
@@ -1,53 +1,92 @@
 'use client'
+import { useState, useEffect } from 'react';
 import { Modal, ModalContent, ModalBody, ModalFooter, ModalHeader } from '@nextui-org/modal';
+import { Session } from "next-auth";
 import { Button } from '@nextui-org/button';
-import { TaskSummary } from '@/types/types';
-import { format } from 'date-fns';
-import { IconArrowRight, IconCalendar, IconCheckbox, IconPaint, IconPaperclip, IconTag, IconTextPlus, IconTrash, IconUser } from '@tabler/icons-react';
-
-import DeleteTaskForm from '../Forms/DeleteTaskForm';
+import { ExpandedTask } from '@/types/types';
 import TaskModalTitle from './TaskModalTitle';
 import TaskModalDescription from './TaskModalDescription';
 import TaskModalAddToCard from './TaskModalAddToCard';
 import TaskModalActions from './TaskModalActions';
 import TaskModalActivity from './TaskModalActivity';
+import { handleFetchTask } from '@/actions/FetchModalTask';
+import toast from 'react-hot-toast';
 
 interface TaskDetailModalProps {
   isOpen: boolean;
   onClose: () => void;
-  selectedTask: TaskSummary | null;
+  taskId: string;
   boardId: string;
+  session: Session | null;
 }
 
 
 export default function TaskModal({
   isOpen,
   onClose,
-  selectedTask,
+  taskId,
   boardId,
+  session,
 }: TaskDetailModalProps) {
 
+  const [taskData, setTaskData] = useState<ExpandedTask | null>(null);
+
+  const fetchTaskData = async () => {
+    try {
+      const response = await handleFetchTask(taskId);
+      if (response.success) {
+        // Check if response.task is defined before setting it
+        if (response.task) {
+          setTaskData(response.task);
+        } else {
+          // Handle the case where task is undefined
+          console.error('Task data is undefined');
+          toast.error('Failed to load task details');
+        }
+      } else {
+        // Check if response.message is defined before using it
+        toast.error(response.message || 'An error occurred');
+      }
+    } catch (error) {
+      console.error('Error fetching task:', error);
+      toast.error('Failed to load task details');
+    }
+  };
+  
+
+  // Fetch task data when the modal opens or taskId changes
+  useEffect(() => {
+    if (isOpen && taskId) {
+      fetchTaskData();
+    }
+  }, [isOpen, taskId]);
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} size='3xl' scrollBehavior='outside'>
       <ModalContent>
-      {selectedTask ? (
+      {taskData ? (
         <> 
         <ModalHeader className='flex gap-2 bg-primary'>
-          <TaskModalTitle selectedTask={selectedTask} boardId={boardId} />
+          <TaskModalTitle selectedTask={taskData} boardId={boardId} />
         </ModalHeader>
 
         <ModalBody>
           <div className='grid grid-cols-1 md:grid-cols-4 gap-5'>
 
             <div className='col-span-3 space-y-5'>
-              <TaskModalDescription selectedTask={selectedTask} boardId={boardId} />
-              <TaskModalActivity />
+              <TaskModalDescription selectedTask={taskData} boardId={boardId} />
+              <TaskModalActivity 
+                activities={taskData.activities} 
+                taskId={taskData.id}
+                boardId={boardId}
+                columnTitle={taskData.columnTitle} 
+                session={session} 
+              />
             </div>
 
             <div className='col-span-1 space-y-2'>
               <TaskModalAddToCard />
-              <TaskModalActions selectedTask={selectedTask} boardId={boardId} onClose={onClose} />
+              <TaskModalActions selectedTask={taskData} boardId={boardId} onClose={onClose} />
             </div>
 
           </div>
