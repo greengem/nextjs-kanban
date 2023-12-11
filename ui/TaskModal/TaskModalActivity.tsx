103b9d82806ed741f6a6cbc2c4a25b36a461bc4f
Green Gem <chris_waitt@me.com>
2023-12-11 16:50:22 +0000

    Activity section in modal

--- ui/TaskModal/TaskModalActivity.tsx
+++ ui/TaskModal/TaskModalActivity.tsx
@@ -1,37 +1,87 @@
 'use client'
-import { IconActivity } from "@tabler/icons-react"
+import { Session } from "next-auth";
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast from 'react-hot-toast';
 import TaskModalActivityItem from "./TaskModalActivityItem"
 import { Avatar } from "@nextui-org/avatar"
 import { Textarea } from "@nextui-org/input"
 import { Button } from "@nextui-org/button"
-export default function TaskModalActivity() {
+import { ActivityWithUser } from "@/types/types"
+import { IconActivity } from "@tabler/icons-react"
+import { handleCreateActivity } from "@/actions/ActivityActions";
+import { CreateActivitySchema } from "@/types/zodTypes";
+import { ActivityCreationData } from "@/types/types";
+
+interface TaskModalActivityProps {
+    activities: ActivityWithUser[];
+    columnTitle?: string;
+    boardId: string;
+    taskId: string;
+    session: Session | null;
+}
+
+export default function TaskModalActivity({ activities, columnTitle, session, taskId, boardId }: TaskModalActivityProps) {
+    const { register, handleSubmit, reset, formState: { isSubmitting } } = useForm<ActivityCreationData>({
+        resolver: zodResolver(CreateActivitySchema),
+        defaultValues: { boardId, taskId }
+    });
+
+    const onSubmit: SubmitHandler<ActivityCreationData> = async (data) => {
+
+        const response = await handleCreateActivity(data);
+
+        if (response.success) {
+            toast.success('Activity Created');
+            reset();
+        } else {
+            toast.error(response.message);
+        }
+    };
 
     return (
         <>
-        <div className='flex gap-2 w-full'>
-            <IconActivity size={20} className='mt-1 w-5' />
-            <h4 className='text-large font-semibold'>Activity</h4>
-        </div>
-                <div className="flex gap-3 mb-3">
-                    <Avatar 
-                        showFallback 
-                        name='Chris Waitt' 
-                        src='https://images.unsplash.com/broken' 
-                    />
-                    <div className="w-full" >
+            <div className='flex gap-2 w-full'>
+                <IconActivity size={20} className='mt-1 w-5' />
+                <h4 className='text-large font-semibold'>Activity</h4>
+            </div>
+
+            <div className="flex gap-3 mb-3">
+                <Avatar 
+                    showFallback 
+                    name={session?.user?.name ?? 'Unknown'} 
+                    src={session?.user?.image ?? undefined}
+                    className="shrink-0"
+                />
+                <div className="w-full" >
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <Textarea
-                            size="sm"
                             className="mb-2"
+                            placeholder="Enter a description..."
+                            {...register('content')}
                         />
+
+                        <input type="hidden" {...register('boardId')} />
+                        <input type="hidden" {...register('taskId')} />
+
                         <div className="flex gap-2">
-                            <Button size="sm" color="primary">Save</Button>
-                            <Button size="sm">Cancel</Button>
+                        <Button type="submit">Submit</Button>
+
                         </div>
-                    </div>
+                    </form>
                 </div>
-                <ul>
-                    <TaskModalActivityItem />
-                </ul>
+            </div>
+
+            <ul className="space-y-3">
+                {activities && activities.map((activity, index) => (
+                    <TaskModalActivityItem 
+                        key={index}
+                        activity={activity}
+                        columnTitle={columnTitle}
+                        boardId={boardId}
+                    />
+                ))}
+            </ul>
         </>
     )
 }
