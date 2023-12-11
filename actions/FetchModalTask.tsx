3ecc1a3768aa06d452df18c94df760b99cb72330
Green Gem <chris_waitt@me.com>
2023-12-11 16:48:13 +0000

    Fetch task by task id for our modal

---
+++ actions/FetchModalTask.tsx
@@ -0,0 +1,49 @@
+'use server';
+import prisma from '@/db/prisma';
+
+export async function handleFetchTask(taskId: string) {
+    try {
+        const task = await prisma.task.findUnique({
+            where: {
+                id: taskId
+            },
+            select: {
+                id: true,
+                title: true,
+                description: true,
+                dueDate: true,
+                createdAt: true,
+                updatedAt: true,
+                order: true,
+                columnId: true,
+                activities: {
+                    orderBy: {
+                        createdAt: 'desc'
+                    },
+                    select: {
+                        id: true,
+                        type: true,
+                        content: true,
+                        createdAt: true,
+                        user: {
+                            select: {
+                                id: true,
+                                name: true,
+                                image: true,
+                            }
+                        },
+                    }
+                }
+            }
+        });
+
+        if (task) {
+            return { success: true, task: task };
+        } else {
+            return { success: false, message: 'Task not found' };
+        }
+    } catch (e) {
+        console.error('Error fetching task:', e);
+        return { success: false, message: 'Failed to fetch task' };
+    }
+}
