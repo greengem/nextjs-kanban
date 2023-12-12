7c29c7e319bd4809e1545b39f9fa40d0ab16955c
Green Gem <chris_waitt@me.com>
2023-12-12 18:03:13 +0000

    To finish the modal implementation we need to change the onClick handler of the task item to be a Link element pointing to the task pages that will be intercepted

--- app/(board-layout)/layout.tsx
+++ app/(board-layout)/layout.tsx
@@ -2,15 +2,17 @@ import DashboardLayout from "@/ui/DashboardLayout"
 import SubNavbar from '@/ui/Navbar/SubNavbar'
 
 export default function RootLayout({
-  children,
+  children, modal
 }: {
   children: React.ReactNode
+  modal: React.ReactNode
 }) {
   return (
     <DashboardLayout>
       <SubNavbar />
       <main className='p-3 md:p-5'>
         {children}
+        {modal}
       </main>
     </DashboardLayout>
   )
--- ui/Task/TaskItem.tsx
+++ ui/Task/TaskItem.tsx
@@ -1,13 +1,13 @@
 import { IconGripVertical } from '@tabler/icons-react';
 import { TaskSummary, ExpandedTask } from '@/types/types';
+import Link from 'next/link';
 
 interface TaskItemProps {
   task: TaskSummary;
-  onTaskClick: (task: TaskSummary) => void;
   dragHandleProps: any;
 }
 
-export default function TaskItem({ task, onTaskClick, dragHandleProps }: TaskItemProps) {
+export default function TaskItem({ task, dragHandleProps }: TaskItemProps) {
   return (
     <div className='flex select-none rounded-md bg-zinc-800 shadow-md ring-0 hover:ring-2 hover:ring-primary'>
 
@@ -16,11 +16,11 @@ export default function TaskItem({ task, onTaskClick, dragHandleProps }: TaskIte
         <IconGripVertical className='text-primary' size={22} />
       </div>
 
-      <div className='flex-grow px-3 py-2' onClick={() => onTaskClick(task)}>
+      <Link className='flex-grow px-3 py-2' href={`/task/${task.id}`}>
         <div className='text-sm cursor-pointer'>
             {task.title}
         </div>
-      </div>
+      </Link>
 
     </div>
   );
