ddfa4f8bff8c261145dedbc6f2e6b6849d4c7950
Green Gem <chris_waitt@me.com>
2023-12-13 17:53:02 +0000

    Updated navbar

--- ui/Navbar/NavbarWrapper.tsx
+++ ui/Navbar/NavbarWrapper.tsx
@@ -1,31 +1,32 @@
 'use client'
 
-import React, { useState } from 'react';
+import React from 'react';
 import NavbarTitle from "./NavbarTitle";
 import ColourPicker from "./ColourPicker";
 import NavbarAvatar from "./NavbarAvatar";
 import { IconPaint } from "@tabler/icons-react";
 import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/react";
+import { Session } from 'next-auth';
 
 export default function NavbarWrapper({
-    userAvatar, userName,
+    session
 } : {
-    userAvatar: string | null | undefined; userName: string | null | undefined;
+    session: Session
 }) {
     return (
         <>
-        <nav className="flex gap-2 px-5 py-1 bg-zinc-900 items-center justify-between opacity-80">
+        <nav className="flex gap-2 px-5 py-1 bg-zinc-900 items-center justify-between opacity-80 border-b-1 border-zinc-950">
             <NavbarTitle />
             <div className="flex gap-5 items-center justify-between">
-                <Popover placement="left">
+                <Popover placement="left-start" backdrop='blur'>
                     <PopoverTrigger>
-                        <IconPaint size={32} className='cursor-pointer' />
+                        <IconPaint size={26} className='cursor-pointer' />
                     </PopoverTrigger>
                     <PopoverContent>
                         <ColourPicker />
                     </PopoverContent>
                 </Popover>
-                <NavbarAvatar userAvatar={userAvatar} userName={userName} />
+                <NavbarAvatar session={session} />
             </div>
         </nav>
         </>
