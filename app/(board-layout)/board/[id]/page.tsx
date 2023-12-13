8071a799b918d7172fbb4dc69af0cf895a57d979
Green Gem <chris_waitt@me.com>
2023-12-13 11:39:20 +0000

    Moved board actions to correect file

--- app/(board-layout)/board/[id]/page.tsx
+++ app/(board-layout)/board/[id]/page.tsx
@@ -4,6 +4,7 @@ import { BoardDetails } from "@/types/types";
 import DeleteBoardForm from "@/ui/Forms/DeleteBoardForm";
 import PageHeading from "@/ui/PageHeading";
 import Board from "@/ui/Board/Board";
+import { IconStar } from "@tabler/icons-react";
 interface BoardProps {
   params: { id: string };
 }
@@ -23,7 +24,11 @@ export default async function BoardPage({ params }: BoardProps) {
         bg-zinc-800 opacity-80 
         px-5 py-2 mb-5"
       >
-        <div>{board.title}</div>
+        <div className="flex gap-5">
+          <div>{board.title}</div>
+          <button><IconStar size={18} /></button>
+          <div>Filters</div>
+        </div>
         <DeleteBoardForm boardId={board.id} />
       </div>
 
--- ui/Navbar/NavbarTitle.tsx
+++ ui/Navbar/NavbarTitle.tsx
@@ -1,12 +1,15 @@
+import { IconLayoutKanban, IconStar } from "@tabler/icons-react";
 import Link from "next/link";
 
 export default function NavbarTitle() {
 
     return (
-        <div className="flex items-center my-2 w-15 shrink-0 grow-0">
-            <p className="font-semibold text-lg tracking-tight">
-                <Link href='/dashboard/'>Next.Kanban</Link>
-            </p>
+        <div className="flex items-center my-2 gap-5">
+
+            <Link href='/dashboard/' className="flex gap-1 items-center font-semibold text-lg tracking-tight">
+                <IconLayoutKanban />Next.Kanban Alpha
+            </Link>
+
         </div>
     )
 }
