2e8ca64748c6218ef4b7e34790ce54fca82efe3b
Green Gem <chris_waitt@me.com>
2023-12-13 16:02:42 +0000

    Removed unused modal actions

--- ui/TaskModal/TaskModalActions.tsx
+++ ui/TaskModal/TaskModalActions.tsx
@@ -1,5 +1,4 @@
 import DeleteTaskForm from '@/ui/Forms/DeleteTaskForm';
-import { IconArrowRight, IconTrash } from '@tabler/icons-react';
 
 export default function TaskModalActions({
     selectedTask, boardId,
@@ -10,7 +9,6 @@ export default function TaskModalActions({
         <>
               <h4 className='text-sm text-zinc-500'>Actions</h4>
               <ul className='text-sm space-y-2'>
-                <li className='flex items-center gap-2 bg-zinc-800 px-2 py-1 rounded-md'><IconArrowRight size={14} /> Move</li>
                 <li className='bg-zinc-800 px-2 py-1 rounded-md'>
                   <DeleteTaskForm 
                     taskId={selectedTask.id} 
@@ -18,7 +16,6 @@ export default function TaskModalActions({
                     columnId={selectedTask.columnId}
                   />
                 </li>
-                <li className='flex items-center gap-2 bg-zinc-800 px-2 py-1 rounded-md'><IconTrash size={14} /> Make Template</li>
               </ul>
         </>
     )
