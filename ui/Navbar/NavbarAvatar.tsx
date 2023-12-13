d98e6785ab1db50aa5753cc42f0703f9a2bb409c
Green Gem <chris_waitt@me.com>
2023-12-13 17:52:40 +0000

    Clicking the avatar now opens an action menu and allows signing out and viewing the profile

--- ui/Navbar/NavbarAvatar.tsx
+++ ui/Navbar/NavbarAvatar.tsx
@@ -1,11 +1,44 @@
+'use client'
 import { Avatar } from "@nextui-org/avatar";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/dropdown";
+import { IconLogout, IconUser } from "@tabler/icons-react";
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
+import toast from "react-hot-toast";
+import { useRouter } from 'next/navigation'
 
 export default function NavbarAvatar({ 
-    userAvatar, 
-    userName
+    session
 }: {
-    userAvatar: string | null | undefined; 
-    userName: string | null | undefined;
+    session: Session
 }) {
-    return <Avatar showFallback isBordered color="primary" name={userName ?? ''} src={userAvatar ?? ''} size="sm" />;
+    const router = useRouter()
+
+    const handleAction = async (action: 'profile' | 'signout') => {
+        if (action === 'profile') {
+            toast.error('Profile coming soon');
+        } else if (action === 'signout') {
+            signOut({ callbackUrl: '/' });
+        }
+    }
+
+    return (
+        <Dropdown backdrop="blur">
+            <DropdownTrigger>
+                <Avatar 
+                    showFallback 
+                    isBordered 
+                    color="primary" 
+                    name={session.user?.name ?? ''} 
+                    src={session.user?.image ?? ''} 
+                    size="sm" 
+                    className="cursor-pointer" 
+                />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="User Actions" onAction={(key) => handleAction(key as 'profile' | 'signout')}>
+                <DropdownItem key="profile" startContent={<IconUser size={18} />}>Profile</DropdownItem>
+                <DropdownItem key="signout" startContent={<IconLogout size={18} />}>Sign out</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+    );
 }
