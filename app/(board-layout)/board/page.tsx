e78ccf2c3d95235cd87dbf967a55586d8bc69564
Green Gem <chris_waitt@me.com>
2023-12-14 12:35:46 +0000

    Show star on board listing if board is a favourite

--- app/(board-layout)/board/page.tsx
+++ app/(board-layout)/board/page.tsx
@@ -4,7 +4,7 @@ import CreateBoardForm from "@/ui/Forms/CreateBoardForm";
 import Link from "next/link";
 import { Card, CardBody } from '@/ui/Card/Card';
 import PageHeading from "@/ui/PageHeading";
-import { IconList  } from "@tabler/icons-react";
+import { IconList, IconStarFilled  } from "@tabler/icons-react";
 
 export default async function Boards() {
   const boards: BoardSummary[] = await getBoardsSummary();
@@ -13,16 +13,16 @@ export default async function Boards() {
     <div className="p-5">
       <PageHeading title='Boards' />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5">
-        
         {boards.map((board) => (
           <Link key={board.id} href={`/board/${board.id}`}>
             <Card>
-              <CardBody className="
-                h-28 
-                flex flex-col justify-end relative
-                hover:bg-zinc-800
-              ">
-                <span className="absolute text-xs top-2 right-2 bg-zinc-800 w-10 h-6 rounded-lg flex items-center justify-center text-primary gap-1">
+              <CardBody className="h-28 flex flex-col justify-end relative hover:bg-zinc-800">
+                {board.isFavorited && (
+                  <span className="absolute text-xs top-3 left-3 text-primary">
+                    <IconStarFilled size={16} />
+                  </span>
+                )}
+                <span className="absolute text-xs top-3 right-3 flex items-center justify-center text-primary gap-1">
                   <IconList size={16} /><span>{board.tasksCount}</span>
                 </span>
                 {board.title}
