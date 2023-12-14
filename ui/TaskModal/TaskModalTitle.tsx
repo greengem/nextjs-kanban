2d96aa80f3a1e75eada0168af2254c02e14ef046
Green Gem <chris_waitt@me.com>
2023-12-14 18:24:11 +0000

    Modal title styling

--- ui/TaskModal/TaskModalTitle.tsx
+++ ui/TaskModal/TaskModalTitle.tsx
@@ -14,8 +14,8 @@ export default function TaskModalTitle( {
             <div className='flex-col w-full pr-5'>
                 <EditTaskNameForm taskId={selectedTask.id} title={selectedTask.title} boardId={boardId} />
                 <div className='text-xs text-zinc-100 font-normal'>
-                    <p>In list {selectedTask.column.title}</p>
-                    <p>Created on {format(new Date(selectedTask.createdAt), 'MMMM d, yyyy')} | Updated on {format(new Date(selectedTask.updatedAt), 'MMMM d, yyyy')}</p>
+                    <p>In list <span className='text-primary'>{selectedTask.column.title}</span></p>
+                    <p className='text-zinc-500'>Created on {format(new Date(selectedTask.createdAt), 'MMMM d, yyyy')} | Updated on {format(new Date(selectedTask.updatedAt), 'MMMM d, yyyy')}</p>
                 </div>
             </div>
         </>
