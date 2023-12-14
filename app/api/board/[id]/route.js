d3a59075f40d8ce00196ef88a90b7951886a8489
Green Gem <chris_waitt@me.com>
2023-12-14 12:36:22 +0000

    Track tasks moving between columns and create activity for them

--- app/api/board/[id]/route.js
+++ app/api/board/[id]/route.js
@@ -1,12 +1,16 @@
 import prisma from '@/db/prisma';
 import { revalidatePath } from 'next/cache';
+import { auth } from "@/auth";
 
 export async function PUT(request, { params }) {
     try {
+        const session = await auth();
+        const userId = session?.user?.id;
         const boardId = params.id;
         const { boardData } = await request.json();
 
         await prisma.$transaction(async (prisma) => {
+            // Updating columns
             for (const column of boardData.columns) {
                 if (column.id) {
                     await prisma.column.update({
@@ -16,9 +20,16 @@ export async function PUT(request, { params }) {
                 }
             }
 
+            // Updating tasks and creating activity entries if needed
             for (const column of boardData.columns) {
                 for (const task of column.tasks) {
                     if (task.id) {
+                        // Fetch the original task data
+                        const originalTask = await prisma.task.findUnique({
+                            where: { id: task.id },
+                        });
+
+                        // Update the task
                         await prisma.task.update({
                             where: { id: task.id },
                             data: {
@@ -26,12 +37,29 @@ export async function PUT(request, { params }) {
                                 columnId: column.id,
                             },
                         });
+
+                        // Check if the task has been moved to a different column
+                        if (originalTask && originalTask.columnId !== column.id) {
+                            // Create a 'TASK_MOVED' activity entry
+                            await prisma.activity.create({
+                                data: {
+                                    type: 'TASK_MOVED',
+                                    userId: userId,
+                                    taskId: task.id,
+                                    boardId: boardId,
+                                    oldColumnId: originalTask.columnId,
+                                    newColumnId: column.id,
+                                },
+                            });
+                        }
                     }
                 }
             }
         });
 
         revalidatePath(`/board/${boardId}`);
+        
+        // Return success response
         return new Response(JSON.stringify({ success: true }), {
             status: 200,
             headers: {
@@ -39,6 +67,7 @@ export async function PUT(request, { params }) {
             },
         });
     } catch (error) {
+        // Return error response
         return new Response(JSON.stringify({ error: "An error occurred updating the board." }), {
             status: 500,
             headers: {
