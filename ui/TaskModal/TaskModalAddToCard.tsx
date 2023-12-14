499668c19bc95ac63c67256cc18325a7eda2a89b
Green Gem <chris_waitt@me.com>
2023-12-14 18:23:58 +0000

    Modal add to card updated to only show what is implenented

--- ui/TaskModal/TaskModalAddToCard.tsx
+++ ui/TaskModal/TaskModalAddToCard.tsx
@@ -1,19 +1,28 @@
 import { IconCalendar, IconCheckbox, IconPaint, IconPaperclip, IconTag, IconUser } from '@tabler/icons-react';
 import TaskModalLabelAddToCard from './ModalAddToCard/TaskModalLabelAddToCard';
+import { getLabelsForBoard } from "@/lib/FetchData";
 
-export default function TaskModalAddToCard() {
+export default async function TaskModalAddToCard({ 
+    task 
+} : {
+    task: any;
+}) {
+    const labels = await getLabelsForBoard(task?.column.boardId);
+
     return (
         <>
             <h4 className='text-sm text-zinc-500'>Add to card</h4>
             <ul className='text-sm space-y-2'>
-                <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconUser size={14} /> Members</li>
+                {/*<li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconUser size={14} /> Members</li>*/}
                 <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'>
-                    <TaskModalLabelAddToCard />
+                    <TaskModalLabelAddToCard labels={labels} />
                 </li>
-                <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconCheckbox size={14} /> Checklist</li>
+                {/*<li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconCheckbox size={14} /> Checklist</li>*/}
                 <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconCalendar size={14} /> Dates</li>
+                {/*
                 <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconPaperclip size={14} /> Attachement</li>
                 <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconPaint size={14} /> Cover</li>
+                */}
             </ul>
         </>
     )
