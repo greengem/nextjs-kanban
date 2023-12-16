f8909bae9fe8405ffb75505374be12c78e7db6f7
Green Gem <chris_waitt@me.com>
2023-12-16 11:33:59 +0000

    removed bg colour on task page for now

--- app/(board-layout)/task/[id]/page.tsx
+++ app/(board-layout)/task/[id]/page.tsx
@@ -7,8 +7,6 @@ interface BoardProps {
 export default async function Task({ params }: BoardProps) {
 
   return (
-    <div className="bg-zinc-900">
       <TaskPage taskId={params.id} />
-    </div>
   );
 }
