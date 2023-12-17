86669cc0ca92837785170d1b52896e1e1d4a3940
Green Gem <chris_waitt@me.com>
2023-12-17 10:59:46 +0000

    Update CardDates popover to work for both start and due Dates

--- ui/TaskModal/ModalAddToCard/AddToCardDates.tsx
+++ ui/TaskModal/ModalAddToCard/AddToCardDates.tsx
@@ -4,25 +4,22 @@ import { IconCalendar } from '@tabler/icons-react';
 import AddToCardDatesCalendar from "./AddToCardDatesCalendar";
 
 export default function AddToCardDates({
-    taskId, boardId, dueDate
+    taskId, boardId, date, dateType
 } : {
-    taskId: string; boardId: string; dueDate: Date | null;
+    taskId: string; boardId: string; date: Date | null; dateType: 'startDate' | 'dueDate';
 }) {
     return (
-        <>
-            <li className='bg-zinc-800 rounded-md'>
-                <Popover placement="bottom-start" triggerScaleOnOpen={false}>
-                    <PopoverTrigger>
-                        <button className='flex items-center gap-2 px-2 py-2 w-full'>
-                            <IconCalendar size={14} /> Due Date
-                        </button>
-                    </PopoverTrigger>
-                    
-                    <PopoverContent>
-                        <AddToCardDatesCalendar taskId={taskId} boardId={boardId} dueDate={dueDate} />
-                    </PopoverContent>
-                </Popover>
-            </li>
-        </>
+        <li className='bg-zinc-800 rounded-md'>
+            <Popover placement="bottom-start" triggerScaleOnOpen={false}>
+                <PopoverTrigger>
+                    <button className='flex items-center gap-2 px-2 py-2 w-full'>
+                        <IconCalendar size={14} /> {dateType === 'dueDate' ? 'Due Date' : 'Start Date'}
+                    </button>
+                </PopoverTrigger>
+                <PopoverContent>
+                    <AddToCardDatesCalendar taskId={taskId} boardId={boardId} date={date} dateType={dateType} />
+                </PopoverContent>
+            </Popover>
+        </li>
     );
 }
