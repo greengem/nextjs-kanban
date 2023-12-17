429670ade594ac4f756b897b341cd797ab3c6a4c
Green Gem <chris_waitt@me.com>
2023-12-17 20:17:58 +0000

    update label server actions fro new schema

--- actions/LabelServerActions.tsx
+++ actions/LabelServerActions.tsx
@@ -3,51 +3,42 @@ import prisma from '@/db/prisma';
 import { revalidatePath } from 'next/cache';
 
 export async function handleSaveLabel({ labelId, taskId, boardId }: { labelId: string; taskId: string, boardId: string }) {
-
-    if (!labelId) {
-        return { success: false, message: 'Label ID is missing' };
-    }
-    if (!taskId) {
-        return { success: false, message: 'Task ID is missing' };
-    }
-    if (!boardId) {
-        return { success: false, message: 'Board ID is missing' };
+    if (!labelId || !taskId) {
+        return { success: false, message: 'Label ID or Task ID is missing' };
     }
 
     try {
-        // Use upsert to either create a new association or update an existing one
-        await prisma.labelOnTask.upsert({
-            where: {
-                labelId_taskId: { labelId, taskId }, // Unique composite key
+        await prisma.task.update({
+            where: { id: taskId },
+            data: {
+                labels: {
+                    connect: { id: labelId },
+                },
             },
-            update: {}, // No update needed, but required in upsert method
-            create: { labelId, taskId }, // Create a new association
         });
 
         revalidatePath(`/board/${boardId}`);
 
         return { success: true, message: 'Label added to task' };
     } catch (e) {
+        console.error('Error adding label to task:', e);
         return { success: false, message: 'Failed to associate label with task' };
     }
 }
 
 
 export async function handleRemoveLabel({ labelId, taskId, boardId }: { labelId: string; taskId: string, boardId: string }) {
-    if (!labelId) {
-        return { success: false, message: 'Label ID is missing' };
-    }
-    if (!taskId) {
-        return { success: false, message: 'Task ID is missing' };
-    }
-    if (!boardId) {
-        return { success: false, message: 'Board ID is missing' };
+    if (!labelId || !taskId) {
+        return { success: false, message: 'Label ID or Task ID is missing' };
     }
 
     try {
-        await prisma.labelOnTask.delete({
-            where: {
-                labelId_taskId: { labelId, taskId }, // Unique composite key
+        await prisma.task.update({
+            where: { id: taskId },
+            data: {
+                labels: {
+                    disconnect: { id: labelId },
+                },
             },
         });
 
@@ -55,7 +46,6 @@ export async function handleRemoveLabel({ labelId, taskId, boardId }: { labelId:
 
         return { success: true, message: 'Label removed from task' };
     } catch (e) {
-
         console.error('Error removing label from task:', e);
         return { success: false, message: 'Failed to remove label from task' };
     }
