2fa1902b136ba3188da4911e66b7449f1092a3dd
Green Gem <chris_waitt@me.com>
2023-12-17 20:17:37 +0000

    Correctly assign labels to tasks and display selected tasks

--- ui/TaskModal/ModalAddToCard/AddToCardLabels.tsx
+++ ui/TaskModal/ModalAddToCard/AddToCardLabels.tsx
@@ -5,44 +5,36 @@ import { Input, Checkbox, CheckboxGroup } from "@nextui-org/react";
 import { IconTag } from '@tabler/icons-react';
 import { handleRemoveLabel, handleSaveLabel } from '@/actions/LabelServerActions';
 
-type ColorName = 'green' | 'yellow' | 'red' | 'orange' | 'purple' | 'blue';
-
-const colorMapping: Record<ColorName, string> = {
-    green: "bg-green-500",
-    yellow: "bg-yellow-500",
-    red: "bg-red-500",
-    orange: "bg-orange-500",
-    purple: "bg-purple-500",
-    blue: "bg-blue-500",
+type Label = {
+    id: string;
+    title: string | null;
+    color: string;
 };
 
-function getTailwindColorClass(colorName: ColorName): string {
-    return colorMapping[colorName] || "bg-gray-500";
-}
-
 export default function AddToCardLabels({ 
-    labels, taskId, boardId
-}: { 
-    labels: Label[], taskId: string, boardId: string;
+    labels, task, boardId
+} : {
+    labels: Label[]; task: any; boardId: string;
 }) {
+    const taskId = task.id;
     const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
 
     useEffect(() => {
-        const initialSelected = labels.filter(label => label.isSelected).map(label => label.id);
-        setSelectedLabels(initialSelected);
-    }, [labels]);
+        // Initialize selectedLabels with the IDs of labels associated with the task
+        const initialSelectedLabels = task.labels.map((label: Label) => label.id);
+        setSelectedLabels(initialSelectedLabels);
+    }, [task, labels]);
 
+    // Update to handle label selection change
     const handleCheckboxChange = async (labelId: string) => {
         const isSelected = selectedLabels.includes(labelId);
         const data = { labelId, taskId, boardId };
-    
+
         try {
             if (isSelected) {
-                // Remove the label from task
                 await handleRemoveLabel(data);
                 setSelectedLabels(selectedLabels.filter(id => id !== labelId));
             } else {
-                // Add the label to task
                 await handleSaveLabel(data);
                 setSelectedLabels([...selectedLabels, labelId]);
             }
@@ -50,39 +42,38 @@ export default function AddToCardLabels({
             console.error('Error updating label association:', error);
         }
     };
-    
 
-    
     return (
-        <Popover placement="bottom-start">
-            <PopoverTrigger>
-                <button className="w-full flex items-center gap-2"><IconTag size={14} /> Labels</button>
-            </PopoverTrigger>
+        <li className='bg-zinc-800 rounded-md'>
+            <Popover placement="bottom-start" triggerScaleOnOpen={false}>
+                <PopoverTrigger>
+                    <button className='flex items-center gap-2 px-2 py-2 w-full'><IconTag size={14} /> Labels</button>
+                </PopoverTrigger>
 
-            <PopoverContent>
-                <div className="px-1 py-2">
-                    <h4 className="text-center font-semibold text-lg mb-2">Labels</h4>
-                    <Input placeholder="Search for labels..." size="sm" className="w-full mb-2" labelPlacement="outside" />
-                    <CheckboxGroup classNames={{ base: "w-full" }}>
-                        {labels.map((label: any) => (
-                            <Checkbox 
-                                key={label.id} 
-                                value={label.color}
-                                isSelected={selectedLabels.includes(label.id)}
-                                onValueChange={() => handleCheckboxChange(label.id)}
-                                classNames={{
-                                    base: "inline-flex max-w-md w-full bg-content1 m-0 items-center justify-start cursor-pointer rounded-lg gap-1 p-1 data-[selected=true]:border-primary",
-                                    label: "w-full",
-                                }}
-                            >
-                                <div className="w-full flex justify-between gap-2">
-                                    <div className={`${getTailwindColorClass(label.color)} h-6 w-full rounded-md`}></div>
-                                </div>
-                            </Checkbox>
-                        ))}
-                    </CheckboxGroup>
-                </div>
-            </PopoverContent>
-        </Popover>
+                <PopoverContent>
+                    <div className="px-1 py-2">
+                        <h4 className="text-center font-semibold text-lg mb-2">Labels</h4>
+                        <Input placeholder="Search for labels..." size="sm" className="w-full mb-2" labelPlacement="outside" />
+                        <CheckboxGroup value={selectedLabels} onValueChange={setSelectedLabels}>
+                            {labels.map(label => (
+                                <Checkbox
+                                    key={label.id}
+                                    value={label.id}
+                                    isSelected={selectedLabels.includes(label.id)}
+                                    onValueChange={() => handleCheckboxChange(label.id)}
+                                    classNames={{
+                                        base: `inline-flex max-w-md w-full bg-content1 m-0 items-center justify-start cursor-pointer rounded-lg gap-1 p-1 data-[selected=true]:border-primary`,
+                                        label: "w-full",
+                                    }}
+                                >
+                                    <div className={`bg-${label.color}-500 h-6 w-full rounded-md mr-2`}></div>
+                                    {label.title && <span>{label.title}</span>}
+                                </Checkbox>
+                            ))}
+                        </CheckboxGroup>
+                    </div>
+                </PopoverContent>
+            </Popover>
+        </li>
     );
 }
