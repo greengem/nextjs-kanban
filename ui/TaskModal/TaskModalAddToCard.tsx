2d831b00b95bdcd5a91e901b65aa9e828f0f91be
Green Gem <chris_waitt@me.com>
2023-12-17 20:18:44 +0000

    List selected labels in modal

--- ui/TaskModal/TaskModalAddToCard.tsx
+++ ui/TaskModal/TaskModalAddToCard.tsx
@@ -16,14 +16,10 @@ export default async function TaskModalAddToCard({
             <h4 className='text-sm text-zinc-500'>Add to card</h4>
             <ul className='text-sm space-y-2'>
                 {/*<li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconUser size={14} /> Members</li>*/}
-                <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'>
-                    <AddToCardLabels labels={labels} taskId={task.id} boardId={task.column.boardId} />
-                </li>
+                <AddToCardLabels labels={labels} task={task} boardId={task.column.boardId} />
                 {/*<li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconCheckbox size={14} /> Checklist</li>*/}
                 <AddToCardDates task={task} dateType="startDate" />
                 <AddToCardDates task={task} dateType="dueDate" />
-            
-
                 {/*
                 <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconPaperclip size={14} /> Attachement</li>
                 <li className='flex items-center gap-2 bg-zinc-800 px-2 py-2 rounded-md'><IconPaint size={14} /> Cover</li>
