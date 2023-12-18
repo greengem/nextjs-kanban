cfce10251be902b1351748f52ccf0ccd29aeeb4a
Green Gem <chris_waitt@me.com>
2023-12-18 16:04:22 +0000

    programmatically close the popover as needed. pass the selected edit label to the edit pane

--- ui/TaskModal/ModalAddToCard/AddToCardLabels.tsx
+++ ui/TaskModal/ModalAddToCard/AddToCardLabels.tsx
@@ -39,14 +39,15 @@ export default function AddToCardLabels({ labels, task, boardId }: { labels: Lab
     const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
     const [editMode, setEditMode] = useState(false);
     const [editingLabel, setEditingLabel] = useState<Label | null>(null);
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+    const [currentLabelColor, setCurrentLabelColor] = useState('');
+
 
     useEffect(() => {
-        // Initialize selectedLabels with the IDs of labels associated with the task
         const initialSelectedLabels = task.labels.map((label: Label) => label.id);
         setSelectedLabels(initialSelectedLabels);
     }, [task, labels]);
 
-    // Update to handle label selection change
     const handleCheckboxChange = async (labelId: string) => {
         const isSelected = selectedLabels.includes(labelId);
         const data = { labelId, taskId, boardId };
@@ -64,10 +65,16 @@ export default function AddToCardLabels({ labels, task, boardId }: { labels: Lab
         }
     };
 
+    const closePopover = () => {
+        setIsPopoverOpen(false);
+    };
+
     const enterEditMode = (label: Label) => {
         setEditingLabel(label);
         setEditMode(true);
+        setCurrentLabelColor(label.color);
     };
+    
 
     const exitEditMode = () => {
         setEditMode(false);
@@ -76,21 +83,21 @@ export default function AddToCardLabels({ labels, task, boardId }: { labels: Lab
 
     return (
         <li className='bg-zinc-800 rounded-md'>
-            <Popover placement="bottom-start" triggerScaleOnOpen={false}>
+            <Popover isOpen={isPopoverOpen} onOpenChange={setIsPopoverOpen} placement="bottom-start" triggerScaleOnOpen={false} backdrop='blur'>
                 <PopoverTrigger>
                     <button className='flex items-center gap-2 px-2 py-2 w-full'><IconTag size={14} /> Labels</button>
                 </PopoverTrigger>
 
                 <PopoverContent className='w-64'>
-                    {editMode ? (
+                {editMode ? (
                         <div className="px-1 py-2 w-full">
                             <div className='flex justify-between mb-3 items-center'>
-                                <button onClick={exitEditMode}><IconArrowLeft /></button>
+                                <button onClick={exitEditMode}><IconArrowLeft size={20} /></button>
                                 <h4 className="text-center font-semibold text-lg">Edit Label</h4>
-                                <button><IconX /></button>
+                                <button onClick={closePopover}><IconX size={20} /></button>
                             </div>
 
-                            <div className='bg-green-500 h-7 w-full mb-3 rounded-md p-1'>Text</div>
+                            <div className={`bg-${currentLabelColor}-500 h-9 w-full mb-3 rounded-md p-2`}>Text</div>
 
                             <div className='mb-2'>
                                 <h4 className='uppercase font-semibold text-xs text-zinc-500 mb-1'>Title</h4>
@@ -99,33 +106,38 @@ export default function AddToCardLabels({ labels, task, boardId }: { labels: Lab
 
                             <div className='grid grid-cols-5 gap-2 mb-3'>
                             {colorOptions.map(color => (
-                                <span
-                                    key={color.key}
-                                    className={`bg-${color.key}-500 h-6 w-full rounded-md cursor-pointer`}
-                                />
+                                <div className='w-full'>
+                                    <div key={color.key} className={`bg-${color.key}-500 h-6 w-full rounded-md cursor-pointer`} />
+                                </div>
                             ))}
                             </div>
 
                             <Button className='w-full' size='sm'>X Remove Color</Button>
 
-                            <hr className='my-3' />
+                            <hr className='my-3 border-zinc-800' />
 
                             <div className='flex justify-between'>
                                 <Button size='sm' color='primary'>Save</Button>
-                                <Button size='sm' color='danger'>Cancel</Button>
+                                <Button onClick={exitEditMode} size='sm' color='danger'>Cancel</Button>
                             </div>
 
                         </div>
                     ) : (
+                        // Label list content
                         <div className="px-1 py-2 w-full">
-                            <h4 className="text-center font-semibold text-lg mb-3">Labels</h4>
+                            <div className='flex justify-between items-center mb-3'>
+                                <div className='opacity-0'>
+                                    <IconArrowLeft className='hidden' size={20} />
+                                </div>
+                                <h4 className="text-center font-semibold text-lg">Labels</h4>
+                                <button onClick={closePopover}><IconX size={20} /></button>
+                            </div>
                             <Input placeholder="Search for labels..." size="sm" className="w-full mb-3" labelPlacement="outside" />
                             <CheckboxGroup value={selectedLabels} onValueChange={setSelectedLabels} className='mb-3'>
                                 {labels.map(label => (
                                     <Checkbox
                                         key={label.id}
                                         value={label.id}
-                                        isSelected={selectedLabels.includes(label.id)}
                                         onValueChange={() => handleCheckboxChange(label.id)}
                                         classNames={{
                                             base: `inline-flex max-w-md w-full bg-content1 m-0 items-center justify-start cursor-pointer rounded-lg gap-1 p-1 data-[selected=true]:border-primary`,
@@ -134,7 +146,7 @@ export default function AddToCardLabels({ labels, task, boardId }: { labels: Lab
                                     >
                                         <div className={`bg-${label.color}-500 h-6 w-full rounded-md mr-2`}></div>
                                         {label.title && <span>{label.title}</span>}
-                                        <button onClick={() => enterEditMode(label)}><IconEdit size={22} /></button>
+                                        <button onClick={() => enterEditMode(label)}><IconEdit className='text-zinc-500' size={24} /></button>
                                     </Checkbox>
                                 ))}
                             </CheckboxGroup>
