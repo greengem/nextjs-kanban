8897bcd4245b582bd2af0ab332266dae67f0693d
Green Gem <chris_waitt@me.com>
2023-12-24 20:31:24 +0000

    Convert add checklist to a form using RHF

--- ui/TaskDetail/TaskDetailSidebar/TaskDetailAddToCard/Checklist/AddChecklist.tsx
+++ ui/TaskDetail/TaskDetailSidebar/TaskDetailAddToCard/Checklist/AddChecklist.tsx
@@ -1,42 +1,42 @@
 'use client'
 import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/popover";
-import { Input } from "@nextui-org/input";
-import { IconArrowLeft, IconCheckbox, IconPlus, IconX } from '@tabler/icons-react';
+import { Input } from "@nextui-org/react";
+import { IconCheckbox, IconPlus, IconX } from '@tabler/icons-react';
 import { Button } from "@nextui-org/button";
 import { handleCreateChecklist } from "@/actions/ChecklistServerActions";
 import toast from 'react-hot-toast';
+
+interface FormValues {
+    title: string;
+}
 
 export default function AddChecklist({
     taskId, boardId
 } : {
     taskId: string, boardId: string;
 }) {
-    const [title, setTitle] = useState('');
-    const [error, setError] = useState('');
+    const { register, handleSubmit, reset, formState: { errors } } = useForm<FormValues>();
     const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
-    const handleSubmit = async () => {
-        if (!title) {
-            setError('Please provide a title.');
-            return;
-        }
-
+    const onSubmit: SubmitHandler<FormValues> = async (data) => {
         try {
-            const response = await handleCreateChecklist({ title, taskId, boardId });
+            const response = await handleCreateChecklist({ title: data.title, taskId, boardId });
             if (response.success) {
-                toast.success(response.message);
+                toast.success('Checklist Created');
                 closePopover();
             } else {
                 toast.error(response.message);
             }
         } catch (e) {
-            setError('An error occurred while creating the checklist.');
+            toast.error('An error occurred while creating the checklist.');
         }
     };
 
     const closePopover = () => {
         setIsPopoverOpen(false);
+        reset(); // Reset the form fields
     };
 
     return (
@@ -46,21 +46,21 @@ export default function AddChecklist({
                     <button className="px-2 py-2 bg-zinc-800 rounded-md flex w-full items-center gap-2"><IconCheckbox size={14} /> Checklist</button>
                 </PopoverTrigger>
                 <PopoverContent className='w-64'>
-                    <div className="px-1 py-2 w-full space-y-3">
+                    <form onSubmit={handleSubmit(onSubmit)} className="px-1 py-2 w-full space-y-3">
                         <Input 
                             labelPlacement="outside" 
                             placeholder="Checklist title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)} 
+                            {...register('title', { required: 'Please provide a title.' })}
+                            isInvalid={!!errors.title}
+                            errorMessage={errors.title?.message}
                         />
                         <div className="flex gap-2">
-                            <Button size="sm" color="primary" className="flex items-center" onClick={handleSubmit}><IconPlus size={16} />Create Checklist</Button>
+                            <Button size="sm" color="primary" className="flex items-center" type="submit"><IconPlus size={16} />Create Checklist</Button>
                             <Button size="sm" onClick={closePopover}><IconX size={16} className="flex items-center" />Cancel</Button>
                         </div>
-                        {error && <div className="text-red-500">{error}</div>}
-                    </div>
+                    </form>
                 </PopoverContent>
             </Popover>
         </li>
-    )
+    );
 }
