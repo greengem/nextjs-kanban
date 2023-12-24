484889527e9214869e8d79b501746d547f648350
Green Gem <chris_waitt@me.com>
2023-12-24 01:13:03 +0000

    Changed sidebar from fixed to relative, hidden on mobile

--- ui/Sidebar/Sidebar.tsx
+++ ui/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarNav from "./SidebarNav";
 export default function Sidebar() {
 
   return (
-    <aside className='bg-zinc-900 fixed left-0 bottom-0 w-16 hidden md:block'>
+    <aside className='bg-zinc-800 opacity-90 w-0 lg:w-48 shrink-0 overflow-hidden'>
       <SidebarNav />
     </aside>
   );
