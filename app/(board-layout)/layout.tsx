922dd55840b9c507c726033d8237c2dde12b0346
Green Gem <chris_waitt@me.com>
2023-12-24 01:24:39 +0000

    Removed modal from layout file

--- app/(board-layout)/layout.tsx
+++ app/(board-layout)/layout.tsx
@@ -1,16 +1,14 @@
 import DashboardLayout from "@/ui/DashboardLayout"
 
 export default function RootLayout({
-  children, modal
+  children
 }: {
   children: React.ReactNode
-  modal: React.ReactNode
 }) {
   return (
     <DashboardLayout>
       <main className="h-full">
         {children}
-        {modal}
       </main>
     </DashboardLayout>
   )
