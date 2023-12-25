4b1a8032511707fd1b5193cc8145e82640f40c03
Green Gem <chris_waitt@me.com>
2023-12-25 22:51:56 +0000

    Added back button to task page

--- ui/TaskDetail/TaskDetail.tsx
+++ ui/TaskDetail/TaskDetail.tsx
@@ -3,6 +3,7 @@ import { ExpandedTask } from "@/types/types";
 import TaskDetailTitle from "./TaskDetailTitle/TaskModalTitle";
 import TaskDetailSidebar from "./TaskDetailSidebar/TaskDetailSidebar";
 import TaskDetailView from "./TaskDetailView/TaskDetailView";
+import Link from "next/link";
 
 export default async function TaskDetail({ taskId } : { taskId: string }) {
   const task: ExpandedTask | null = await getTask(taskId);
@@ -13,6 +14,7 @@ export default async function TaskDetail({ taskId } : { taskId: string }) {
 
   return (
     <>
+      <Link href={`/board/${task.column?.boardId}`} >Back to board</Link>
       <TaskDetailTitle selectedTask={task} boardId={task?.column.boardId} />
       
       <div className='grid grid-cols-1 md:grid-cols-4 gap-5 p-5'>
