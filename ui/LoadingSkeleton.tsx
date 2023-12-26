8a3060b8d8dd3c496632a3167147dd6a296106f6
Green Gem <chris_waitt@me.com>
2023-12-26 22:51:53 +0000

    Added simple spinner to loading skeleton

--- ui/LoadingSkeleton.tsx
+++ ui/LoadingSkeleton.tsx
@@ -1,5 +1,9 @@
+import { IconLoader2 } from "@tabler/icons-react";
+
 export default function LoadingSkeleton() {
     return(
-        <p>Loading</p>
+        <div className="flex justify-center pt-20">
+            <IconLoader2 size={32} className="animate-spin mr-2 text-primary" />
+        </div>
     )
 }
