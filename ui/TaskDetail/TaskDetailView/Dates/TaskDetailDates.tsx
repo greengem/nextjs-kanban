a2fd41becf41850d351453e6d49cf5a5cd142423
Green Gem <chris_waitt@me.com>
2023-12-26 22:53:39 +0000

    Move dates to its own component

---
+++ ui/TaskDetail/TaskDetailView/Dates/TaskDetailDates.tsx
@@ -0,0 +1,36 @@
+import { format, isSameMonth, isSameYear } from 'date-fns';
+import TaskDetailItemHeading from '../ui/TaskDetailItemHeading';
+import { IconCalendar } from '@tabler/icons-react';
+import TaskDetailItemContent from '../ui/TaskDetailItemContent';
+
+interface TaskDetailDatesProps {
+    startDate: Date | null;
+    dueDate: Date | null;
+}
+
+export default function TaskDetailDates({ startDate, dueDate }: TaskDetailDatesProps) {
+    function formatDateDisplay(startDate: Date | null, dueDate: Date | null): string {
+        if (!startDate && !dueDate) return '';
+    
+        if (startDate && dueDate) {
+            if (isSameMonth(startDate, dueDate) && isSameYear(startDate, dueDate)) {
+                return `${format(startDate, 'dd')} - ${format(dueDate, 'dd MMM')}`;
+            }
+            return `${format(startDate, 'dd MMM')} - ${format(dueDate, 'dd MMM')}`;
+        } else if (startDate) {
+            return `Start Date: ${format(startDate, 'dd MMM')}`;
+        } else if (dueDate) {
+            return `Due: ${format(dueDate, 'dd MMM')}`;
+        }
+        return '';
+    }
+
+    return (
+        <>
+            <TaskDetailItemHeading title='Dates' icon={<IconCalendar />} />
+            <TaskDetailItemContent indented>
+                {(startDate || dueDate) && <p>{formatDateDisplay(startDate, dueDate)}</p>}
+            </TaskDetailItemContent>
+        </>
+    );
+}
