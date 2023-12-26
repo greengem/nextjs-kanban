1aa6e0e654382cf06e810a0c2fd1583cab6955bc
Green Gem <chris_waitt@me.com>
2023-12-26 22:52:30 +0000

    Replaced back link with a button in task detail

--- ui/TaskDetail/TaskDetail.tsx
+++ ui/TaskDetail/TaskDetail.tsx
@@ -4,6 +4,8 @@ import TaskDetailTitle from "./TaskDetailTitle/TaskModalTitle";
 import TaskDetailSidebar from "./TaskDetailSidebar/TaskDetailSidebar";
 import TaskDetailView from "./TaskDetailView/TaskDetailView";
 import Link from "next/link";
+import { IconArrowLeft } from "@tabler/icons-react";
+import { Button } from "@nextui-org/button";
 
 export default async function TaskDetail({ taskId } : { taskId: string }) {
   const task: ExpandedTask | null = await getTask(taskId);
@@ -14,7 +16,17 @@ export default async function TaskDetail({ taskId } : { taskId: string }) {
 
   return (
     <>
-      <Link href={`/board/${task.column?.boardId}`} >Back to board</Link>
+      <div className="mx-5">
+        <Button 
+          as={Link} 
+          href={`/board/${task.column?.boardId}`} 
+          size="sm" 
+          startContent={<IconArrowLeft size={18} />}
+        >
+          Back to board
+        </Button>
+      </div>
+
       <TaskDetailTitle selectedTask={task} boardId={task?.column.boardId} />
       
       <div className='grid grid-cols-1 md:grid-cols-4 gap-5 p-5'>
