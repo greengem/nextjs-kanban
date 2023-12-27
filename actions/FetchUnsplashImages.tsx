1411ef6b81f44a4db0f7e8b1ec54fbba83dbb599
Green Gem <chris_waitt@me.com>
2023-12-27 01:34:49 +0000

    New server action to fetch from the unsplash api. we can keep the api secret safe by not exposing it to the client

---
+++ actions/FetchUnsplashImages.tsx
@@ -0,0 +1,28 @@
+
+'use server';
+
+interface UnsplashImage {
+    urls: {
+        regular: string;
+    };
+}
+
+export async function fetchUnsplashImages(query: string): Promise<string[]> {
+    const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${encodeURIComponent(query)}&orientation=landscape&client_id=${UNSPLASH_ACCESS_KEY}`;
+
+    try {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Failed to fetch images: ${res.status} ${res.statusText}`);
+        }
+
+        const data = await res.json();
+        return data.results.map((image: UnsplashImage) => image.urls.regular);
+
+    } catch (error) {
+        console.error('Error fetching Unsplash images:', error);
+        return [];
+    }
+}
