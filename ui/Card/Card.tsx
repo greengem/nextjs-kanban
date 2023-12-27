d5f5ce54a024eaa1ea0b314ce2c424da1a91b3dc
Green Gem <chris_waitt@me.com>
2023-12-27 22:55:15 +0000

    Testing a lighter theme

--- ui/Card/Card.tsx
+++ ui/Card/Card.tsx
@@ -4,7 +4,7 @@ import { DraggableProvidedDragHandleProps } from '@hello-pangea/dnd';
 
 export function Card({ children, className = '' }: { children: ReactNode, className?: string }) {
     return (
-        <div className={`shadow-xl rounded-lg bg-zinc-900 text-white ${className}`}>
+        <div className={`shadow-xl rounded-lg bg-zinc-100/90 text-white ${className}`}>
             {children}
         </div>
     )
@@ -18,7 +18,7 @@ export function CardHeaderGrab({
     return (
         <div 
             {...dragHandleProps}
-            className='py-1 text-primary bg-zinc-800 cursor-grab flex justify-center items-center rounded-t-lg'
+            className='py-1 text-primary bg-zinc-100 cursor-grab flex justify-center items-center rounded-t-lg'
         >
             <IconGripHorizontal size={30} />
         </div>
