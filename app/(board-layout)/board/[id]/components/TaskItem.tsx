d4cbd5db96e2c7de36ee3f4c69a14d8f4dc9ad2b
Green Gem <chris_waitt@me.com>
2023-12-30 14:01:04 +0000

    colour tweaks

--- app/(board-layout)/board/[id]/components/TaskItem.tsx
+++ app/(board-layout)/board/[id]/components/TaskItem.tsx
@@ -30,7 +30,7 @@ export default function TaskItem({ task, dragHandleProps }: TaskItemProps) {
   };
 
   return (
-    <div className='flex select-none rounded-md bg-zinc-900 shadow-md ring-0 hover:ring-2 hover:ring-primary'>
+    <div className='flex select-none rounded-md bg-zinc-800 shadow-md ring-0 hover:ring-2 hover:ring-primary'>
 
       <div className='pl-1 pr-1 flex items-center cursor-grab touch-none' {...dragHandleProps}>
         <IconGripVertical className='text-primary' size={24} />
