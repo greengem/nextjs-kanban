013119851d96c902958a8ffec3157bac64142075
Green Gem <chris_waitt@me.com>
2023-12-30 10:34:09 +0000

    Adjust padding in task detail view

--- ui/TaskDetail/TaskDetailView/ui/TaskDetailItemHeading.tsx
+++ ui/TaskDetail/TaskDetailView/ui/TaskDetailItemHeading.tsx
@@ -10,7 +10,7 @@ export default function TaskDetailItemHeading({
     icon, title, endContent
 }: TaskDetailItemHeadingProps) {
     return (
-        <div className="flex w-full items-center justify-between mb-3">
+        <div className="flex w-full items-center justify-between mb-1">
             <div className="flex items-center">
                 <div className='w-[40px] grow-0 shrink-0 justify-center'>{icon}</div>
                 <h4 className='text-xl font-semibold'>{title}</h4>
