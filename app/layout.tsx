799499d5ff86daa1aed6d53b31b4c37517044f36
Green Gem <chris_waitt@me.com>
2023-12-31 19:45:40 +0000

    removed text-white from body

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${inter.className} text-white h-screen antialiased`}>
+      <body className={`${inter.className} h-screen antialiased`}>
         <Toaster />
         <Providers>
           {children}
