870b74c96215aba3c8f83c140968be9b76a60af5
Green Gem <chris_waitt@me.com>
2024-01-01 23:10:51 +0000

    Fix tailwind syntax on bf gradient for splash page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { IconLayoutKanban } from "@tabler/icons-react";
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-br from-zinc-100 to-bg-primary">
+    <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-br from-zinc-100 to-primary">
       <div className="flex-none text-center">
         <h1 className="text-7xl tracking-tighter font-semibold flex items-center">
           <IconLayoutKanban size={80} className="text-primary" /><span className="text-primary">N</span>ext
