ab0a464046f36673e67462c6dd15fd42961bef38
Green Gem <chris_waitt@me.com>
2024-01-01 19:06:08 +0000

    Delete button

--- ui/TaskDetail/TaskDetailSidebar/TaskDetailActions/TaskDetailActions.tsx
+++ ui/TaskDetail/TaskDetailSidebar/TaskDetailActions/TaskDetailActions.tsx
@@ -9,7 +9,7 @@ export default function TaskDetailActions({
         <>
           <h4 className='text-sm text-zinc-500'>Actions</h4>
           <ul className='text-sm space-y-2'>
-            <li className='bg-zinc-800 px-2 py-1 rounded-md'>
+            <li className='bg-zinc-200 px-2 py-1 rounded-md'>
               <DeleteTaskForm 
                 taskId={selectedTask.id} 
                 boardId={boardId} 
