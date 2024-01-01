5b341f923f6f7bfc9eec6acff9efd6094d8fde90
Green Gem <chris_waitt@me.com>
2024-01-01 13:30:37 +0000

    Added suppressHydrationWarning to html element to prevent warning from next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} h-screen antialiased`}>
         <Toaster />
         <Providers>
