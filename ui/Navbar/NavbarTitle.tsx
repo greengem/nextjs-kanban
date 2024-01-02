03ec827cfcb576395939f4b52bfdd56550e1b5ae
Green Gem <chris_waitt@me.com>
2024-01-02 14:44:35 +0000

    Improved navbar titel text

--- ui/Navbar/NavbarTitle.tsx
+++ ui/Navbar/NavbarTitle.tsx
@@ -6,8 +6,8 @@ export default function NavbarTitle() {
     return (
         <div className="flex items-center my-2 gap-5">
 
-            <Link href='/board/' className="flex gap-1 items-center font-semibold text-lg tracking-tight">
-                <IconLayoutKanban />Next.Kanban Alpha
+            <Link href='/board/' className="flex gap-1 items-center text-xl tracking-tight">
+                <IconLayoutKanban />Next.Kanban <span className="text-primary">Alpha</span>
             </Link>
 
         </div>
