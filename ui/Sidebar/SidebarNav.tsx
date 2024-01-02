fd17e4c05ca7687f6fa63e0aef1ef5fcb733163d
Green Gem <chris_waitt@me.com>
2024-01-02 15:02:03 +0000

    Adjust sidebar nav spacing

--- ui/Sidebar/SidebarNav.tsx
+++ ui/Sidebar/SidebarNav.tsx
@@ -9,10 +9,10 @@ export default async function SidebarNav() {
 
     return (
         <div className="px-5 pt-5">
-            <h4 className="uppercase text-xs font-semibold  mb-1">
+            <h4 className="text-sm font-semibold  mb-1">
                 <Link className="text-zinc-500 hover:text-primary flex items-center gap-1" href="/board"><IconFolder size={16} />Boards</Link>
             </h4>
-            <ul className="space-y-1 text-sm ml-2">
+            <ul className="space-y-1 ml-2">
                 {boards.map((board) => (
                     <SidebarNavItem key={board.id} boardId={board.id} boardTitle={board.title} />
                 ))}
