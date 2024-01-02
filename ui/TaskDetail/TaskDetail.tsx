1630481af74e6414d6634f0ebdc7ddcc4f561f12
Green Gem <chris_waitt@me.com>
2024-01-02 14:25:56 +0000

    Added background image and card to the task detail view

--- ui/TaskDetail/TaskDetail.tsx
+++ ui/TaskDetail/TaskDetail.tsx
@@ -6,33 +6,54 @@ import TaskDetailView from "./TaskDetailView/TaskDetailView";
 import Link from "next/link";
 import { IconArrowLeft } from "@tabler/icons-react";
 import { Button } from "@nextui-org/button";
+import { Card } from "../Card/Card";
 
-export default async function TaskDetail({ taskId } : { taskId: string }) {
+export default async function TaskDetail({ taskId }: { taskId: string }) {
   const task: ExpandedTask | null = await getTask(taskId);
 
   if (!task) {
     return <div>Task not found or loading error</div>;
   }
 
+  // Define a variable to store the background style
+  let backgroundStyle = {};
+
+  // Check if the task's column has a backgroundUrl
+  if (task.column && task.column.board && task.column.board.backgroundUrl) {
+    backgroundStyle = {
+      backgroundImage: `url(${task.column.board.backgroundUrl})`,
+    };
+  }
+
   return (
-    <>
-      <div className="mx-5">
-        <Button 
-          as={Link} 
-          href={`/board/${task.column?.boardId}`} 
-          size="sm" 
-          startContent={<IconArrowLeft size={18} />}
-        >
+    <div
+      className={`flex flex-col grow backdrop-blur-sm py-5 bg-cover bg-center bg-gradient-to-tl from-zinc-100 to-primary`}
+      style={backgroundStyle}
+    >
+      <div className="flex-none ml-5 mb-5">
+              <Button
+                as={Link}
+                className="inline-flex"
+                href={`/board/${task.column?.boardId}`}
+                size="sm"
+                startContent={<IconArrowLeft size={18} />}
+              >
           Back to board
         </Button>
-      </div>
+        </div>
+
+      <Card className="mx-5">
+
 
       <TaskDetailTitle selectedTask={task} boardId={task?.column.boardId} />
-      
-      <div className='grid grid-cols-1 md:grid-cols-4 gap-5 p-5'>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 p-5">
         <TaskDetailView task={task} />
         <TaskDetailSidebar task={task} />
       </div>
-    </>
+
+      </Card>
+    </div>
+    
   );
 }
