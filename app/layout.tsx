e3528b9db8faf6bdc97d6821d7b1776a540bb81c
Green Gem <chris_waitt@me.com>
2024-01-02 12:05:58 +0000

    replace h-screen with h-dvh for mobile screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${GeistSans.className} h-screen antialiased`}>
+      <body className={`${GeistSans.className} h-dvh antialiased`}>
         <Toaster />
         <Providers>
           {children}
