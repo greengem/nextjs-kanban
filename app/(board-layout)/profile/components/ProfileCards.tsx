728e7fa4ac9aa9c86a3885e035d2ec4d3ba5e12f
Green Gem <chris_waitt@me.com>
2024-01-03 18:30:13 +0000

    Updated user profile cards

--- app/(board-layout)/profile/components/ProfileCards.tsx
+++ app/(board-layout)/profile/components/ProfileCards.tsx
@@ -1,36 +1,44 @@
 import { Card, CardBody, CardHeader } from "@/ui/Card/Card";
 import { IconCheck, IconLayoutKanban, IconTimeDuration45 } from "@tabler/icons-react";
-
-const cardData = [
-    {
-        icon: <IconLayoutKanban />, 
-        count: 23, 
-        label: "Boards"
-    },
-    {
-        icon: <IconCheck />, 
-        count: 10, 
-        label: "Tasks Completed"
-    },
-    {
-        icon: <IconTimeDuration45 />, 
-        count: 5, 
-        label: "Tasks in Progress"
-    }
-];
+import TotalBoardCount from "./TotalBoardCount";
+import TotalTaskCount from "./TotalTaskCount";
 
 export default function ProfileCards() {
     return (
-        <div className="grid grid-cols-3 gap-5 mb-10">
-            {cardData.map((card, index) => (
-                <Card key={index} className="w-full">
-                    <CardBody className="text-center space-y-2 bg-white p-5  border-2 border-primary">
-                        <div className="flex justify-center">{card.icon}</div>
-                        <div className="font-semibold text-xl">{card.count}</div>
-                        <div>{card.label}</div>
-                    </CardBody>
-                </Card>
-            ))}
+        <div className="grid grid-cols-4 gap-5 mb-10">
+            
+            <Card className="w-full">
+                <CardBody className="text-center space-y-2 bg-white p-5  border-2 border-primary">
+                    <div className="flex justify-center"><IconLayoutKanban /></div>
+                    <div className="font-semibold text-xl"><TotalBoardCount /></div>
+                    <div>Boards</div>
+                </CardBody>
+            </Card>
+
+            <Card className="w-full">
+                <CardBody className="text-center space-y-2 bg-white p-5  border-2 border-primary">
+                    <div className="flex justify-center"><IconLayoutKanban /></div>
+                    <div className="font-semibold text-xl"><TotalTaskCount /></div>
+                    <div>Tasks</div>
+                </CardBody>
+            </Card>
+
+            <Card className="w-full">
+                <CardBody className="text-center space-y-2 bg-white p-5  border-2 border-primary">
+                    <div className="flex justify-center"><IconLayoutKanban /></div>
+                    <div className="font-semibold text-xl"><TotalBoardCount /></div>
+                    <div>Upcoming Deadlines</div>
+                </CardBody>
+            </Card>
+
+            <Card className="w-full">
+                <CardBody className="text-center space-y-2 bg-white p-5  border-2 border-primary">
+                    <div className="flex justify-center"><IconLayoutKanban /></div>
+                    <div className="font-semibold text-xl"><TotalBoardCount /></div>
+                    <div>Overdue Tasks</div>
+                </CardBody>
+            </Card>
+
         </div>
     );
 
