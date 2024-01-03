7aa10e13dac9ed519d41480d09c616aa264d2fab
Green Gem <chris_waitt@me.com>
2024-01-03 16:09:25 +0000

    Removed legacy code

--- ui/Navbar/SubNavbar.tsx
+++
@@ -1,7 +0,0 @@
-import { getBoardsSummary } from "@/lib/FetchData";
-import SubNavbarMenu from "./SubNavbarMenu";
-
-export default async function SubNavbar () {
-    const boards = await getBoardsSummary();
-    return <SubNavbarMenu boards={boards} />;
-}
--- ui/Sidebar/SidebarWrapper.tsx
+++ ui/Sidebar/SidebarWrapper.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useSidebar } from "@/contexts/SidebarContext";
+import { usePathname } from 'next/navigation';
 import { BoardSummary } from "@/types/types";
 import { IconCalendar, IconLayoutKanban, IconTextCaption, IconUser } from "@tabler/icons-react";
 import Link from "next/link";
@@ -10,13 +11,26 @@ export default function SidebarWrapper({
 } : {
     boards: BoardSummary[]
 }) {
+    const pathname = usePathname();
     const { toggled, toggleSidebar } = useSidebar();
-
+    
+    // Define the BoardItemSuffix component here or import it if defined elsewhere
     const BoardItemSuffix = ({ tasksCount }: { tasksCount: number }) => (
         <div className="bg-primary text-xs rounded-full h-6 w-6 flex items-center justify-center text-white">
             {tasksCount}
         </div>
     );
+
+    // Function to determine if the link is active
+    const isActive = (linkPath: string) => pathname === linkPath;
+
+    const activeStyles = {
+        backgroundColor: '#eaabff',
+        color: '#9f0099',
+        '&:hover': {
+          backgroundColor: '#eecef9',
+        },
+    };
 
     return (
         <Sidebar 
@@ -33,6 +47,8 @@ export default function SidebarWrapper({
                     {boards.map((board) => (
                         <MenuItem 
                             key={board.id}
+                            active={isActive(`/board/${board.id}`)}
+                            rootStyles={isActive(`/board/${board.id}`) ? activeStyles : {}}
                             component={<Link href={`/board/${board.id}`} />}
                             suffix={<BoardItemSuffix tasksCount={board.tasksCount} />}
                         >
@@ -40,9 +56,27 @@ export default function SidebarWrapper({
                         </MenuItem>
                     ))}
                 </SubMenu>
-                <MenuItem icon={<IconCalendar size={18} />} component={<Link href="/calendar" />}>Calendar</MenuItem>
-                <MenuItem icon={<IconTextCaption size={18} />} component={<Link href="/docs" />}>Documentation</MenuItem>
-                <MenuItem icon={<IconUser size={18} />} component={<Link href="/profile" />}>Profile</MenuItem>
+                <MenuItem 
+                    icon={<IconCalendar size={18} />} 
+                    component={<Link href="/calendar" />}
+                    active={isActive("/calendar")}
+                >
+                    Calendar
+                </MenuItem>
+                <MenuItem 
+                    icon={<IconTextCaption size={18} />} 
+                    component={<Link href="/docs" />}
+                    active={isActive("/docs")}
+                >
+                    Documentation
+                </MenuItem>
+                <MenuItem 
+                    icon={<IconUser size={18} />} 
+                    component={<Link href="/profile" />}
+                    active={isActive("/profile")}
+                >
+                    Profile
+                </MenuItem>
             </Menu>
         </Sidebar>
     )
