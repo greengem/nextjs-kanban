5372ad817cfc7047b595e1b8c510b1f6942d3801
Green Gem <chris_waitt@me.com>
2024-01-03 18:06:55 +0000

    New Sidebar menu

---
+++ ui/Sidebar/SidebarMenu.tsx
@@ -0,0 +1,22 @@
+import { BoardSummary } from "@/types/types";
+import { IconCalendar, IconChevronDown, IconLayoutKanban, IconTextCaption, IconUser } from "@tabler/icons-react";
+import Link from "next/link";
+import { Sidebar, Menu, SubMenu, MenuItem } from "./SidebarComponent";
+
+export default function SidebarMenu({ boards } : { boards: BoardSummary[]}) {
+    return (
+        <Sidebar>
+            <Menu>
+                <SubMenu title="Boards" icon={<IconLayoutKanban size={18} />}>
+                    <MenuItem path="/board" title="All Boards" />
+                    {boards.map((board) => (
+                        <MenuItem key={board.id} path={`/board/${board.id}`} title={board.title} />
+                    ))}
+                </SubMenu>
+                <MenuItem path="/calendar" title="Calendar" icon={<IconCalendar size={18} />} />
+                <MenuItem path="/docs" title="Docs" icon={<IconTextCaption size={18} />} />
+                <MenuItem path="/profile" title="Profile" icon={<IconUser size={18} />} />
+            </Menu>
+        </Sidebar>
+    )
+}
