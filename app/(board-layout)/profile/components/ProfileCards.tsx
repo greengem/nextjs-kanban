226a71be41a4b8bc865bdc861f84f693142f53c9
Green Gem <chris_waitt@me.com>
2024-01-03 11:45:15 +0000

    Adding bg-white to card body

--- app/(board-layout)/profile/components/ProfileCards.tsx
+++ app/(board-layout)/profile/components/ProfileCards.tsx
@@ -26,7 +26,7 @@ export default function ProfileCards() {
         <div className="grid grid-cols-3 gap-5 mb-10">
             {cardData.map((card, index) => (
                 <Card key={index} className="w-full">
-                    <CardBody className="text-center space-y-2 bg-primary text-white p-5">
+                    <CardBody className="text-center space-y-2 bg-white p-5">
                         <div className="flex justify-center">{card.icon}</div>
                         <div className="font-semibold text-xl">{card.count}</div>
                         <div>{card.label}</div>
