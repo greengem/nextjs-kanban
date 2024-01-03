6e900dd89866cb2bbb645cddd7f2900d82924d00
Green Gem <chris_waitt@me.com>
2024-01-03 11:47:11 +0000

    List favorite boards in profile

---
+++ app/(board-layout)/profile/components/ProfileBoards.tsx
@@ -0,0 +1,23 @@
+import { getFavoriteBoards } from "@/lib/FetchData"
+import { Card, CardBody } from "@/ui/Card/Card";
+import Link from "next/link";
+
+export default async function ProfileBoards() {
+    const favBoards = await getFavoriteBoards();
+    return (
+        <>
+            <h2 className="text-large font-semibold mb-3">Favorite Boards</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 mb-10">
+                {favBoards.map((board) => (
+                    <Link key={board.id} href={`/board/${board.id}`}>
+                        <Card>
+                            <CardBody className="h-28 flex flex-col justify-end relative bg-white hover:bg-zinc-300">
+                                <span className="whitespace-nowrap overflow-ellipsis block overflow-x-hidden">{board.title}</span>
+                            </CardBody>
+                        </Card>
+                    </Link>
+                ))}
+            </div>
+        </>
+    )
+}
