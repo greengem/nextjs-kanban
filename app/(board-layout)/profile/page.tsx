035886ff22d11e5da84550185ca3d18d40320aa7
Green Gem <chris_waitt@me.com>
2024-01-03 11:20:54 +0000

    Updated profile page to use new components

--- app/(board-layout)/profile/page.tsx
+++ app/(board-layout)/profile/page.tsx
@@ -1,7 +1,22 @@
 import { auth } from "@/auth";
+import { Suspense } from "react";
+import ProfileHeader from "./components/ProfileHeader";
+import ProfileCards from "./components/ProfileCards";
+import ProfileActivity from "./components/ProfileActivity";
+
 export default async function ProfilePage() {
     const session = await auth();
-    return(
-        <pre>{JSON.stringify(session, null, 2)}</pre>
-    )
+
+    const userName = session?.user?.name ? session.user.name : 'Default Name';
+    const userImage = session?.user?.image ? session.user.image : 'default-image-url.jpg';
+
+    return (
+        <main className="grow p-5">
+            <ProfileHeader userName={userName} userImage={userImage} />
+            <ProfileCards />
+            <Suspense fallback={<div>Loading Activity...</div>}>
+                <ProfileActivity />
+            </Suspense>
+        </main>
+    );
 }
