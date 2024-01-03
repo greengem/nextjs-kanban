6d2fa89d6f57b3309f13bf318385b5e1fb0f32fa
Green Gem <chris_waitt@me.com>
2024-01-03 11:46:26 +0000

    Updated signout button to use UI button

--- ui/AuthButtons.tsx
+++ ui/AuthButtons.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { signIn, signOut } from "next-auth/react";
+import { Button } from "@nextui-org/button";
 
 export function SignInButton() {
     return <button onClick={() => signIn()}>Sign In</button>;
 }
 
 export function SignOutButton() {
-    return <button onClick={() => signOut({ callbackUrl: '/'})} className="text-sm">Sign Out</button>;
+    return <Button onClick={() => signOut({ callbackUrl: '/'})}>Sign Out</Button>;
 }
