46989ad53b9725ff5a349b80e77b286bbe25e625
Green Gem <chris_waitt@me.com>
2024-01-03 14:47:12 +0000

    Set breakpoints and context for the sidebar wrapepr

--- ui/Sidebar/SidebarWrapper.tsx
+++ ui/Sidebar/SidebarWrapper.tsx
@@ -1,16 +1,23 @@
 'use client'
+import { useSidebar } from "@/contexts/SidebarContext";
 import { BoardSummary } from "@/types/types";
-import { IconCalendar, IconLayoutKanban, IconTag, IconTextCaption, IconUser } from "@tabler/icons-react";
+import { IconCalendar, IconLayoutKanban, IconTextCaption, IconUser } from "@tabler/icons-react";
 import Link from "next/link";
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, SubMenu } from '@ascendtis/react-pro-sidebar';
 
 export default function SidebarWrapper({
     boards
 } : {
     boards: BoardSummary[]
 }) {
+    const { toggled, toggleSidebar } = useSidebar();
+
     return (
-        <Sidebar>
+        <Sidebar 
+            onBackdropClick={() => toggleSidebar()}
+            toggled={toggled} breakPoint="lg"
+            backgroundColor="#ffffff"
+        >
             <Menu>
                 <SubMenu 
                     label="Boards" 
@@ -30,11 +37,8 @@ export default function SidebarWrapper({
                         </MenuItem>
                     ))}
                 </SubMenu>
-                <SubMenu label="Labels" icon={<IconTag size={18} />}>
-                    <MenuItem>Labels</MenuItem>
-                </SubMenu>
-                <MenuItem icon={<IconTextCaption size={18} />}>Documentation</MenuItem>
-                <MenuItem icon={<IconCalendar size={18} />}>Calendar</MenuItem>
+                <MenuItem icon={<IconCalendar size={18} />} component={<Link href="/calendar" />}>Calendar</MenuItem>
+                <MenuItem icon={<IconTextCaption size={18} />} component={<Link href="/docs" />}>Documentation</MenuItem>
                 <MenuItem icon={<IconUser size={18} />} component={<Link href="/profile" />}>Profile</MenuItem>
             </Menu>
         </Sidebar>
