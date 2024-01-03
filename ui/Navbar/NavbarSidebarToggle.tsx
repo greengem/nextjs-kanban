951d37dec8ddb645e21170db864b34419fdb3be7
Green Gem <chris_waitt@me.com>
2024-01-03 14:46:30 +0000

    Toggle the sidebar from the navbar with a hamburger

---
+++ ui/Navbar/NavbarSidebarToggle.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { IconMenu2 } from "@tabler/icons-react";
+import { useSidebar } from "@/contexts/SidebarContext";
+
+export default function NavbarSidebarToggle() {
+    const { toggleSidebar } = useSidebar();
+
+    return (
+        <button className="mr-3" onClick={toggleSidebar}>
+            <IconMenu2 size={24} />
+        </button>
+    );
+}
