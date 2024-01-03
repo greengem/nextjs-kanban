9154c486ed39aa1134feae7688add8a58da8ab39
Green Gem <chris_waitt@me.com>
2024-01-03 14:43:48 +0000

    New sidebar context to handle toggling the sidebar on small breakpoints

--- contexts/SidebarContext.tsx
+++ contexts/SidebarContext.tsx
@@ -1,42 +1,32 @@
-import React, { useState, createContext, useContext, ReactNode } from 'react';
+import { createContext, useState, useContext, ReactNode, useEffect } from 'react';
 
 interface SidebarContextType {
-  isSidebarVisible: boolean;
-  isSlimSidebar: boolean;
-  toggleSidebar: () => void;
-  toggleSlimSidebar: () => void;
+    toggled: boolean;
+    toggleSidebar: () => void;
 }
 
-const SidebarVisibilityContext = createContext<SidebarContextType>({
-  isSidebarVisible: false,
-  isSlimSidebar: true,
-  toggleSidebar: () => {},
-  toggleSlimSidebar: () => {},
-});
-
-interface SidebarProviderProps {
-  children: ReactNode;
-}
-
-export const SidebarProvider: React.FC<SidebarProviderProps> = ({ children }) => {
-  const [isSidebarVisible, setSidebarVisible] = useState(false);
-  const [isSlimSidebar, setSlimSidebar] = useState(true);
-
-  const toggleSidebar = () => {
-    setSidebarVisible(!isSidebarVisible);
-  };
-
-  const toggleSlimSidebar = () => {
-    setSlimSidebar(!isSlimSidebar);
-  };
-
-  return (
-    <SidebarVisibilityContext.Provider value={{ isSidebarVisible, isSlimSidebar, toggleSidebar, toggleSlimSidebar }}>
-      {children}
-    </SidebarVisibilityContext.Provider>
-  );
+const defaultValue: SidebarContextType = {
+    toggled: false,
+    toggleSidebar: () => {},
 };
 
-export const useSidebarVisibility = (): SidebarContextType => {
-  return useContext(SidebarVisibilityContext);
+const SidebarContext = createContext<SidebarContextType>(defaultValue);
+
+export const useSidebar = () => useContext(SidebarContext);
+
+export const SidebarProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [toggled, setToggled] = useState(false);
+
+    const toggleSidebar = () => {
+        setToggled(!toggled);
+    };
+
+    useEffect(() => {
+    }, [toggled]);
+
+    return (
+        <SidebarContext.Provider value={{ toggled, toggleSidebar }}>
+            {children}
+        </SidebarContext.Provider>
+    );
 };
