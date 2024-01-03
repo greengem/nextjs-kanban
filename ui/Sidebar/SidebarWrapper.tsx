782036e5d9411323d7f824e90551f12d662525c6
Green Gem <chris_waitt@me.com>
2024-01-03 18:06:35 +0000

    Updated sidebar context

--- ui/Sidebar/SidebarWrapper.tsx
+++ ui/Sidebar/SidebarWrapper.tsx
@@ -1,82 +1,20 @@
 'use client'
 import { useSidebar } from "@/contexts/SidebarContext";
-import { usePathname } from 'next/navigation';
+import Drawer from 'react-modern-drawer'
 import { BoardSummary } from "@/types/types";
-import { IconCalendar, IconLayoutKanban, IconTextCaption, IconUser } from "@tabler/icons-react";
-import Link from "next/link";
-import { Sidebar, Menu, MenuItem, SubMenu } from '@ascendtis/react-pro-sidebar';
+import 'react-modern-drawer/dist/index.css'
+import SidebarMenu from "./SidebarMenu";
 
-export default function SidebarWrapper({
-    boards
-} : {
-    boards: BoardSummary[]
-}) {
-    const pathname = usePathname();
-    const { toggled, toggleSidebar } = useSidebar();
-    
-    // Define the BoardItemSuffix component here or import it if defined elsewhere
-    const BoardItemSuffix = ({ tasksCount }: { tasksCount: number }) => (
-        <div className="bg-primary text-xs rounded-full h-6 w-6 flex items-center justify-center text-white">
-            {tasksCount}
-        </div>
-    );
-
-    // Function to determine if the link is active
-    const isActive = (linkPath: string) => pathname === linkPath;
-
-    const activeStyles = {
-        backgroundColor: '#eaabff',
-        color: '#9f0099',
-        '&:hover': {
-          backgroundColor: '#eecef9',
-        },
-    };
+export default function SidebarWrapper({ boards } : { boards: BoardSummary[]}) {
+    const { isOpen, toggleDrawer } = useSidebar();
 
     return (
-        <Sidebar 
-            onBackdropClick={() => toggleSidebar()}
-            toggled={toggled} breakPoint="lg"
-            backgroundColor="#ffffff"
+        <Drawer
+            open={isOpen}
+            onClose={toggleDrawer}
+            direction='left'
         >
-            <Menu>
-                <SubMenu 
-                    label="Boards" 
-                    icon={<IconLayoutKanban size={18} />} 
-                    defaultOpen
-                >
-                    {boards.map((board) => (
-                        <MenuItem 
-                            key={board.id}
-                            active={isActive(`/board/${board.id}`)}
-                            component={<Link href={`/board/${board.id}`} />}
-                            suffix={<BoardItemSuffix tasksCount={board.tasksCount} />}
-                        >
-                                {board.title}
-                        </MenuItem>
-                    ))}
-                </SubMenu>
-                <MenuItem 
-                    icon={<IconCalendar size={18} />} 
-                    component={<Link href="/calendar" />}
-                    active={isActive("/calendar")}
-                >
-                    Calendar
-                </MenuItem>
-                <MenuItem 
-                    icon={<IconTextCaption size={18} />} 
-                    component={<Link href="/docs" />}
-                    active={isActive("/docs")}
-                >
-                    Documentation
-                </MenuItem>
-                <MenuItem 
-                    icon={<IconUser size={18} />} 
-                    component={<Link href="/profile" />}
-                    active={isActive("/profile")}
-                >
-                    Profile
-                </MenuItem>
-            </Menu>
-        </Sidebar>
+            <SidebarMenu boards={boards} />
+        </Drawer>
     )
 }
