ecf6e427b0dcf40fd449ca8d3d30c16f9e6ba87a
Green Gem <chris_waitt@me.com>
2024-01-03 09:41:10 +0000

    Updated skseleton with new flex layout

--- ui/LoadingSkeleton.tsx
+++ ui/LoadingSkeleton.tsx
@@ -2,7 +2,7 @@ import { IconLoader2 } from "@tabler/icons-react";
 
 export default function LoadingSkeleton() {
     return(
-        <div className="flex justify-center pt-20">
+        <div className="flex flex-col grow min-w-0 justify-center pt-20">
             <IconLoader2 size={32} className="animate-spin mr-2 text-primary" />
         </div>
     )
