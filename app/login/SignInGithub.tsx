26c422c582727a4dd56bf97cdff07bcc5b4f5180
Green Gem <chris_waitt@me.com>
2024-01-05 17:24:35 +0000

    custom button for github login

---
+++ app/login/SignInGithub.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { signIn } from "next-auth/react";
+import { Button } from "@nextui-org/react";
+import { IconBrandGithub } from "@tabler/icons-react";
+
+const SignInGithub = () => {
+  return (
+    <Button
+      onClick={() => signIn("github", { callbackUrl: "/board" })}
+      type="button"
+      variant="ghost"
+    >
+      <IconBrandGithub />Sign In With GitHub
+    </Button>
+  );
+};
+
+export default SignInGithub;
