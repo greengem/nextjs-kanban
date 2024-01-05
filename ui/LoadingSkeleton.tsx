2c000b5c74014de6e550ac7c9baeec18f3986eb9
Green Gem <chris_waitt@me.com>
2024-01-05 17:25:09 +0000

    Loading skeleton now correctly aligned

--- ui/LoadingSkeleton.tsx
+++ ui/LoadingSkeleton.tsx
@@ -2,8 +2,8 @@ import { IconLoader2 } from "@tabler/icons-react";
 
 export default function LoadingSkeleton() {
     return(
-        <div className="flex flex-col grow min-w-0 justify-center pt-20">
-            <IconLoader2 size={32} className="animate-spin mr-2 text-primary" />
-        </div>
+        <main className="flex flex-col grow p-5 justify-center items-center bg-zinc-200">
+            <IconLoader2 size={64} className="animate-spin text-primary" />
+        </main>
     )
 }
