af0169d0d47c57a4a1ef6816ac7cba6019b865a0
Green Gem <chris_waitt@me.com>
2024-01-06 15:35:58 +0000

    Moved getTask fetch and refactored get Boards to use our new multi user board schema

--- lib/FetchData.tsx
+++ lib/FetchData.tsx
@@ -2,7 +2,6 @@ import prisma from '@/db/prisma';
 import { auth } from "@/auth";
 import { BoardSummary } from '@/types/types';
 
-
 export async function getBoardsSummary(): Promise<BoardSummary[]> {
     const session = await auth();
     const userId = session?.user?.id;
@@ -10,132 +9,46 @@ export async function getBoardsSummary(): Promise<BoardSummary[]> {
         return [];
     }
 
-    const boards = await prisma.board.findMany({
+    const boardMemberships = await prisma.boardMember.findMany({
         where: {
-            userId: userId
+            userId: userId,
         },
-        select: {
-            id: true,
-            title: true,
-            backgroundUrl: true,
-            columns: {
+        include: {
+            board: {
                 select: {
-                    tasks: {
-                        select: { id: true },
-                    }
+                    id: true,
+                    title: true,
+                    backgroundUrl: true,
+                    columns: {
+                        select: {
+                            tasks: {
+                                select: { id: true },
+                            }
+                        }
+                    },
+                    favoritedBy: {
+                        where: {
+                            userId: userId,
+                        },
+                        select: {
+                            userId: true,
+                        },
+                    },
                 }
-            },
-            favoritedBy: {
-                where: {
-                    userId: userId,
-                },
-                select: {
-                    userId: true,
-                },
-            },
+            }
         },
         orderBy: {
             createdAt: 'asc',
         }
     });
 
-    return boards.map(board => ({
+    return boardMemberships.map(({ board }) => ({
         ...board,
         tasksCount: board.columns.reduce((sum, column) => sum + column.tasks.length, 0),
         isFavorited: board.favoritedBy.length > 0
     }));
 }
 
-
-export async function getTask(taskId: string) {
-    const task = await prisma.task.findUnique({
-        where: {
-            id: taskId
-        },
-        select: {
-            id: true,
-            title: true,
-            description: true,
-            dueDate: true,
-            startDate: true,
-            createdAt: true,
-            updatedAt: true,
-            order: true,
-            columnId: true,
-            column: {
-                select: {
-                    title: true,
-                    boardId: true,
-                    board: {
-                        select: {
-                            backgroundUrl: true,
-                        }
-                    }
-                },
-            },
-            labels: {
-                select: {
-                    id: true,
-                    title: true,
-                    color: true
-                }
-            },
-            checklists: {
-                select: {
-                    id: true,
-                    title: true,
-                    items: {
-                        orderBy: {
-                            createdAt: 'asc'
-                        },
-                        select: {
-                            id: true,
-                            content: true,
-                            isChecked: true,
-                            createdAt: true
-                        }
-                    }
-                }
-            },
-            activities: {
-                orderBy: {
-                    createdAt: 'desc'
-                },
-                select: {
-                    id: true,
-                    type: true,
-                    content: true,
-                    createdAt: true,
-                    startDate: true,
-                    dueDate: true,
-                    oldColumn: {
-                        select: { title: true },
-                    },
-                    newColumn: {
-                        select: { title: true },
-                    },
-                    originalColumn: {
-                        select: { title: true },
-                    },
-                    task: {
-                        select: {
-                          title: true,
-                        },
-                      },
-                    user: {
-                        select: {
-                            id: true,
-                            name: true,
-                            image: true,
-                        }
-                    },
-                }
-            }
-        }
-    });
-
-    return task;
-}
 
 
 // Fetch labels
