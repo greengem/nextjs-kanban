fabc29aa4df45aa557396a1a946b61e025959358
Green Gem <chris_waitt@me.com>
2024-01-06 11:18:28 +0000

    Removed redundant fetches to move them into suspense

--- app/(board-layout)/profile/components/TotalBoardCount.tsx
+++
@@ -1,5 +0,0 @@
-import { getBoardCount } from "@/lib/FetchData"
-export default async function TotalBoardCount() {
-    const boardCount = await getBoardCount();
-    return boardCount;
-}
--- app/(board-layout)/profile/components/TotalTaskCount.tsx
+++
@@ -1,5 +0,0 @@
-import { getTaskCount } from "@/lib/FetchData";
-export default async function TotalTaskCount() {
-    const taskCount = await getTaskCount();
-    return taskCount;
-}
