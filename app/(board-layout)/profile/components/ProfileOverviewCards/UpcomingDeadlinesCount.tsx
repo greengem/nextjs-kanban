3343d3e4807e66d6676a5cc5042000597c52bdb6
Green Gem <chris_waitt@me.com>
2024-01-06 11:20:10 +0000

    Function to return the Upcoming task deadline count

---
+++ app/(board-layout)/profile/components/ProfileOverviewCards/UpcomingDeadlinesCount.tsx
@@ -0,0 +1,31 @@
+import { auth } from "@/auth";
+import prisma from '@/db/prisma';
+
+export default async function UpcomingDeadlinesCount() {
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if (!userId) {
+        throw new Error("User not authenticated");
+    }
+
+    const today = new Date();
+    const sevenDaysFromNow = new Date(today);
+    sevenDaysFromNow.setDate(sevenDaysFromNow.getDate() + 7);
+
+    const count = await prisma.task.count({
+        where: {
+            column: {
+                board: {
+                    userId: userId,
+                },
+            },
+            dueDate: {
+                gte: today,
+                lt: sevenDaysFromNow
+            }
+        }
+    });
+
+    return count;
+}
