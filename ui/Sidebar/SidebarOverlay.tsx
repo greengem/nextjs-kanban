2d50afb09fb3f12e2c5bc709f88831b2c7be381a
Green Gem <chris_waitt@me.com>
2024-01-06 11:21:59 +0000

    Hide sidebar overlay on lg+ breakpoint so we dont have to access the js window

--- ui/Sidebar/SidebarOverlay.tsx
+++ ui/Sidebar/SidebarOverlay.tsx
@@ -6,6 +6,6 @@ export default function SidebarOverlay({
     if (!isVisible) return null;
 
     return (
-        <div onClick={onClick} className="fixed top-0 left-0 right-0 bottom-0 bg-black/70 z-40" />
+        <div onClick={onClick} className="fixed top-0 left-0 right-0 bottom-0 bg-black/70 z-40 lg:hidden" />
     );
 };
