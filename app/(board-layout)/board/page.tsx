f71b158c0ec216381734b4c6f37fc51c3222f242
Green Gem <chris_waitt@me.com>
2024-01-06 13:11:42 +0000

    Modify board listing grid

--- app/(board-layout)/board/page.tsx
+++ app/(board-layout)/board/page.tsx
@@ -8,7 +8,7 @@ export default function Boards() {
   return (
     <main className="flex flex-col grow min-w-0 p-5 bg-zinc-200">
       <PageHeading title='Boards' />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         <Suspense fallback={<p>Loading Boards</p>}>
           <FetchBoards />
         </Suspense>
