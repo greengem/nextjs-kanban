8df7e542f74ea720ec6879ea5e572bc4e979b3d9
Green Gem <chris_waitt@me.com>
2024-01-06 11:20:31 +0000

    Function to return the total board count

---
+++ app/(board-layout)/profile/components/ProfileOverviewCards/TotalBoardsCount.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@/auth";
+import prisma from '@/db/prisma';
+
+export default async function TotalBoardsCount() {
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if (!userId) {
+        throw new Error("User not authenticated");
+    }
+
+    const totalBoards = await prisma.board.count({
+        where: {
+            userId: userId,
+        },
+    });
+
+    return totalBoards;
+}
