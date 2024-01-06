eb460d801dae8282231525c34f8e3e9889534e9c
Green Gem <chris_waitt@me.com>
2024-01-06 11:20:21 +0000

    Function to return the total task count

---
+++ app/(board-layout)/profile/components/ProfileOverviewCards/TotalTasksCount.tsx
@@ -0,0 +1,23 @@
+import { auth } from "@/auth";
+import prisma from '@/db/prisma';
+
+export default async function TotalTasksCount() {
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if (!userId) {
+        throw new Error("User not authenticated");
+    }
+
+    const totalTasks = await prisma.task.count({
+        where: {
+            column: {
+                board: {
+                    userId: userId,
+                },
+            },
+        },
+    });
+
+    return totalTasks;
+}
