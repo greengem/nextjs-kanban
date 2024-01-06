6e140e92617f6f5d1b973abf9131de7eb6e1aa17
Green Gem <chris_waitt@me.com>
2024-01-06 11:21:34 +0000

    Finally assemble to profile page with new suspense boundaries and fallbacks

--- app/(board-layout)/profile/page.tsx
+++ app/(board-layout)/profile/page.tsx
@@ -12,13 +12,28 @@ export default function ProfilePage() {
             <Suspense fallback={<div>Loading User Data...</div>}>
                 <ProfileHeader />
             </Suspense>
+
             <ProfileCards />
-            <Suspense fallback={<div>Loading Activity...</div>}>
-                <ProfileActivity />
-            </Suspense>
-            <Suspense fallback={<div>Loading Favorite Boards...</div>}>
-                <ProfileBoards />
-            </Suspense>
+
+            <h2 className="text-large font-semibold mb-3">Recent Activity</h2>
+            <ul className="mb-10">
+                <Suspense fallback={<li className="border-b-1 last:border-b-0 border-zinc-300 py-1">Loading activity...</li>}>
+                    <ProfileActivity />
+                </Suspense>
+            </ul>
+
+            <h2 className="text-large font-semibold mb-3">Favorite Boards</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 mb-10">
+                <Suspense fallback={
+                    <div className="h-28 flex flex-col justify-end relative bg-white hover:bg-zinc-300 border-2 border-primary rounded-xl p-2">
+                        <span className="whitespace-nowrap overflow-ellipsis block overflow-x-hidden">Loading boards...</span>
+                    </div>
+                }>
+                    <ProfileBoards />
+                </Suspense>
+            </div>
+
+            
             <ProfileActions />
         </main>
     );
