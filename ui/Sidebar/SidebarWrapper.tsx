48fa46da77d13445651ddc649cb19ff865f93f2e
Green Gem <chris_waitt@me.com>
2024-01-06 10:05:59 +0000

    Add overlay to sidebar wrapepr

--- ui/Sidebar/SidebarWrapper.tsx
+++ ui/Sidebar/SidebarWrapper.tsx
@@ -2,11 +2,14 @@
 import { useSidebar } from "@/contexts/SidebarContext";
 import { BoardSummary } from "@/types/types";
 import SidebarMenu from "./SidebarMenu";
+import SidebarOverlay from "./SidebarOverlay";
 
 export default function SidebarWrapper({ boards } : { boards: BoardSummary[]}) {
     const { isOpen, toggleDrawer } = useSidebar();
-
     return (
-        <SidebarMenu boards={boards} />
+        <>
+            <SidebarOverlay isVisible={isOpen} onClick={toggleDrawer} />
+            <SidebarMenu boards={boards} />
+        </>
     )
 }
