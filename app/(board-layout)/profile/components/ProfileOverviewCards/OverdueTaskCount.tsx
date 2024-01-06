031c7155bdfb1c23f39550efbd74a30d747446cb
Green Gem <chris_waitt@me.com>
2024-01-06 11:19:52 +0000

    Function to return the Overdue task count

---
+++ app/(board-layout)/profile/components/ProfileOverviewCards/OverdueTaskCount.tsx
@@ -0,0 +1,28 @@
+import { auth } from "@/auth";
+import prisma from '@/db/prisma';
+
+export default async function OverdueTaskCount() {
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if (!userId) {
+        throw new Error("User not authenticated");
+    }
+
+    const today = new Date();
+
+    const count = await prisma.task.count({
+        where: {
+            column: {
+                board: {
+                    userId: userId,
+                },
+            },
+            dueDate: {
+                lt: today
+            }
+        }
+    });
+
+    return count;
+}
