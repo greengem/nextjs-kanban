7fd3921906dcb6f54d1e5c41532bf5686f78d538
Green Gem <chris_waitt@me.com>
2024-01-07 12:42:53 +0000

    App Name change

--- ui/Navbar/NavbarTitle.tsx
+++ ui/Navbar/NavbarTitle.tsx
@@ -4,11 +4,9 @@ export default function NavbarTitle() {
 
     return (
         <div className="flex items-center my-2 gap-5">
-
             <Link href='/board/' className="text-xl tracking-tight">
-                Next.Kanban <span className="text-primary">Alpha</span>
+                NextBoard <span className="text-primary">Alpha</span>
             </Link>
-
         </div>
     )
 }
