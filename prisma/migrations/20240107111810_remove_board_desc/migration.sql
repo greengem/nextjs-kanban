d634dfbf15987d69d5050f7204cc0b84135faec8
Green Gem <chris_waitt@me.com>
2024-01-07 11:30:01 +0000

    removed description from board

---
+++ prisma/migrations/20240107111810_remove_board_desc/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Board` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Board" DROP COLUMN "description";
