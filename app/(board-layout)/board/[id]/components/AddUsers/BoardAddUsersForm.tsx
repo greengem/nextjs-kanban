199d54e90e28cb6121ffb1f4bcf6ea4d31a5f3b6
Green Gem <chris_waitt@me.com>
2024-01-08 12:29:55 +0000

    only show invite form if user is the board owner

--- app/(board-layout)/board/[id]/components/AddUsers/BoardAddUsersForm.tsx
+++ app/(board-layout)/board/[id]/components/AddUsers/BoardAddUsersForm.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import { Button } from "@nextui-org/button"
-import { Input } from "@nextui-org/input"
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
 import toast from 'react-hot-toast';
 import { handleSendBoardInvitation } from "@/actions/InvitationActions";
 
-export default function BoardAddUsersForm({ boardId, onInvitationLinkChange }: { boardId: string, onInvitationLinkChange: (link: string) => void }) {
+export default function BoardAddUsersForm({ boardId, isOwner, onInvitationLinkChange }: { boardId: string, isOwner: boolean, onInvitationLinkChange: (link: string) => void }) {
     const [email, setEmail] = useState('');
 
     const handleInvite = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -25,18 +25,22 @@ export default function BoardAddUsersForm({ boardId, onInvitationLinkChange }: {
         }
     };
 
-    return (
-        <form className="space-y-2" onSubmit={handleInvite}>
-            <Input 
-                type="email" 
-                variant='bordered'
-                size="sm" 
-                label="Email" 
-                placeholder="Invite by email address" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-            />
-            <Button className='w-full' size="sm" type="submit" color='primary'>Send invite</Button>
-        </form>
-    )
+    if (isOwner) {
+        return (
+            <form className="space-y-2" onSubmit={handleInvite}>
+                <Input 
+                    type="email" 
+                    variant='bordered'
+                    size="sm" 
+                    label="Email" 
+                    placeholder="Invite by email address" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                />
+                <Button className='w-full' size="sm" type="submit" color='primary'>Send invite</Button>
+            </form>
+        );
+    } else {
+        return <p className='text-danger'>Only board owners can invite new users.</p>;
+    }
 }
