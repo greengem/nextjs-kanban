ae32c37defd61d0ab71e753528ab83e5017da2e6
Green Gem <chris_waitt@me.com>
2024-01-09 14:16:11 +0000

    Added flex-col and grow to new general layout

--- app/(default-layout)/layout.tsx
+++ app/(default-layout)/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <DashboardLayout>
-      <main className="p-3 md:p-5 grow bg-zinc-200">
+      <main className="p-3 md:p-5 flex flex-col grow bg-zinc-200">
         {children}
       </main>
     </DashboardLayout>
