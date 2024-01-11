7bc3ca8e3ee6aef0313db83b355726a501b4fea4
Green Gem <chris_waitt@me.com>
2024-01-11 11:54:36 +0000

    added rounded-xl to card paretn to remove shadow ghosting from the bottom of cards

--- ui/Card/Card.tsx
+++ ui/Card/Card.tsx
@@ -4,7 +4,7 @@ import { DraggableProvidedDragHandleProps } from '@hello-pangea/dnd';
 
 export function Card({ children, className = '' }: { children: ReactNode, className?: string }) {
     return (
-        <div className={`shadow-xl ${className}`}>
+        <div className={`shadow-xl rounded-xl ${className}`}>
             {children}
         </div>
     )
