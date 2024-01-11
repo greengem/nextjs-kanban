56e30673b230d7e4f94c90909d3b636962e8b3b4
Green Gem <chris_waitt@me.com>
2024-01-11 14:42:07 +0000

    Add delete account button to profile page

--- app/(default-layout)/profile/components/ProfileActions.tsx
+++ app/(default-layout)/profile/components/ProfileActions.tsx
@@ -1,4 +1,5 @@
-import { SignOutButton } from "@/ui/AuthButtons";
+import { SignOutButton } from "@/ui/AuthButtons.client";
+import ProfileDeleteAllData from "./ProfileDeleteAllData.client";
 
 export default function ProfileActions() {
     return (
@@ -6,6 +7,7 @@ export default function ProfileActions() {
             <h2 className="text-large font-semibold mb-3">Actions</h2>
             <div className="flex gap-3">
                 <SignOutButton />
+                <ProfileDeleteAllData />
             </div>
         </>
     )
