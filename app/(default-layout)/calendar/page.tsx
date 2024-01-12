f75e06417079adb9abd6920f62d55b54c265e0c6
Green Gem <chris_waitt@me.com>
2024-01-12 10:55:35 +0000

    renamed calendars route

--- app/(default-layout)/calendar/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CalendarRoute() {
-    return (
-        <div>
-            <h1 className="text-3xl font-semibold tracking-tight">Calendar</h1>
-            <p className="text-zinc-800 text-sm">View tasks with dates in calendar format - <span className="text-red-500">Coming Soon</span></p>
-        </div>
-    )
-}
