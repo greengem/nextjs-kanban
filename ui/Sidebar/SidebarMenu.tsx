8f38f8e7fa273bc4ac24847ccd0dfb16dcca265e
Green Gem <chris_waitt@me.com>
2024-01-12 10:56:33 +0000

    rearranged sidebar menu and added a dashboard submenu

--- ui/Sidebar/SidebarMenu.tsx
+++ ui/Sidebar/SidebarMenu.tsx
@@ -1,20 +1,22 @@
 import { BoardSummary } from "@/types/types";
-import { IconCalendar, IconChevronDown, IconLayoutKanban, IconTextCaption, IconUser } from "@tabler/icons-react";
+import { IconCalendar, IconDashboard, IconLayoutKanban, IconTextCaption, IconUser } from "@tabler/icons-react";
 import { Sidebar, Menu, SubMenu, MenuItem } from "./SidebarComponent";
 
 export default function SidebarMenu({ boards } : { boards: BoardSummary[]}) {
     return (
         <Sidebar>
             <Menu>
-                <SubMenu title="Boards" defaultOpen icon={<IconLayoutKanban size={18} />}>
+                <MenuItem path="/profile" title="Profile" icon={<IconUser size={18} />} />
+                <SubMenu title="Dashboard" icon={<IconDashboard size={18} />}>
                     <MenuItem path="/board" title="All Boards" />
+                </SubMenu>
+                <SubMenu title="Boards" defaultOpen icon={<IconLayoutKanban size={18} />}>
                     {boards.map((board) => (
                         <MenuItem key={board.id} path={`/board/${board.id}`} title={board.title} />
                     ))}
                 </SubMenu>
-                <MenuItem path="/calendar" title="Calendar" icon={<IconCalendar size={18} />} />
+                <MenuItem path="/calendars" title="Calendars" icon={<IconCalendar size={18} />} />
                 <MenuItem path="/docs" title="Docs" icon={<IconTextCaption size={18} />} />
-                <MenuItem path="/profile" title="Profile" icon={<IconUser size={18} />} />
             </Menu>
         </Sidebar>
     )
