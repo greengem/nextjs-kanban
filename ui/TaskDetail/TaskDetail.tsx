732fad0995f2fc259e1a8812acb003fcac42d95b
Green Gem <chris_waitt@me.com>
2024-01-13 11:48:29 +0000

    Used next Image component to render backgrounds more efficiently

--- ui/TaskDetail/TaskDetail.tsx
+++ ui/TaskDetail/TaskDetail.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { IconArrowLeft } from "@tabler/icons-react";
 import { Button } from "@nextui-org/button";
 import { Card, CardBody } from "../Card/Card";
+import Image from "next/image";
 
 export default async function TaskDetail({ taskId }: { taskId: string }) {
   const session = await auth();
@@ -26,94 +27,94 @@ export default async function TaskDetail({ taskId }: { taskId: string }) {
       }
     }
   });
-  
+
   if (!boardMembership) {
     return <div>Access denied: User is not authorized to view this task</div>;
   }
 
   const task = await prisma.task.findUnique({
     where: {
-        id: taskId
+      id: taskId
     },
     select: {
-        id: true,
-        title: true,
-        description: true,
-        dueDate: true,
-        startDate: true,
-        createdAt: true,
-        updatedAt: true,
-        order: true,
-        columnId: true,
-        column: {
+      id: true,
+      title: true,
+      description: true,
+      dueDate: true,
+      startDate: true,
+      createdAt: true,
+      updatedAt: true,
+      order: true,
+      columnId: true,
+      column: {
+        select: {
+          title: true,
+          boardId: true,
+          board: {
             select: {
-                title: true,
-                boardId: true,
-                board: {
-                    select: {
-                        backgroundUrl: true,
-                    }
-                }
-            },
-        },
-        labels: {
-            select: {
-                id: true,
-                title: true,
-                color: true
+              backgroundUrl: true,
             }
+          }
         },
-        checklists: {
-            select: {
-                id: true,
-                title: true,
-                items: {
-                    orderBy: {
-                        createdAt: 'asc'
-                    },
-                    select: {
-                        id: true,
-                        content: true,
-                        isChecked: true,
-                        createdAt: true
-                    }
-                }
-            }
-        },
-        activities: {
-            orderBy: {
-                createdAt: 'desc'
-            },
-            select: {
-                id: true,
-                type: true,
-                content: true,
-                createdAt: true,
-                startDate: true,
-                dueDate: true,
-                oldColumn: {
-                    select: { title: true },
-                },
-                newColumn: {
-                    select: { title: true },
-                },
-                originalColumn: {
-                    select: { title: true },
-                },
-                task: {
-                    select: {
-                      title: true,
-                    },
-                  },
-                user: {
-                    select: {
-                        id: true,
-                        name: true,
-                        image: true,
-                    }
-                },
-            }
+      },
+      labels: {
+        select: {
+          id: true,
+          title: true,
+          color: true
         }
+      },
+      checklists: {
+        select: {
+          id: true,
+          title: true,
+          items: {
+            orderBy: {
+              createdAt: 'asc'
+            },
+            select: {
+              id: true,
+              content: true,
+              isChecked: true,
+              createdAt: true
+            }
+          }
+        }
+      },
+      activities: {
+        orderBy: {
+          createdAt: 'desc'
+        },
+        select: {
+          id: true,
+          type: true,
+          content: true,
+          createdAt: true,
+          startDate: true,
+          dueDate: true,
+          oldColumn: {
+            select: { title: true },
+          },
+          newColumn: {
+            select: { title: true },
+          },
+          originalColumn: {
+            select: { title: true },
+          },
+          task: {
+            select: {
+              title: true,
+            },
+          },
+          user: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            }
+          },
+        }
+      }
     }
   });
 
@@ -121,32 +122,32 @@ export default async function TaskDetail({ taskId }: { taskId: string }) {
     return <div>Task not found or loading error</div>;
   }
 
-  let backgroundStyle = {};
-
-  if (task.column && task.column.board && task.column.board.backgroundUrl) {
-    backgroundStyle = {
-      backgroundImage: `url(${task.column.board.backgroundUrl})`,
-    };
-  }
-
   return (
-    <div
-      className={`p-3 md:p-5 flex flex-col grow`}
-      style={backgroundStyle}
-    >
-      <div className="flex-none mb-3 md:mb-5">
-              <Button
-                as={Link}
-                className="inline-flex"
-                href={`/board/${task.column?.boardId}`}
-                size="sm"
-                startContent={<IconArrowLeft size={18} />}
-              >
+    <div className='p-3 md:p-5 flex flex-col grow relative'>
+      {task.column.board.backgroundUrl && (
+        <Image 
+          className='z-0'
+          src={task.column.board.backgroundUrl} 
+          alt='Board Wallpaper' 
+          layout='fill'
+          objectFit='cover'
+          objectPosition='center'
+        />
+      )}
+
+      <div className="flex-none mb-3 md:mb-5 z-10">
+        <Button
+          as={Link}
+          className="inline-flex"
+          href={`/board/${task.column?.boardId}`}
+          size="sm"
+          startContent={<IconArrowLeft size={18} />}
+        >
           Back to board
         </Button>
-        </div>
+      </div>
 
-      <Card>
+      <Card className="z-10">
         <CardBody className="bg-white">
           <TaskDetailTitle selectedTask={task} boardId={task?.column.boardId} />
           <div className="grid grid-cols-1 md:grid-cols-4 p-3 md:p-5 gap-x-5">
@@ -156,6 +157,5 @@ export default async function TaskDetail({ taskId }: { taskId: string }) {
         </CardBody>
       </Card>
     </div>
-    
   );
 }
