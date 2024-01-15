4f0e929ed2fe56844709d2881cd233bff7d994b5
Green Gem <chris_waitt@me.com>
2024-01-15 16:24:36 +0000

    Updated checklist server actions for new structure

--- actions/ChecklistServerActions.tsx
+++ actions/ChecklistServerActions.tsx
@@ -1,8 +1,9 @@
 'use server';
 import prisma from '@/db/prisma';
 import { revalidatePath } from 'next/cache';
+import { z } from 'zod'
 
-export async function handleDeleteChecklist({ checklistId, boardId }: { checklistId: string, boardId: string }) {
+export async function handleDeleteChecklist({ checklistId, taskId }: { checklistId: string, taskId: string }) {
 
     if (!checklistId) {
         return { success: false, message: 'Checklist ID is missing' };
@@ -15,7 +16,7 @@ export async function handleDeleteChecklist({ checklistId, boardId }: { checklis
             }
         });
 
-        revalidatePath(`/board/${boardId}`);
+        revalidatePath(`/task/${taskId}`);
         return { success: true, message: 'Deleted checklist' };
     } catch (e) {
         return { success: false, message: 'Failed to delete checklist', error: e };
@@ -46,24 +47,39 @@ export async function handleCreateChecklist({ title, taskId, boardId }: { title?
 }
 
 
-export async function handleCreateChecklistItem({ content, checklistId, boardId }: { content: string, checklistId: string, boardId: string }) {
-    if (!checklistId) {
-        return { success: false, message: 'Checklist ID is missing' };
-    }
-    if (!content) {
-        return { success: false, message: 'Content is missing' };
+// Create a new checklist item
+const handleCreateChecklistItemSchema = z.object({
+    content: z.string().trim().min(1).max(300),
+    checklistId: z.string(),
+    taskId: z.string(),
+});
+
+export async function handleCreateChecklistItem(formData: FormData) {
+    const validatedFields = handleCreateChecklistItemSchema.safeParse({
+        content: formData.get('content')?.toString(),
+        checklistId: formData.get('checklistId')?.toString(),
+        taskId: formData.get('taskId')?.toString(),
+    });
+
+    if (!validatedFields.success) {
+        const errorMessage = Object.values(validatedFields.error.flatten().fieldErrors).map(e => e.join(', ')).join('; ');
+    
+        return {
+            success: false,
+            message: `Validation failed: ${errorMessage}`,
+        };
     }
 
     try {
         await prisma.checklistItem.create({
             data: {
-                content: content,
+                content: validatedFields.data.content,
                 isChecked: false,
-                checklistId: checklistId,
+                checklistId: validatedFields.data.checklistId,
             },
         });
 
-        revalidatePath(`/board/${boardId}`);
+        revalidatePath(`/task/${validatedFields.data.taskId}`);
 
         return { success: true, message: 'Item added to checklist' };
     } catch (e) {
@@ -72,44 +88,62 @@ export async function handleCreateChecklistItem({ content, checklistId, boardId 
 }
 
 
-export async function handleDeleteChecklistItem({ checklistItemId, boardId }: { checklistItemId: string, boardId: string }) {
-    if (!checklistItemId) {
-        return { success: false, message: 'Checklist item ID is missing' };
-    }
-
-    try {
-        await prisma.checklistItem.delete({
-            where: {
-                id: checklistItemId,
-            },
-        });
-
-        revalidatePath(`/board/${boardId}`);
-
-        return { success: true, message: 'Item deleted from checklist' };
-    } catch (e) {
-        return { success: false, message: 'Failed to delete item from checklist' };
-    }
+// Delete a checklist item.
+export async function handleDeleteChecklistItem({ checklistItemId, taskId }: { checklistItemId: string, taskId: string }) {
+    await prisma.checklistItem.delete({
+        where: {
+            id: checklistItemId,
+        },
+    });
+    revalidatePath(`/task/${taskId}`);
 }
 
 
-export async function handleToggleCheckedItem({ checklistItemId, isChecked, boardId }: { checklistItemId: string, isChecked: boolean, boardId: string }) {
-    if (!checklistItemId) {
-        return { success: false, message: 'Checklist item ID is missing' };
+// Toggle a checklist item.
+export async function handleToggleCheckedItem({ checklistItemId, isChecked, taskId }: { checklistItemId: string, isChecked: boolean, taskId: string }) {
+    await prisma.checklistItem.update({
+        where: { id: checklistItemId },
+        data: { isChecked },
+    });
+    revalidatePath(`/task/${taskId}`);
+}
+
+
+
+const handleEditChecklistNameSchema = z.object({
+    title: z.string().trim().min(1, "Title cannot be empty").max(30, "Title is too long"),
+    checklistId: z.string(),
+    taskId: z.string(),
+});
+
+export async function handleEditChecklistName(formData: FormData) {
+    const validatedFields = handleEditChecklistNameSchema.safeParse({
+        title: formData.get('title')?.toString(),
+        checklistId: formData.get('checklistId')?.toString(),
+        taskId: formData.get('taskId')?.toString(),
+    });
+
+    if (!validatedFields.success) {
+        const errorMessage = Object.values(validatedFields.error.flatten().fieldErrors).map(e => e.join(', ')).join('; ');
+    
+        return {
+            success: false,
+            message: `Validation failed: ${errorMessage}`,
+        };
     }
 
     try {
-        await prisma.checklistItem.update({
-            where: { id: checklistItemId },
-            data: { isChecked },
+        await prisma.checklist.update({
+            where: { id: validatedFields.data.checklistId },
+            data: { title: validatedFields.data.title},
         });
 
-        revalidatePath(`/board/${boardId}`);
+        revalidatePath(`/task/${validatedFields.data.taskId}`);
 
-        return { success: true, message: 'Checklist item toggled' };
+        return { success: true, message: 'Checklist title updated' };
     } catch (e) {
-        console.error('Error toggling checklist item:', e);
-        return { success: false, message: 'Failed to toggle checklist item' };
+        console.error(e);
+        return { success: false, message: 'Failed to update checklist name' };
     }
-}
 
+}
