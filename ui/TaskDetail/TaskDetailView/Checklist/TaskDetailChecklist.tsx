44fd7ec1969606cf687990b8bf42e0b8369145a1
Green Gem <chris_waitt@me.com>
2024-01-16 10:47:03 +0000

    Move checklist title and form to sub component to isolate as use client

--- ui/TaskDetail/TaskDetailView/Checklist/TaskDetailChecklist.tsx
+++ ui/TaskDetail/TaskDetailView/Checklist/TaskDetailChecklist.tsx
@@ -1,6 +1,3 @@
-import {
-    handleEditChecklistName 
-} from "@/actions/ChecklistServerActions";
 import { IconCheckbox } from "@tabler/icons-react";
 import { ExpandedTask } from "@/types/types";
 import TaskDetailItemContent from "../ui/TaskDetailItemContent";
@@ -8,9 +5,9 @@ import { Progress } from "@nextui-org/progress";
 import ChecklistItemForm from "./ChecklistItemForm.client";
 import DeleteChecklistButton from "./DeleteChecklistButton.client";
 import ChecklistCheckboxGroup from "./ChecklistCheckboxGroup.client";
+import ChecklistTitleForm from "./ChecklistTitleForm.client";
 
 export default function TaskDetailChecklist({ task }: { task: ExpandedTask }) {
-
     return (
         <>
             {task.checklists.map(checklist => {
@@ -21,26 +18,24 @@ export default function TaskDetailChecklist({ task }: { task: ExpandedTask }) {
                 const checkedItemIds = checklist.items
                     .filter(item => item.isChecked)
                     .map(item => item.id);
-
+                
                 return (
-                <div key={checklist.id}>
-                    <div className="flex w-full items-center justify-between mb-1">
-                        <div className="flex items-center grow">
-                            <div className='w-[40px] grow-0 shrink-0 justify-center'><IconCheckbox size={32} /></div>
-                            <h4 className='text-xl font-semibold grow'>
-                                {checklist.title || "Untitled Checklist"}
-                            </h4>
+                    <div key={checklist.id}>
+                        <div className="flex w-full items-center justify-between gap-5 mb-3">
+                            <div className="flex items-center grow">
+                                <div className='w-[40px] grow-0 shrink-0 justify-center'><IconCheckbox size={32} /></div>
+                                <ChecklistTitleForm checklistTitle={checklist.title} checklistId={checklist.id} taskId={task.id} />
+                            </div>
+                            <DeleteChecklistButton checklistId={checklist.id} taskId={task.id} />
                         </div>
-                        <DeleteChecklistButton checklistId={checklist.id} taskId={task.id} />
+                        <TaskDetailItemContent indented>
+                            <Progress aria-label="Completion progress" value={completionPercentage} className="w-full mb-3"/>
+                            <ChecklistCheckboxGroup taskId={task.id} checkedItemIds={checkedItemIds} checklist={checklist} />
+                            <ChecklistItemForm checklistId={checklist.id} taskId={task.id}/>
+                        </TaskDetailItemContent>
                     </div>
-                    <TaskDetailItemContent indented>
-                        <Progress aria-label="Completion progress" value={completionPercentage} className="w-full mb-3"/>
-                        <ChecklistCheckboxGroup taskId={task.id} checkedItemIds={checkedItemIds} checklist={checklist} />
-                        <ChecklistItemForm checklistId={checklist.id} taskId={task.id}/>
-                    </TaskDetailItemContent>
-                </div>
-            );
-        })}
-    </>
-);
+                );
+            })}
+        </>
+    );
 }
