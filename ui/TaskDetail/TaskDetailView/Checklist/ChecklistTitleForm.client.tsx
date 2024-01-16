63cf7bc4f988b404548d00bed89aa5c8aa748d35
Green Gem <chris_waitt@me.com>
2024-01-16 15:27:20 +0000

    Refactored checklist title form server action similar to the checklist item submission. with the addition that instead of using a default value i just set the inital state to the checklist name or a default name

--- ui/TaskDetail/TaskDetailView/Checklist/ChecklistTitleForm.client.tsx
+++ ui/TaskDetail/TaskDetailView/Checklist/ChecklistTitleForm.client.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { useFormState } from 'react-dom'
 import { useState, useEffect } from 'react';
 import { handleEditChecklistName } from "@/actions/ChecklistServerActions";
 import { Input } from "@nextui-org/input";
@@ -12,46 +11,49 @@ interface ChecklistTitleFormProps {
     taskId: string;
 }
 
-interface StateType {
-    success?: boolean;
-    message: string;
-}
-
-const initialState: StateType = {
-    message: '',
-    success: undefined,
-}
-
 export default function ChecklistTitleForm({ checklistTitle, checklistId, taskId }: ChecklistTitleFormProps) {
-    const [state, formAction] = useFormState(handleEditChecklistName, initialState);
-
     const [isEditing, setIsEditing] = useState(false);
+    const [error, setError] = useState("");
+    const [inputValue, setInputValue] = useState(checklistTitle || "Checklist");
 
     const toggleEditState = () => {
         setIsEditing(!isEditing);
     };
 
-    useEffect(() => {
-        if (state.success) {
-            toggleEditState();
-        }
-    }, [state.success]);
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+        if (error) setError('');
+    };
 
     return (
         <>
             {isEditing ? (
-                <form action={formAction} className="flex grow justify-between gap-2 items-center">
+                <form 
+                    className='flex grow justify-between gap-2 items-center'
+                    action={async (data) => {
+                        data.append('checklistId', checklistId);
+                        data.append('taskId', taskId);
+                    
+                        const response = await handleEditChecklistName(data);
+                        if (response.success) {
+                            toggleEditState();
+                        } else { 
+                            setError(response.message);
+                        }
+                    }}
+                >
                     <Input 
                         type="text" 
                         name="title" 
-                        defaultValue={checklistTitle || "Checklist"} 
+                        value={inputValue}
+                        onChange={handleInputChange}
                         labelPlacement="outside"
                         placeholder="Enter a name for your checklist..."
-                        autoFocus
+                        autoFocus 
+                        isInvalid={!!error}
+                        errorMessage={error}
                         size="sm"
                     />
-                    <input type="hidden" name="checklistId" value={checklistId} />
-                    <input type="hidden" name="taskId" value={taskId} />
                     <Button size="sm" color="primary" type="submit">Submit</Button>
                     <button type="button" onClick={toggleEditState}><IconX size={16} /></button>
                 </form>
