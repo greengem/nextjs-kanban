3d10e7ce3c1f00804227c2fa9dcf5b204838517b
Green Gem <chris_waitt@me.com>
2024-01-16 12:09:46 +0000

    feat: edit checklist item, needs ui toggling still

--- ui/TaskDetail/TaskDetailView/Checklist/ChecklistCheckboxGroup.client.tsx
+++ ui/TaskDetail/TaskDetailView/Checklist/ChecklistCheckboxGroup.client.tsx
@@ -1,9 +1,12 @@
 'use client'
+import { useState } from "react";
 import { CheckboxGroup, Checkbox } from "@nextui-org/checkbox";
-import { IconEdit } from "@tabler/icons-react";
+import { IconEdit, IconX } from "@tabler/icons-react";
 import DeleteChecklistItemButton from "./DeleteChecklistItemButton.client";
-import { handleToggleCheckedItem } from "@/actions/ChecklistServerActions";
+import { handleEditChecklistItemContent, handleToggleCheckedItem } from "@/actions/ChecklistServerActions";
 import { ChecklistSummary, ChecklistItemSummary } from "@/types/types";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
 
 interface ChecklistCheckboxGroupProps {
     checkedItemIds: string[],
@@ -12,6 +15,13 @@ interface ChecklistCheckboxGroupProps {
 }
 
 export default function ChecklistCheckboxGroup({ checkedItemIds, checklist, taskId} : ChecklistCheckboxGroupProps) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => {
+        setIsEditing(!isEditing);
+    };
+
     return (
         <CheckboxGroup className="mb-3" defaultValue={checkedItemIds}>
             {checklist.items.map((item: ChecklistItemSummary) => (
@@ -23,12 +33,23 @@ export default function ChecklistCheckboxGroup({ checkedItemIds, checklist, task
                             taskId: taskId 
                         })}
                     >
-                        {item.content}
+                        <span>{item.content}</span>
                     </Checkbox>
+                    <form action={handleEditChecklistItemContent} className="flex gap-2">
+                        <Input 
+                            size="sm"
+                            placeholder="Enter checklist item name..."
+                            labelPlacement="outside"
+                            name="content"
+                        />
+                        <input type="hidden" name="checklistItemId" value={item.id} />
+                        <input type="hidden" name="taskId" value={taskId} />
+                        <Button type="submit" size="sm" color="primary">Save</Button>
+                        <button><IconX size={16} /></button>
+                    </form>
                     <div className="flex gap-2">
                         <button className="shrink-0 grow-0"><IconEdit className="text-zinc-500 hover:text-primary" size={18} /></button>
                         <DeleteChecklistItemButton checklistItemId={item.id} taskId={taskId} />
-
                     </div>
                 </div>
             ))}
