c05e0ee29888e08ea3cf9dad2e6380f544d4f58f
Green Gem <chris_waitt@me.com>
2024-01-16 17:04:35 +0000

    tweaks to splash page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import Link from "next/link";
-import { IconBrandGithub, IconLayoutKanban, IconSparkles } from "@tabler/icons-react";
+import { IconBrandGithub, IconLayoutKanban } from "@tabler/icons-react";
 import { Button } from "@nextui-org/button";
 export default function Home() {
   return (
-    <main className="h-dvh">
+    <main className="h-dvh bg-black text-white flex flex-col justify-center">
 
-      <div className="flex justify-center py-5 mt-20">
-        <div className="bg-zinc-200 px-3 py-1 rounded-full text-sm inline-flex items-center gap-2 tracking-tight font-semibold"><IconSparkles size={16} /> Under construction</div>
-      </div>
-
-      <h1 className="text-7xl tracking-tighter font-bold flex items-center justify-center">
+      <h1 className="text-7xl tracking-tight font-bold flex flex-wrap items-center justify-center mb-5">
         <span><IconLayoutKanban size={80} className="text-primary" /></span>
         <span className="mr-2">
           <span className="text-primary">N</span>ext
@@ -19,14 +15,13 @@ export default function Home() {
         </span>
       </h1>
 
-      <div className="text-center mb-10">
-        <h1 className="text-7xl font-bold tracking-tighter mb-2">Elevate your efficiency</h1>
-        <p className="text-large text-zinc-700">Lorem ipsum bla bla bla</p>
+      <div className="text-center mb-10 mx-auto">
+        <h1 className="text-4xl font-bold tracking-tighter">Revolutionizing Task Management With <span className="text-primary">AI</span></h1>
       </div>
 
-      <div className="flex items-center justify-center gap-2 text-center">
-        <Button as={Link} color="primary" href="/board">🚀 Get Started</Button>
-        <Button as={Link} href='https://github.com/greengem/nextjs-kanban'><IconBrandGithub size={16} />Github</Button>
+      <div className="flex items-center justify-center gap-2 text-center dark">
+        <Button variant="ghost" as={Link} href="/board">🚀 Get Started</Button>
+        <Button variant="ghost" as={Link} href='https://github.com/greengem/nextjs-kanban'><IconBrandGithub size={16} />Github</Button>
       </div>
 
     </main>
