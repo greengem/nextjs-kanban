461284bb9c5aa325e44c159475d437a541a1c288
Green Gem <chris_waitt@me.com>
2024-01-16 12:29:34 +0000

    Added ui toggling to edit checklist items. the checklist section is now mostly done except for handling the form state and returning it to the front end

--- ui/TaskDetail/TaskDetailView/Checklist/ChecklistCheckboxGroup.client.tsx
+++ ui/TaskDetail/TaskDetailView/Checklist/ChecklistCheckboxGroup.client.tsx
@@ -14,41 +14,51 @@ interface ChecklistCheckboxGroupProps {
     taskId: string
 }
 
-export default function ChecklistCheckboxGroup({ checkedItemIds, checklist, taskId} : ChecklistCheckboxGroupProps) {
+export default function ChecklistCheckboxGroup({ checkedItemIds, checklist, taskId }: ChecklistCheckboxGroupProps) {
+    const [editingItemId, setEditingItemId] = useState<string | null>(null);
 
-    const [isEditing, setIsEditing] = useState(false);
+    const handleEditClick = (itemId: string) => {
+        setEditingItemId(itemId);
+    };
 
-    const handleEditClick = () => {
-        setIsEditing(!isEditing);
+    const handleCancelOrSubmit = () => {
+        setEditingItemId(null);
     };
 
     return (
         <CheckboxGroup className="mb-3" defaultValue={checkedItemIds}>
             {checklist.items.map((item: ChecklistItemSummary) => (
                 <div className="flex justify-between gap-5 hover:bg-zinc-300 py-1 px-2 rounded-md" key={item.id}>
-                    <Checkbox className="grow" value={item.id}
-                        onChange={(event) => handleToggleCheckedItem({ 
+                    <div className="flex grow items-center">
+                        <Checkbox value={item.id} onChange={(event) => handleToggleCheckedItem({ 
                             checklistItemId: item.id, 
                             isChecked: event.target.checked, 
                             taskId: taskId 
-                        })}
-                    >
-                        <span>{item.content}</span>
-                    </Checkbox>
-                    <form action={handleEditChecklistItemContent} className="flex gap-2">
-                        <Input 
-                            size="sm"
-                            placeholder="Enter checklist item name..."
-                            labelPlacement="outside"
-                            name="content"
-                        />
-                        <input type="hidden" name="checklistItemId" value={item.id} />
-                        <input type="hidden" name="taskId" value={taskId} />
-                        <Button type="submit" size="sm" color="primary">Save</Button>
-                        <button><IconX size={16} /></button>
-                    </form>
+                        })}>
+                            {editingItemId !== item.id && <span>{item.content}</span>}
+                        </Checkbox>
+
+                        {editingItemId === item.id && (
+                            <form action={handleEditChecklistItemContent} className="flex gap-2 ml-2" onSubmit={handleCancelOrSubmit}>
+                                <Input 
+                                    size="sm"
+                                    placeholder="Enter checklist item name..."
+                                    labelPlacement="outside"
+                                    name="content"
+                                    defaultValue={item.content}
+                                />
+                                <input type="hidden" name="checklistItemId" value={item.id} />
+                                <input type="hidden" name="taskId" value={taskId} />
+                                <Button type="submit" size="sm" color="primary">Save</Button>
+                                <button type="button" onClick={handleCancelOrSubmit}><IconX size={16} /></button>
+                            </form>
+                        )}
+                    </div>
+
                     <div className="flex gap-2">
-                        <button className="shrink-0 grow-0"><IconEdit className="text-zinc-500 hover:text-primary" size={18} /></button>
+                        <button className="shrink-0 grow-0" onClick={() => handleEditClick(item.id)}>
+                            <IconEdit className="text-zinc-500 hover:text-primary" size={18} />
+                        </button>
                         <DeleteChecklistItemButton checklistItemId={item.id} taskId={taskId} />
                     </div>
                 </div>
