167232d49c4c8666c4787e9eef518ab64cfc1dcc
Green Gem <chris_waitt@me.com>
2024-01-31 21:40:02 +0000

    Fix for type error in build time

--- actions/BoardServerActions.tsx
+++ actions/BoardServerActions.tsx
@@ -1,6 +1,7 @@
 'use server';
 import { auth } from "@/auth";
 import prisma from '@/db/prisma';
+import { PrismaClient } from "@prisma/client";
 import { revalidatePath } from 'next/cache';
 import { CreateBoardSchema, EditBoardSchema } from '@/types/zodTypes';
 import { BoardCreationData, BoardEditData } from "@/types/types";
@@ -108,7 +109,7 @@ export async function handleDeleteBoard(boardId: string) {
   }
 
   try {
-    await prisma.$transaction(async (prisma) => {
+    await prisma.$transaction(async (prisma: PrismaClient) => {
       const owner = await prisma.boardMember.findFirst({
         where: {
           boardId: boardId,
