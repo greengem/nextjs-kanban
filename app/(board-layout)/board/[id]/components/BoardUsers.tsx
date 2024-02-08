f9a4aba2aebf81c7c91c382fb3765723603b3669
Green Gem <chris_waitt@me.com>
2024-02-08 18:29:18 +0000

    Refactor BoardUsers component to include user information

--- app/(board-layout)/board/[id]/components/BoardUsers.tsx
+++ app/(board-layout)/board/[id]/components/BoardUsers.tsx
@@ -1,8 +1,11 @@
 import { auth } from "@/auth";
 import prisma from '@/db/prisma';
-import BoardUsersList from './BoardUsersList';
 import BoardAddUsers from './AddUsers/BoardAddUsers';
-import { BoardMember } from "@prisma/client";
+import { BoardMember, User } from "@prisma/client";
+
+type BoardMemberWithUser = BoardMember & {
+    user: User;
+  };
 
 export default async function BoardUsers({ boardId }: { boardId: string }) {
     const session = await auth();
@@ -12,24 +15,16 @@ export default async function BoardUsers({ boardId }: { boardId: string }) {
       return <div>User not authenticated</div>;
     }
 
-    // Assuming 'boardMembers' is an array of BoardMember (including the user relation)
     const boardMembers = await prisma.boardMember.findMany({
-        where: { boardId: boardId },
-        include: { user: true }
-    });
+    where: { boardId: boardId },
+    include: { user: true }
+    }) as BoardMemberWithUser[];
 
-    // Explicitly type 'member' as 'BoardMember' to fix the TypeScript error
-    const owner = boardMembers.find((member: BoardMember) => member.role === 'owner')?.user ?? null;
+    const owner = boardMembers.find((member) => member.role === 'owner')?.user ?? null;
     const isOwner = owner?.id === userId;
-
-    // Similarly, explicitly type 'member' in the filter and map functions
-    const members = boardMembers.filter((member: BoardMember) => member.role === 'member').map((member: BoardMember) => member.user);
+    
+    const members = boardMembers.filter((member) => member.role === 'member').map((member) => member.user);
   
-    return (
-        <>
-            <BoardUsersList owner={owner} members={members} />
-            <BoardAddUsers boardId={boardId} owner={owner} members={members} isOwner={isOwner} loggedInUserId={userId} />
-        </>
-    );
+    return <BoardAddUsers boardId={boardId} owner={owner} members={members} isOwner={isOwner} loggedInUserId={userId} />;
 }
 
