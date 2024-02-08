d64f5a628c772418252e0741bc871a68a9e96001
Green Gem <chris_waitt@me.com>
2024-02-08 18:28:17 +0000

    Refactor BoardPage component to include extended types

--- app/(board-layout)/board/[id]/page.tsx
+++ app/(board-layout)/board/[id]/page.tsx
@@ -1,29 +1,27 @@
+import { Board as BoardType, Column, Task, Label } from '@prisma/client';
 import prisma from '@/db/prisma';
 import { auth } from "@/auth";
-import { BoardDetails, ColumnWithTasks, LabelSummary } from "@/types/types";
 import Board from "./components/Board";
 import BoardNavbar from "./components/BoardNavbar";
 import Image from 'next/image';
 import { redirect } from 'next/navigation'
 
-export default async function BoardPage({
-  params,
-  searchParams
-}: {
-  params: { id: string };
-  searchParams: { labels?: string };
-}) {
+type ExtendedTask = Task & {
+  labels: Label[];
+};
+
+type ExtendedColumn = Column & {
+  tasks: ExtendedTask[];
+};
+
+type ExtendedBoard = BoardType & {
+  columns: ExtendedColumn[];
+};
+
+export default async function BoardPage({ params, searchParams }: { params: { id: string }; searchParams: { labels?: string };}) {
   const session = await auth();
   const userId = session?.user?.id;
-
-  if (!userId) {
-    return <div>User not authenticated</div>;
-  }
-
-  const labelIds = searchParams.labels ? searchParams.labels.split(',') : [];
-  const labelCondition: { labels?: { some: { id: { in: string[] } } } } = labelIds.length > 0
-    ? { labels: { some: { id: { in: labelIds } } } }
-    : {};
+  if (!userId) { return <div>User not authenticated</div> };
   
   const isMember = await prisma.boardMember.findFirst({
     where: {
@@ -31,73 +29,27 @@ export default async function BoardPage({
       userId: userId,
     }
   });
+  if (!isMember) { redirect('/board')};
 
-  if (!isMember) {
-    redirect('/board')
-  }
-
-  const board = await prisma.board.findUnique({
+  const board: ExtendedBoard | null = await prisma.board.findUnique({
     where: { id: params.id },
-    select: {
-      id: true,
-      title: true,
-      backgroundUrl: true,
-      favoritedBy: {
-        where: {
-          userId: userId
-        },
-        select: {
-          userId: true
-        }
-      },
+    include: {
       columns: {
-        orderBy: {
-          order: 'asc'
-        },
-        select: {
-          id: true,
-          title: true,
-          order: true,
+        orderBy: { order: 'asc' },
+        include: {
           tasks: {
-            orderBy: {
-              order: 'asc'
+            orderBy: { order: 'asc' },
+            include: {
+              labels: true,
             },
-            where: labelCondition,
-            select: {
-              id: true,
-              title: true,
-              description: true,
-              order: true,
-              columnId: true,
-              startDate: true,
-              dueDate: true,
-              labels: {
-                select: {
-                  id: true,
-                  title: true,
-                  color: true
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+          },
+        },
+      },
+    },
   });
+  
 
-  if (!board) {
-    return <div>Board not found</div>;
-  }
-
-  const uniqueLabels = extractUniqueLabels(board.columns);
-
-  const boardDetails: BoardDetails & { uniqueLabels: LabelSummary[] } = {
-    ...board,
-    isFavorited: !!board.favoritedBy?.length,
-    tasksCount: board.columns?.reduce((sum: number, column: ColumnWithTasks) => sum + column.tasks.length, 0) ?? 0,
-    uniqueLabels
-};
-
+  if (!board) { return <div>Board not found</div> };
 
   return (
     <main className="flex flex-col grow min-w-0 bg-cover bg-center bg-zinc-200 relative">
@@ -109,20 +61,8 @@ export default async function BoardPage({
           fill
       />
       )}
-      <BoardNavbar board={boardDetails} />
-      <Board board={boardDetails} session={session} />
+      <BoardNavbar board={board} />
+      <Board board={board} />
     </main>
   );
-}
-
-function extractUniqueLabels(columns: ColumnWithTasks[] | undefined): LabelSummary[] {
-  const uniqueLabelsMap = new Map<string, LabelSummary>();
-  columns?.forEach(column => {
-    column.tasks.forEach(task => {
-      task.labels.forEach(label => {
-        uniqueLabelsMap.set(label.id, label);
-      });
-    });
-  });
-  return Array.from(uniqueLabelsMap.values());
 }
