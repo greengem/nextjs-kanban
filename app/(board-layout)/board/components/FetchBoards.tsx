60184c01a0de5f96bc1d82ecd1f73196894aa00d
Green Gem <chris_waitt@me.com>
2024-02-08 16:31:43 +0000

    Refactor FetchBoards component to include board details

--- app/(board-layout)/board/components/FetchBoards.tsx
+++ app/(board-layout)/board/components/FetchBoards.tsx
@@ -1,44 +1,52 @@
-import { getBoardsSummary } from "@/lib/FetchData";
-import { BoardSummary } from "@/types/types";
+import { BoardMember, Board } from "@prisma/client";
+import { auth } from "@/auth";
+import prisma from '@/db/prisma';
 import Link from "next/link";
 import Image from "next/image";
-import { IconList, IconStarFilled  } from "@tabler/icons-react";
+
+type BoardWithDetails = BoardMember & {
+    board: Pick<Board, 'id' | 'title' | 'backgroundUrl'>;
+};
 
 export default async function FetchBoards() {
-    const boards: BoardSummary[] = await getBoardsSummary();
+    const session = await auth();
+    const userId = session?.user?.id;
+    if (!userId) {
+        return [];
+    }
+
+    const boards: BoardWithDetails[] = await prisma.boardMember.findMany({
+        where: {
+            userId: userId,
+        },
+        include: {
+            board: {
+                select: {
+                    id: true,
+                    title: true,
+                    backgroundUrl: true,
+                }
+            }
+        },
+        orderBy: {
+            createdAt: 'asc',
+        }
+    });
 
     return (
         <>
-            {boards.map((board) => (
-                <Link key={board.id} href={`/board/${board.id}`}>
+            {boards.map((boardMember) => (
+                <Link key={boardMember.board.id} href={`/board/${boardMember.board.id}`}>
                     <div className="h-32 flex flex-col justify-end relative rounded-xl shadow-lg bg-white hover:bg-zinc-100 relative overflow-hidden">
                         <div className="absolute top-0 bottom-0 left-0 right-0 bg-white/40 backdrop-blur-md z-10"></div>
-                        {board.backgroundUrl && (
+                        {boardMember.board.backgroundUrl && (
                             <Image 
                                 className='object-cover object-center z-0'
-                                src={board.backgroundUrl} 
+                                src={boardMember.board.backgroundUrl} 
                                 alt='Board Wallpaper' 
                                 fill
                             />
                         )}
-
-                        {board.isFavorited && (
-                            <span className="absolute text-xs top-2 left-2 text-primary bg-white p-1 rounded-md z-20">
-                                <IconStarFilled size={14} />
-                            </span>
-                        )}
-
-                        <span 
-                            className="
-                                absolute top-2 right-2 
-                                flex items-center justify-center gap-1
-                                text-xs text-primary bg-white
-                                p-1 rounded-md z-20
-                            "
-                        >
-                            <IconList size={16} /><span>{board.tasksCount}</span>
-                        </span>
-                        
                         <h4 className="
                             text-black font-semibold tracking-tight
                             z-20
@@ -46,11 +54,11 @@ export default async function FetchBoards() {
                             p-2 
                             overflow-ellipsis overflow-x-hidden whitespace-nowrap block
                             "
-                        >{board.title}</h4>
+                        >{boardMember.board.title}</h4>
 
                     </div>
                 </Link>
             ))}
         </>  
-    )
+    );
 }
