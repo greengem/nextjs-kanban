b92b0503bff851eb5c4bbde044928b5ccf51cce7
Green Gem <chris_waitt@me.com>
2024-02-09 13:59:08 +0000

    Update Navbar link in NavbarTitle component

--- ui/Navbar/NavbarTitle.tsx
+++ ui/Navbar/NavbarTitle.tsx
@@ -4,7 +4,7 @@ export default function NavbarTitle() {
 
     return (
         <div className="flex items-center my-2 gap-5">
-            <Link href='/board/' className="text-xl tracking-tight">
+            <Link href='/' className="text-xl tracking-tight">
                 NextBoard <span className="text-primary">Alpha</span>
             </Link>
         </div>
