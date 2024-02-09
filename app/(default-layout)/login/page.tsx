29ae12ea87021feb05b41521ff08c32de8c681d1
Green Gem <chris_waitt@me.com>
2024-02-09 13:10:56 +0000

    Remove email sign-in option and add Discord sign-in option

--- app/(default-layout)/login/page.tsx
+++ app/(default-layout)/login/page.tsx
@@ -2,8 +2,9 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import SignInGithub from "./SignInGithub";
-import { IconLayoutKanban, IconMail } from "@tabler/icons-react";
+import { IconLayoutKanban} from "@tabler/icons-react";
 import SignInGoogle from "./SignInGoogle";
+import SignInDiscord from "./SignInDiscord";
 
 export default async function CustomSignInPage() {
     const session = await auth();
@@ -12,26 +13,16 @@ export default async function CustomSignInPage() {
       } else {
         return (
             <div className="flex justify-center items-center h-full">
-                <Card className="max-w-96 shadow-xl bg-white border-3 border-primary" shadow="none">
-                    <CardHeader className="px-5 py-3 flex items-center">
+                <Card className="min-w-96 shadow-xl bg-white border-2 border-primary" shadow="none">
+                    <CardHeader className="px-10 py-5 flex items-center">
                       <IconLayoutKanban className="text-primary w-8 h-8 md:w-14 md:h-14" />
                       <h3 className="text-3xl md:text-5xl tracking-tighter text-center w-full font-bold">Next Kanban</h3>  
                     </CardHeader>
-                    <CardBody className="space-y-4 p-5 pt-0">
+                    <CardBody className="space-y-3 p-10 pt-0">
                         <p className="uppercase text-xs text-center text-primary">- OAuth- </p>
                         <SignInGithub />
                         <SignInGoogle />
-                        {/*
-                        <Divider />
-                        <p className="uppercase text-xs text-center text-primary">- Email- </p>
-                        <form className="flex flex-col space-y-2">
-                            <Input size="sm" label="Enter your email" type="email" placeholder="Email" />
-                            <Input size="sm" label="Enter a Password" type="password" placeholder="Password" />
-                            <Button variant="ghost">
-                                <IconMail />Sign In With Email
-                            </Button>
-                        </form>
-                        */}
+                        <SignInDiscord />
                     </CardBody>
                 </Card>
             </div>
