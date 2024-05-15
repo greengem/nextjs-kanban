dc86670f8718b0cf32385753931b48460dc8ae13
Green Gem <chris_waitt@me.com>
2024-05-15 19:32:38 +0100

    Reintroduced intercepting paralllel modal route

---
+++ app/(board-layout)/@modal/(.)task/[id]/page.tsx
@@ -0,0 +1,14 @@
+import TaskDetail from "@/ui/TaskDetail/TaskDetail";
+import TaskModal from "@/ui/TaskModal";
+
+interface BoardProps {
+  params: { id: string };
+}
+
+export default async function Task({ params }: BoardProps) {
+    return (
+        <TaskModal>
+            <TaskDetail taskId={params.id} />
+        </TaskModal>
+    );
+}
