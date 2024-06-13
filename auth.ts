4ffaa7e8ff2dfb9aff5bcf6fe9210fc5c962f50d
Green Gem <chris_waitt@me.com>
2024-06-13 12:58:35 +0100

    fix: correct prisma import in auth.ts

--- auth.ts
+++ auth.ts
@@ -1,29 +1,29 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "./db/prisma";
+import prisma from "@/prisma/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GitHubProvider({
-            clientId: process.env.AUTH_GITHUB_ID,
-            clientSecret: process.env.AUTH_GITHUB_SECRET,
-        }),
-    ],
-    session: { strategy: "jwt" },
-    callbacks: {
-        jwt: async ({ user, token }) => {
-            if (user) {
-                token.uid = user.id;
-            }
-            return token;
-        },
-        session: async ({ session, token }) => {
-            if (session?.user) {
-                session.user.id = token.uid as string;
-            }
-            return session;
-        }
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+  session: { strategy: "jwt" },
+  callbacks: {
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
     },
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.uid as string;
+      }
+      return session;
+    },
+  },
 });
