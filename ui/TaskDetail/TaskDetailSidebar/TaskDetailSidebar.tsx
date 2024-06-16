0387b3406a5f267fdad344709fc1f874128221a0
Green Gem <chris_waitt@me.com>
2024-06-16 21:40:23 +0100

    refactor: Update TaskDetailSidebar to use DetailedTask type and improve task detail rendering

--- ui/TaskDetail/TaskDetailSidebar/TaskDetailSidebar.tsx
+++ ui/TaskDetail/TaskDetailSidebar/TaskDetailSidebar.tsx
@@ -1,12 +1,12 @@
-import { ExpandedTask } from "@/types/types";
+import { DetailedTask } from "@/types/types";
 import TaskDetailAddToCard from "./TaskDetailAddToCard/TaskDetailAddToCard";
 import TaskDetailActions from "./TaskDetailActions/TaskDetailActions";
 
-export default function TaskDetailSidebar({ task }: { task: ExpandedTask }) {
+export default function TaskDetailSidebar({ task }: { task: DetailedTask }) {
   return (
     <div className="col-span-1">
       <TaskDetailAddToCard task={task} />
-      <TaskDetailActions selectedTask={task} boardId={task.column.boardId} />
+      <TaskDetailActions task={task} />
     </div>
   );
 }
