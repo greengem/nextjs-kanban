147b8195ebdb0626c4428de9bd1362693c6ce8dd
Green Gem <chris_waitt@me.com>
2024-06-16 21:41:26 +0100

    refactor: Update TaskDetailTitle component to use taskId, taskTitle, columnTitle, boardId, taskCreatedAt, and taskUpdatedAt

--- ui/TaskDetail/TaskDetailTitle/TaskModalTitle.tsx
+++ ui/TaskDetail/TaskDetailTitle/TaskModalTitle.tsx
@@ -1,35 +1,36 @@
-import { ExpandedTask } from "@/types/types";
 import EditTaskNameForm from "@/ui/Forms/EditTaskNameForm";
 import { IconCards } from "@tabler/icons-react";
 import { format } from "date-fns";
 
 export default function TaskDetailTitle({
-  selectedTask,
+  taskId,
+  taskTitle,
+  columnTitle,
   boardId,
+  taskCreatedAt,
+  taskUpdatedAt,
 }: {
-  selectedTask: ExpandedTask;
+  taskId: string;
+  taskTitle: string;
+  columnTitle: string;
   boardId: string;
+  taskCreatedAt: Date;
+  taskUpdatedAt: Date;
 }) {
   return (
     <div className="flex gap-3 p-5">
       <IconCards size={32} />
 
       <div className="flex-col w-full pr-5">
-        <EditTaskNameForm
-          taskId={selectedTask.id}
-          title={selectedTask.title}
-          boardId={boardId}
-        />
+        <EditTaskNameForm taskId={taskId} title={taskTitle} boardId={boardId} />
 
         <div className="text-sm font-normal">
           <p>
-            Created on{" "}
-            {format(new Date(selectedTask.createdAt), "MMMM d, yyyy")} | Updated
-            on {format(new Date(selectedTask.updatedAt), "MMMM d, yyyy")}
+            Created on {format(new Date(taskCreatedAt), "MMMM d, yyyy")} |
+            Updated on {format(new Date(taskUpdatedAt), "MMMM d, yyyy")}
           </p>
           <p>
-            In column{" "}
-            <span className="text-primary">{selectedTask.column.title}</span>
+            In column <span className="text-primary">{columnTitle}</span>
           </p>
         </div>
       </div>
