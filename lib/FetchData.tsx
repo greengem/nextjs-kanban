784436dd1650900424fcaaf1e0337ad8d11ed49c
Green Gem <chris_waitt@me.com>
2024-06-16 21:39:20 +0100

    refactor: Update FetchData to return Promise<Label[]> for getLabelsForBoard function

--- lib/FetchData.tsx
+++ lib/FetchData.tsx
@@ -1,16 +1,11 @@
 import prisma from "@/prisma/prisma";
+import { Label } from "@prisma/client";
 
 // Fetch labels
-export async function getLabelsForBoard(boardId: string) {
+export async function getLabelsForBoard(boardId: string): Promise<Label[]> {
   const labels = await prisma.label.findMany({
     where: {
       boardId: boardId,
-    },
-    select: {
-      id: true,
-      title: true,
-      color: true,
-      isDefault: true,
     },
   });
 
