141993012c4defd251cedbe1aba62ad76c09f96b
Green Gem <chris_waitt@me.com>
2024-06-16 21:41:31 +0100

    refactor: Update TaskDetailView component to use DetailedTask type and add AvatarGroup component

--- ui/TaskDetail/TaskDetailView/TaskDetailView.tsx
+++ ui/TaskDetail/TaskDetailView/TaskDetailView.tsx
@@ -1,24 +1,47 @@
 import { auth } from "@/auth";
-import { ExpandedTask } from "@/types/types";
+import { DetailedTask } from "@/types/types";
 import TaskDetailDescription from "./Description/TaskDetaillDescription";
 import TaskDetailActivity from "./Activity/TaskDetailActivity";
 import TaskDetailChecklist from "./Checklist/TaskDetailChecklist";
 import TaskDetailDates from "./Dates/TaskDetailDates";
 import TaskDetailLabels from "./Labels/TaskDetailLabels";
+import { Avatar, AvatarGroup } from "@nextui-org/avatar";
 
-export default async function TaskDetailView({ task }: { task: ExpandedTask }) {
+export default async function TaskDetailView({ task }: { task: DetailedTask }) {
   const session = await auth();
 
   return (
     <div className="col-span-3">
+      {/* 
+      todo: add AvatarGroup component
+      <AvatarGroup isBordered>
+        {task.assignedUsers.map((assignment) => (
+          <Avatar
+            key={assignment.user.id}
+            showFallback
+            name={assignment.user.name || "Unknown"}
+            src={assignment.user.image || undefined}
+            isBordered
+          />
+        ))}
+      </AvatarGroup> */}
+
       <TaskDetailLabels labels={task.labels} />
       <TaskDetailDates startDate={task.startDate} dueDate={task.dueDate} />
       <TaskDetailDescription
-        selectedTask={task}
+        taskDescription={task.description}
+        taskId={task.id}
         boardId={task.column.boardId}
       />
-      <TaskDetailChecklist task={task} />
-      <TaskDetailActivity task={task} session={session} />
+      <TaskDetailChecklist taskId={task.id} checklists={task.checklists} />
+      <TaskDetailActivity
+        taskId={task.id}
+        boardId={task.column.boardId}
+        activities={task.activities}
+        columnTitle={task.column.title}
+        userName={session?.user?.name ?? null}
+        userImage={session?.user?.image ?? null}
+      />
     </div>
   );
 }
