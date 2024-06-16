12a9efec2cd273fce148055b3a9ecb5e289be697
Green Gem <chris_waitt@me.com>
2024-06-16 21:40:19 +0100

    refactor: Update TaskDetail component to use DetailedTask type and improve task detail rendering

--- ui/TaskDetail/TaskDetail.tsx
+++ ui/TaskDetail/TaskDetail.tsx
@@ -1,12 +1,19 @@
 import TaskDetailTitle from "./TaskDetailTitle/TaskModalTitle";
 import TaskDetailSidebar from "./TaskDetailSidebar/TaskDetailSidebar";
 import TaskDetailView from "./TaskDetailView/TaskDetailView";
+import { DetailedTask } from "@/types/types";
 
-export default async function TaskDetail({ task }: { task: any }) {
+export default async function TaskDetail({ task }: { task: DetailedTask }) {
   return (
     <>
-      <TaskDetailTitle selectedTask={task} boardId={task?.column.boardId} />
-
+      <TaskDetailTitle
+        taskId={task.id}
+        taskTitle={task.title}
+        columnTitle={task.column.title}
+        boardId={task.column.boardId}
+        taskCreatedAt={task.createdAt}
+        taskUpdatedAt={task.updatedAt}
+      />
       <div className="grid grid-cols-1 md:grid-cols-4 p-3 md:p-5 gap-x-5">
         <TaskDetailView task={task} />
         <TaskDetailSidebar task={task} />
