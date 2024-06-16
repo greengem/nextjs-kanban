d99deb951189ee9ddfeaa80241c220cbd47eba7a
Green Gem <chris_waitt@me.com>
2024-06-16 21:37:50 +0100

    refactor: Update BoardNavbar component to accept boardId and boardTitle as props

--- app/(board-layout)/board/[id]/components/BoardNavbar.tsx
+++ app/(board-layout)/board/[id]/components/BoardNavbar.tsx
@@ -9,29 +9,27 @@ import BoardBackgroundImageButton from "./BoardBackgroundImageButton";
 import BoardUsers from "./BoardUsers";
 import { IconLoader2 } from "@tabler/icons-react";
 
-type ExtendedColumn = Column & {
-  tasks: Task[];
-};
-
-type ExtendedBoard = BoardType & {
-  columns: ExtendedColumn[];
-};
-
-export default async function BoardNavbar({ board }: { board: ExtendedBoard }) {
+export default async function BoardNavbar({
+  boardId,
+  boardTitle,
+}: {
+  boardId: string;
+  boardTitle: string;
+}) {
   return (
     <div className="mb-5 z-10">
       <div className="flex justify-between items-center bg-zinc-950 px-5 py-2 overflow-x-auto no-scrollbar gap-2">
         <div className="flex gap-2 items-center">
-          <BoardTitle boardTitle={board.title} boardId={board.id} />
+          <BoardTitle boardTitle={boardTitle} boardId={boardId} />
           <Suspense
             fallback={<IconLoader2 className="animate-spin mx-3" size={18} />}
           >
-            <BoardFavourite boardId={board.id} />
+            <BoardFavourite boardId={boardId} />
           </Suspense>
           <Suspense
             fallback={<IconLoader2 className="animate-spin" size={16} />}
           >
-            <BoardFilterFetch boardId={board.id} />
+            <BoardFilterFetch boardId={boardId} />
           </Suspense>
           <BoardBackgroundImageButton />
         </div>
@@ -39,12 +37,12 @@ export default async function BoardNavbar({ board }: { board: ExtendedBoard }) {
           <Suspense
             fallback={<IconLoader2 className="animate-spin" size={16} />}
           >
-            <BoardUsers boardId={board.id} />
+            <BoardUsers boardId={boardId} />
           </Suspense>
-          <BoardMenu boardId={board.id} />
+          <BoardMenu boardId={boardId} />
         </div>
       </div>
-      <BoardBackgroundImage boardId={board.id} />
+      <BoardBackgroundImage boardId={boardId} />
     </div>
   );
 }
