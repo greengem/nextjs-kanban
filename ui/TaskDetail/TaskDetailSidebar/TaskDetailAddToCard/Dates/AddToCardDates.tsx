e735fa70c0a7ad8d36440b0b34251b30b5f6a517
Green Gem <chris_waitt@me.com>
2024-06-16 21:40:56 +0100

    refactor: Update AddToCardDates component to use taskId, boardId, startDate, and dueDate

--- ui/TaskDetail/TaskDetailSidebar/TaskDetailAddToCard/Dates/AddToCardDates.tsx
+++ ui/TaskDetail/TaskDetailSidebar/TaskDetailAddToCard/Dates/AddToCardDates.tsx
@@ -1,9 +1,20 @@
 "use client";
+import { DetailedTask } from "@/types/types";
 import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/popover";
 import { IconCalendar } from "@tabler/icons-react";
 import AddToCardDatesCalendar from "./AddToCardDatesCalendar";
 
-export default function AddToCardDates({ task }: { task: any }) {
+export default function AddToCardDates({
+  taskId,
+  boardId,
+  startDate,
+  dueDate,
+}: {
+  taskId: string;
+  boardId: string;
+  startDate: Date | null;
+  dueDate: Date | null;
+}) {
   return (
     <li className="bg-zinc-900 hover:bg-zinc-800 ring-zinc-800 rounded-md ring-2  hover:ring-primary">
       <Popover placement="left-start" triggerScaleOnOpen={false}>
@@ -13,7 +24,12 @@ export default function AddToCardDates({ task }: { task: any }) {
           </button>
         </PopoverTrigger>
         <PopoverContent>
-          <AddToCardDatesCalendar task={task} />
+          <AddToCardDatesCalendar
+            taskId={taskId}
+            boardId={boardId}
+            startDate={startDate}
+            dueDate={dueDate}
+          />
         </PopoverContent>
       </Popover>
     </li>
