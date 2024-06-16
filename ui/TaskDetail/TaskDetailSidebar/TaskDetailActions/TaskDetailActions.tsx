054fd85fd1f58b0241d9980669d03c9bd5b15041
Green Gem <chris_waitt@me.com>
2024-06-16 21:40:28 +0100

    refactor: Update TaskDetailActions to use DetailedTask type and optimize task deletion

--- ui/TaskDetail/TaskDetailSidebar/TaskDetailActions/TaskDetailActions.tsx
+++ ui/TaskDetail/TaskDetailSidebar/TaskDetailActions/TaskDetailActions.tsx
@@ -1,21 +1,16 @@
+import { DetailedTask } from "@/types/types";
 import DeleteTaskForm from "@/ui/Forms/DeleteTaskForm";
 
-export default function TaskDetailActions({
-  selectedTask,
-  boardId,
-}: {
-  selectedTask: any;
-  boardId: string;
-}) {
+export default function TaskDetailActions({ task }: { task: DetailedTask }) {
   return (
     <>
       <h4 className="text-sm text-zinc-700 font-semibold mb-1">Actions</h4>
       <ul className="text-sm space-y-2">
         <li className="px-2 py-2 rounded-md bg-danger text-white">
           <DeleteTaskForm
-            taskId={selectedTask.id}
-            boardId={boardId}
-            columnId={selectedTask.columnId}
+            taskId={task.id}
+            boardId={task.column.boardId}
+            columnId={task.columnId}
           />
         </li>
       </ul>
